aab2bacf1f480a7f3105c9c54db87b2ed63cd0a9
André Kohn <kohn.a@outlook.com>
2023-02-05 22:23:28 +0100

    Link native libraries if they exist

--- packages/flatsql-parser-sys/build.rs
+++ packages/flatsql-parser-sys/build.rs
@@ -3,11 +3,7 @@ extern crate cmake;
 use std::env;
 
 fn main() {
-    let dst = cmake::Config::new("../flatsql-parser")
-        .build_target("flatsql_parser")
-        .always_configure(true)
-        .build();
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    // Native build flags
     let target_os = env::var("CARGO_CFG_TARGET_OS");
     match target_os.as_ref().map(|x| &**x) {
         Ok("macos") => {
@@ -17,4 +13,25 @@ fn main() {
             println!("cargo:rustc-flags=-l dylib=stdc++");
         }
     }
+
+    // Check if native library exist
+    let build_dir = env::current_dir()
+        .unwrap()
+        .join("../flatsql-parser/build/native");
+    let profile = std::env::var("PROFILE").unwrap();
+    let build_dir = match profile.as_str() {
+        "debug" => build_dir.join("Debug"),
+        _ => build_dir.join("Release"),
+    };
+    if build_dir.join("libflatsql_parser.a").exists() {
+        println!("cargo:rustc-link-search=native={}", build_dir.display());
+        return;
+    }
+
+    // Build the cmake file
+    let dst = cmake::Config::new("../flatsql-parser")
+        .build_target("flatsql_parser")
+        .always_configure(true)
+        .build();
+    println!("cargo:rustc-link-search=native={}/build", dst.display());
 }
