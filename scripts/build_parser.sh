bf316b3d2c9742ab41c6412e15abfec931046bab
André Kohn <kohn.a@outlook.com>
2023-02-05 11:39:03 +0100

    Generate flatbuffer files

---
+++ scripts/build_parser.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+trap exit SIGINT
+
+PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
+
+MODE=${1:-Fast}
+echo "MODE=${MODE}"
+
+CPP_BUILD_DIR="${PROJECT_ROOT}/build/flatsql-parser/wasm/${MODE}"
+CPP_SOURCE_DIR="${PROJECT_ROOT}/libs/flatsql-parser"
+
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+
+ADDITIONAL_FLAGS=
+case $MODE in
+  "Debug") ADDITIONAL_FLAGS="-DCMAKE_BUILD_TYPE=Debug" ;;
+  "Fast") ADDITIONAL_FLAGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo" ;;
+  "Release") ADDITIONAL_FLAGS="-DCMAKE_BUILD_TYPE=Release" ;;
+   *) ;;
+esac
+echo "BUILD_TYPE=${MODE}"
+echo "WASI_SDK_PREFIX=${WASI_SDK_PREFIX}"
+echo "WASI_TOOLCHAIN=${WASI_CMAKE_TOOLCHAIN}"
+
+mkdir -p ${CPP_SOURCE_DIR}/build/wasm/${MODE}
+
+set -x
+cmake \
+    -S"${CPP_SOURCE_DIR}/" \
+    -B"${CPP_BUILD_DIR}/" \
+    -DWASI_SDK_PREFIX=${WASI_SDK_PREFIX} \
+    -DCMAKE_SYSROOT=${WASI_SYSROOT} \
+    -DCMAKE_TOOLCHAIN_FILE=${WASI_CMAKE_TOOLCHAIN} \
+    -DWASM=1 \
+    ${ADDITIONAL_FLAGS}
+
+make \
+    -C"${CPP_BUILD_DIR}" \
+    -j${CORES} \
+    flatsql_parser
+
+if [ ${MODE} == "Release" ]; then
+    wasm-opt -O3 -o ${CPP_BUILD_DIR}/flatsql_parser_opt.wasm ${CPP_BUILD_DIR}/dashql_parser.wasm
+    wasm-strip ${CPP_BUILD_DIR}/flatsql_parser_opt.wasm
+    mv ${CPP_BUILD_DIR}/flatsql_parser_opt.wasm ${CPP_BUILD_DIR}/dashql_parser.wasm
+fi
---
+++ scripts/generate_proto.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
+
+FLATC="flatc"
+${FLATC} --version \
+    && { echo "[ OK  ] Command: flatc"; } \
+    || { echo "[ ERR ] Command: flatc"; exit 1; }
+
+SPEC_DIR="${PROJECT_ROOT}/proto/"
+SPEC_INDEX="${SPEC_DIR}/flatsql/proto.fbs"
+
+OUT_DIR_CPP="${PROJECT_ROOT}/libs/flatsql-proto-cpp/include/flatsql/proto"
+OUT_DIR_TS="${PROJECT_ROOT}/libs/flatsql-proto-es/src/gen"
+OUT_DIR_RS="${PROJECT_ROOT}/libs/flatsql-proto-rs/src/"
+
+mkdir -p ${OUT_DIR_CPP} ${OUT_DIR_TS}
+
+${FLATC} -I ${SPEC_DIR} -o ${OUT_DIR_CPP} ${SPEC_INDEX} --cpp \
+        --gen-all \
+        --no-prefix --scoped-enums \
+        --reflect-types --reflect-names \
+        --gen-object-api --gen-name-strings --gen-compare \
+        --gen-mutable \
+    && { echo "[ OK  ] Generate C++ Library"; } \
+    || { echo "[ ERR ] Generate C++ Library"; exit 1; }
+
+${FLATC} -I ${SPEC_DIR} -o ${OUT_DIR_RS} ${SPEC_INDEX} --rust \
+        --gen-all \
+        --gen-object-api --gen-name-strings \
+    && { echo "[ OK  ] Generate Rust Library"; } \
+    || { echo "[ ERR ] Generate Rust Library"; exit 1; }
+
+${FLATC} -I ${SPEC_DIR} -o ${OUT_DIR_TS} ${SPEC_INDEX} --ts \
+        --gen-all \
+        --reflect-types --reflect-names \
+        --gen-name-strings --gen-compare \
+        --gen-mutable \
+    && { echo "[ OK  ] Generate Typescript Library"; } \
+    || { echo "[ ERR ] Generate Typescript Library"; exit 1; }
+
+
+TS_OUT_PROTO_BASE="${PROJECT_ROOT}/libs/flatsql-proto-es/src/gen/flatsql/proto/"
+TS_OUT_PROTO_DIRS=`ls ${TS_OUT_PROTO_BASE}/syntax/`
+rm "${TS_OUT_PROTO_BASE}/syntax.ts"
+
+PROTO_INDEX="${TS_OUT_PROTO_BASE}/syntax/index.ts"
+echo "Generating $PROTO_INDEX"
+echo > ${PROTO_INDEX}
+PROTO_FILES=`ls ${TS_OUT_PROTO_BASE}/syntax/*.ts`
+for PROTO_FILE in ${PROTO_FILES}; do
+    IMPORT="$(basename $PROTO_FILE)"
+    if [ "${IMPORT}" = "index.ts" ]; then continue; fi
+    echo "export * from \"./${IMPORT%.*}\";" >> ${PROTO_INDEX}
+done
