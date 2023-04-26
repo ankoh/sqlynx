2ca925d689a4d354e79062e99dc1ecbbaa09f500
André Kohn <kohn.a@outlook.com>
2023-04-26 22:33:57 +0200

    Fix leaks through missing bison actions

--- packages/flatsql/src/parser/parser_driver.cc
+++ packages/flatsql/src/parser/parser_driver.cc
@@ -346,15 +346,19 @@ std::shared_ptr<proto::ProgramT> ParserDriver::Parse(rope::Rope& in, bool trace_
 
     // Make sure we didn't leak into our temp allocators.
     // This can happen quickly when not consuming an allocated list in a bison rule.
-#define DEBUG_TEMP_POOL_LEAKS 1
-#if DEBUG_TEMP_POOL_LEAKS
-    driver.temp_list_elements.ForEachAllocated([](size_t value_id, NodeList::ListElement& elem) {
+#define DEBUG_BISON_LEAKS 0
+#if DEBUG_BISON_LEAKS
+    auto text = in.ToString();
+    auto text_view = std::string_view{text};
+    driver.temp_list_elements.ForEachAllocated([&](size_t value_id, NodeList::ListElement& elem) {
         std::cout << proto::EnumNameAttributeKey(static_cast<proto::AttributeKey>(elem.node.attribute_key())) << " "
-                  << proto::EnumNameNodeType(elem.node.node_type()) << std::endl;
+                  << proto::EnumNameNodeType(elem.node.node_type()) << " "
+                  << text_view.substr(elem.node.location().offset(), elem.node.location().length()) << std::endl;
     });
 #else
-    assert(driver.temp_lists.GetAllocatedNodeCount() == 0);
-    assert(driver.temp_list_elements.GetAllocatedNodeCount() == 0);
+    if (driver.errors.empty()) {
+        assert(driver.temp_list_elements.GetAllocatedNodeCount() == 0);
+    }
 #endif
 
     assert(driver.temp_nary_expressions.GetAllocatedNodeCount() == 0);
