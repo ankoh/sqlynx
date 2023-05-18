cfbcc98e6d1c8383ccfaf9a0060f8e11728ee484
Andre Kohn <kohn.a@outlook.com>
2023-05-18 21:05:10 +0200

    Stub printer and analyzer dump tools

--- packages/flatsql/tools/dump_analyzer.cc
+++ packages/flatsql/tools/dump_analyzer.cc
@@ -1,1 +1,23 @@
-int main(int argc, char* argv[]) {}
+#include <filesystem>
+#include <string_view>
+
+#include "flatsql/parser/parse_context.h"
+#include "gflags/gflags.h"
+#include "gtest/gtest.h"
+#include "pugixml.hpp"
+
+using namespace flatsql;
+
+DEFINE_string(source_dir, "", "Source directory");
+
+int main(int argc, char* argv[]) {
+    gflags::SetUsageMessage("Usage: ./dump_analyzer --source_dir <dir>");
+    gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+    if (!std::filesystem::exists(FLAGS_source_dir)) {
+        std::cout << "Invalid source directory: " << FLAGS_source_dir << std::endl;
+    }
+    auto source_dir = std::filesystem::path{FLAGS_source_dir};
+    (void)source_dir;
+    return 0;
+}
