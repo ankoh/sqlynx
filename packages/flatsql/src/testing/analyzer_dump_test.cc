dd55de57fc0f54a7df6d9c530bf0b6366d28a022
André Kohn <kohn.a@outlook.com>
2023-05-22 18:59:40 +0200

    Write analyzer dumps

--- packages/flatsql/src/testing/analyzer_dump_test.cc
+++ packages/flatsql/src/testing/analyzer_dump_test.cc
@@ -30,7 +30,7 @@ static void writeMaybeQuotedIdentifier(std::string& buffer, std::string_view nam
 }
 
 /// Print a qualified name as a string
-static std::string printQualifiedName(const proto::ProgramT& program, proto::QualifiedTableName& name) {
+static std::string printQualifiedName(const proto::ProgramT& program, const proto::QualifiedTableName& name) {
     std::string buffer;
     if (name.schema_name() != NULL_ID) {
         if (name.database_name() != NULL_ID) {
@@ -46,12 +46,34 @@ static std::string printQualifiedName(const proto::ProgramT& program, proto::Qua
     return buffer;
 }
 
+/// Print a qualified name
+static std::string printQualifiedName(const proto::ProgramT& program, const proto::QualifiedColumnName& name) {
+    std::string buffer;
+    if (name.table_name() != NULL_ID) {
+        if (name.schema_name() != NULL_ID) {
+            if (name.database_name() != NULL_ID) {
+                writeMaybeQuotedIdentifier(buffer, program.name_dictionary[name.database_name()]);
+                buffer += ".";
+            }
+            writeMaybeQuotedIdentifier(buffer, program.name_dictionary[name.schema_name()]);
+            buffer += ".";
+        }
+        writeMaybeQuotedIdentifier(buffer, program.name_dictionary[name.table_name()]);
+        buffer += ".";
+    }
+    if (name.column_name() != NULL_ID) {
+        writeMaybeQuotedIdentifier(buffer, program.name_dictionary[name.column_name()]);
+    }
+    return buffer;
+}
+
 namespace testing {
 
 void AnalyzerDumpTest::EncodeProgram(pugi::xml_node root, const proto::ProgramT& program, std::string_view text) {
     // Unpack modules
     auto& nodes = program.nodes;
     auto& statements = program.statements;
+    auto& name_res = *program.name_resolution;
     auto* stmt_type_tt = proto::StatementTypeTypeTable();
     auto* node_type_tt = proto::NodeTypeTypeTable();
 
@@ -65,35 +87,82 @@ void AnalyzerDumpTest::EncodeProgram(pugi::xml_node root, const proto::ProgramT&
     if (!program.name_resolution) {
         return;
     }
-    auto& name_res = *program.name_resolution;
 
+    // Write table declarations
     for (auto& table_decl : name_res.table_declarations) {
-        auto table = tables.append_child("table");
+        auto xml_tbl = tables.append_child("table");
         // Write table name
         auto table_name = printQualifiedName(program, *table_decl->table_name);
-        table.append_attribute("name").set_value(table_name.c_str());
+        xml_tbl.append_attribute("name").set_value(table_name.c_str());
         // Is external?
-        table.append_attribute("external").set_value(table_decl->is_external);
+        xml_tbl.append_attribute("external").set_value(table_decl->is_external);
         // Has a node id?
         if (auto node_id = table_decl->ast_node_id; node_id != NULL_ID) {
-            auto& ast_node = program.nodes[node_id];
-            EncodeLocation(table, ast_node.location(), text);
+            EncodeLocation(xml_tbl, program.nodes[node_id].location(), text);
         }
         // Write child columns
         for (auto& column_decl : table_decl->columns) {
-            auto column = table.append_child("column");
+            auto xml_col = xml_tbl.append_child("column");
             if (column_decl.column_name() != NULL_ID) {
                 auto column_name = program.name_dictionary[column_decl.column_name()];
-                column.append_attribute("name").set_value(column_name.c_str());
+                xml_col.append_attribute("name").set_value(column_name.c_str());
             } else {
-                column.append_attribute("name").set_value("?");
+                xml_col.append_attribute("name").set_value("?");
             }
             if (auto node_id = column_decl.ast_node_id(); node_id != NULL_ID) {
-                auto& ast_node = program.nodes[node_id];
-                EncodeLocation(column, ast_node.location(), text);
+                EncodeLocation(xml_col, program.nodes[node_id].location(), text);
             }
         }
-        // XXX
+    }
+
+    // Write table references
+    for (auto& ref : name_res.table_references) {
+        auto xml_ref = table_refs.append_child("table-reference");
+        auto table_name = printQualifiedName(program, ref.table_name());
+        xml_ref.append_attribute("name").set_value(table_name.c_str());
+        if (auto table_id = ref.resolved_table_id(); table_id != NULL_ID) {
+            xml_ref.append_attribute("table").set_value(table_id);
+        }
+        if (auto node_id = ref.ast_node_id(); node_id != NULL_ID) {
+            EncodeLocation(xml_ref, program.nodes[node_id].location(), text);
+        }
+    }
+
+    // Write column references
+    for (auto& ref : name_res.column_references) {
+        auto xml_ref = column_refs.append_child("column-reference");
+        auto table_name = printQualifiedName(program, ref.column_name());
+        xml_ref.append_attribute("name").set_value(table_name.c_str());
+        if (auto table_id = ref.resolved_table_id(); table_id != NULL_ID) {
+            xml_ref.append_attribute("table").set_value(table_id);
+        }
+        if (auto column_id = ref.resolved_column_id(); column_id != NULL_ID) {
+            xml_ref.append_attribute("column").set_value(column_id);
+        }
+        if (auto node_id = ref.ast_node_id(); node_id != NULL_ID) {
+            EncodeLocation(xml_ref, program.nodes[node_id].location(), text);
+        }
+    }
+
+    // Write join edges
+    for (auto& edge : name_res.join_edges) {
+        auto xml_edge = join_edges.append_child("join-edge");
+        if (auto node_id = edge->ast_node_id; node_id != NULL_ID) {
+            EncodeLocation(xml_edge, program.nodes[node_id].location(), text);
+        }
+        auto writeNodes = [](pugi::xml_node& edge, const proto::ProgramT& program, std::string_view text,
+                             const std::vector<flatsql::proto::HyperGraphNode>& nodes, size_t at) {
+            for (auto node : nodes) {
+                auto xml_node = edge.append_child("node");
+                xml_node.append_attribute("at").set_value(0);
+                xml_node.append_attribute("table").set_value(node.table_id());
+                xml_node.append_attribute("column").set_value(node.column_id());
+                assert(node.ast_node_id() != NULL_ID);
+                EncodeLocation(xml_node, program.nodes[node.ast_node_id()].location(), text);
+            }
+        };
+        writeNodes(xml_edge, program, text, edge->nodes_0, 0);
+        writeNodes(xml_edge, program, text, edge->nodes_1, 1);
     }
 }
 
