b317dce326436af2b516ec236b7287c44680312a
André Kohn <kohn.a@outlook.com>
2023-05-25 11:21:01 +0200

    Won't build on windows anytime soon

--- .github/actions/cpu-cores/action.ts
+++
@@ -1,6 +0,0 @@
-const os = require('os');
-const core = require('@actions/core');
-
-const numberOfCpus = typeof os.availableParallelism === 'function' ? os.availableParallelism() : os.cpus().length;
-
-core.setOutput('count', numberOfCpus);
