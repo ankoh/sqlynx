a90b3d0712f4ce0215feceed5bb4fccdd335f145
André Kohn <kohn.a@outlook.com>
2023-06-11 00:28:46 +0200

    Work on first analyzer dump test

--- packages/flatsql/src/testing/xml_tests.cc
+++ packages/flatsql/src/testing/xml_tests.cc
@@ -56,6 +56,28 @@ void EncodeLocation(pugi::xml_node n, proto::Location loc, std::string_view text
     }
 }
 
+void EncodeLocation(pugi::xml_node n, proto::Location loc, rope::Rope& text) {
+    std::string tmp;
+    auto begin = loc.offset();
+    auto end = loc.offset() + loc.length();
+    {
+        std::stringstream ss;
+        ss << begin << ".." << end;
+        n.append_attribute("loc") = ss.str().c_str();
+    }
+    {
+        std::stringstream ss;
+        if (loc.length() < INLINE_LOCATION_CAP) {
+            ss << text.Read(loc.offset(), loc.length(), tmp);
+        } else {
+            auto prefix = text.Read(loc.offset(), LOCATION_HINT_LENGTH, tmp);
+            auto suffix = text.Read(loc.offset() + loc.length() - LOCATION_HINT_LENGTH, LOCATION_HINT_LENGTH, tmp);
+            ss << prefix << ".." << suffix;
+        }
+        n.append_attribute("text") = ss.str().c_str();
+    }
+}
+
 void EncodeError(pugi::xml_node n, const proto::ErrorT& err, std::string_view text) {
     n.append_attribute("message") = err.message.c_str();
     EncodeLocation(n, *err.location, text);
