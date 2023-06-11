9051501b915cc1dbe8e261dd7b20b3ecb9c16457
André Kohn <kohn.a@outlook.com>
2023-06-11 10:42:13 +0200

    Work on name resolution tests

--- packages/flatsql/include/flatsql/testing/xml_tests.h
+++ packages/flatsql/include/flatsql/testing/xml_tests.h
@@ -16,7 +16,7 @@ namespace flatsql::testing {
 /// Encode a location
 void EncodeLocation(pugi::xml_node n, proto::Location loc, std::string_view text);
 /// Encode a location
-void EncodeLocation(pugi::xml_node n, proto::Location loc, rope::Rope& rope);
+void WriteLocation(pugi::xml_node n, proto::Location loc, rope::Rope& rope);
 /// Encode an error
 void EncodeError(pugi::xml_node n, const proto::ErrorT& err, std::string_view text);
 
