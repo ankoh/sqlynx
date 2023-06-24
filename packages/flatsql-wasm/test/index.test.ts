095b7b60ab64f5069a71986074716facdd2c38e9
André Kohn <kohn.a@outlook.com>
2023-06-25 00:42:30 +0200

    Get rid of readFileSync

--- packages/flatsql-wasm/test/index.test.ts
+++ packages/flatsql-wasm/test/index.test.ts
@@ -10,7 +10,7 @@ const wasmPath = path.resolve(distPath, './flatsql.wasm');
 describe('FlatSQL.create', () => {
     it('instantiate WebAssembly module', () => {
         flatsql.FlatSQL.create(async (imports: WebAssembly.Imports) => {
-            const buf = fs.readFileSync(wasmPath);
+            const buf = await fs.promises.readFile(wasmPath);
             return await WebAssembly.instantiate(buf, imports);
         });
     });
