7c545825e6671993cfe14a39176a67fe371f7c0c
André Kohn <kohn.a@outlook.com>
2023-06-28 00:51:07 +0200

    Add api highlighting test

---
+++ packages/flatsql/test/highlighting_test.cc
@@ -0,0 +1,43 @@
+#include <initializer_list>
+
+#include "flatsql/api.h"
+#include "flatsql/proto/proto_generated.h"
+#include "gtest/gtest.h"
+
+using namespace flatsql;
+
+using Token = proto::HighlightingTokenType;
+
+constexpr auto OK = static_cast<uint32_t>(proto::StatusCode::OK);
+
+static void match_tokens(const void* data, const std::vector<uint32_t>& offsets, const std::vector<Token>& types) {
+    auto scanned = flatbuffers::GetRoot<proto::ScannedScript>(data);
+    auto unpacked = scanned->UnPack();
+    ASSERT_EQ(unpacked->highlighting->token_offsets, offsets);
+    ASSERT_EQ(unpacked->highlighting->token_types, types);
+}
+
+TEST(HighlightingTest, InsertChars) {
+    auto script = flatsql_script_new();
+    FFIResult* result = nullptr;
+
+    size_t size = 0;
+    auto add_char = [&](char c, std::vector<uint32_t> offsets, std::vector<proto::HighlightingTokenType> types) {
+        flatsql_script_insert_char_at(script, size++, c);
+        result = flatsql_script_scan(script);
+        ASSERT_EQ(result->status_code, OK);
+        match_tokens(result->data_ptr, offsets, types);
+        flatsql_result_delete(result);
+    };
+
+    add_char('s', {0, 1}, {Token::IDENTIFIER, Token::NONE});
+    add_char('e', {0, 2}, {Token::IDENTIFIER, Token::NONE});
+    add_char('l', {0, 3}, {Token::IDENTIFIER, Token::NONE});
+    add_char('e', {0, 4}, {Token::IDENTIFIER, Token::NONE});
+    add_char('c', {0, 5}, {Token::IDENTIFIER, Token::NONE});
+    add_char('t', {0, 6}, {Token::KEYWORD, Token::NONE});
+    add_char(' ', {0, 6}, {Token::KEYWORD, Token::NONE});
+    add_char('1', {0, 6, 7, 8}, {Token::KEYWORD, Token::NONE, Token::LITERAL_INTEGER, Token::NONE});
+
+    flatsql_script_delete(script);
+}
