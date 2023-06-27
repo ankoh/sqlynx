9ea7638f64ef9a86f5309f58bafa0f78384b30e6
André Kohn <kohn.a@outlook.com>
2023-06-27 08:27:47 +0200

    Add ffi header

---
+++ packages/flatsql/include/flatsql/ffi.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+#include "flatsql/script.h"
+
+extern "C" std::byte* flatsql_malloc(size_t length);
+/// Delete memory
+extern "C" void flatsql_free(void* buffer);
+
+/// A managed FFI result container
+struct FFIResult {
+    uint32_t status_code;
+    uint32_t data_length;
+    const void* data_ptr;
+    void* owner_ptr;
+    void (*owner_deleter)(void*);
+};
+
+/// Create a script
+extern "C" flatsql::Script* flatsql_script_new();
+/// Delete a result
+extern "C" void flatsql_result_delete(FFIResult* result);
+/// Insert char at a position
+extern "C" void flatsql_script_insert_char_at(flatsql::Script* script, size_t offset, char unicode);
+/// Insert text at a position
+extern "C" void flatsql_script_insert_text_at(flatsql::Script* script, size_t offset, char* text_ptr,
+                                              size_t text_length);
+/// Erase a text range
+extern "C" void flatsql_script_erase_text_range(flatsql::Script* script, size_t offset, size_t count);
+/// Get the script content as string
+extern "C" FFIResult* flatsql_script_to_string(flatsql::Script* script);
+/// Scan a script
+extern "C" FFIResult* flatsql_script_scan(flatsql::Script* script);
+/// Parse a script
+extern "C" FFIResult* flatsql_script_parse(flatsql::Script* script);
+/// Analyze a script
+extern "C" FFIResult* flatsql_script_analyze(flatsql::Script* script, flatsql::Script* external);
