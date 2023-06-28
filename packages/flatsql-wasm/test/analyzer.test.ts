a705ad2a845efede0cb0e082a407287cf92bb28d
André Kohn <kohn.a@outlook.com>
2023-06-28 18:48:47 +0200

    Add js analyzer test

--- packages/flatsql-wasm/test/analyzer.test.ts
+++ packages/flatsql-wasm/test/analyzer.test.ts
@@ -18,5 +18,46 @@ beforeAll(async () => {
 });
 
 describe('FlatSQL Analyzer', () => {
-    it(`foo`, () => {});
+    it(`external ref`, () => {
+        const ext_script = fsql!.createScript();
+        ext_script.insertTextAt(0, 'create table foo(a int);');
+
+        const ext_scanner_res = ext_script.scan();
+        const ext_parser_res = ext_script.parse();
+        const ext_analyzer_res = ext_script.analyze();
+
+        const ext_scanner = ext_scanner_res.read(new flatsql.proto.ScannedScript());
+        const ext_parser = ext_parser_res.read(new flatsql.proto.ParsedScript());
+        const ext_analyzer = ext_analyzer_res.read(new flatsql.proto.AnalyzedScript());
+        expect(ext_scanner.highlighting()?.tokenTypesArray()?.length).toBeGreaterThan(0);
+        expect(ext_parser.nodesLength()).toBeGreaterThan(0);
+        expect(ext_analyzer.tablesLength()).toEqual(1);
+
+        const main_script = fsql!.createScript();
+        main_script.insertTextAt(0, 'select * from foo');
+
+        const main_scanner_res = main_script.scan();
+        const main_parser_res = main_script.parse();
+        const main_analyzer_res = main_script.analyze(ext_script);
+
+        const main_scanner = main_scanner_res.read(new flatsql.proto.ScannedScript());
+        const main_parser = main_parser_res.read(new flatsql.proto.ParsedScript());
+        const main_analyzer = main_analyzer_res.read(new flatsql.proto.AnalyzedScript());
+        expect(main_scanner.highlighting()?.tokenTypesArray()?.length).toBeGreaterThan(0);
+        expect(main_parser.nodesLength()).toBeGreaterThan(0);
+        expect(main_analyzer.tableReferencesLength()).toEqual(1);
+
+        const table_ref = main_analyzer.tableReferences(0);
+        const table_id = table_ref?.tableId()!;
+        expect(flatsql.FlatID.isExternal(table_id)).toEqual(true);
+        expect(flatsql.FlatID.isNull(table_id)).not.toEqual(true);
+        expect(flatsql.FlatID.maskIndex(table_id)).toEqual(0);
+
+        ext_scanner_res.delete();
+        ext_parser_res.delete();
+        ext_analyzer_res.delete();
+        main_scanner_res.delete();
+        main_parser_res.delete();
+        main_analyzer_res.delete();
+    });
 });
