7c9f6381d443eec9617b2617be87c1bb466c1b91
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-06-30 01:34:01 +0200

    LSP: Scaffolding for LSP and VS Code extension

---
+++ packages/flatsql-lsp/src/server.ts
@@ -0,0 +1,140 @@
+// Based on https://github.com/microsoft/vscode-extension-samples/tree/main/lsp-sample
+
+import {
+    createConnection,
+    TextDocuments,
+    Diagnostic,
+    DiagnosticSeverity,
+    ProposedFeatures,
+    InitializeParams,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentPositionParams,
+    TextDocumentSyncKind,
+    InitializeResult,
+} from 'vscode-languageserver/node';
+
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+    const capabilities = params.capabilities;
+
+    hasDiagnosticRelatedInformationCapability = !!(
+        capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation
+    );
+
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true,
+            },
+        },
+    };
+    return result;
+});
+
+connection.onInitialized(() => {});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+    validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+    const maxNumberOfProblems = 1000;
+
+    // The validator creates diagnostics for all uppercase words length 2 and more
+    const text = textDocument.getText();
+    const pattern = /\b[A-Z]{2,}\b/g;
+    let m: RegExpExecArray | null;
+
+    let problems = 0;
+    const diagnostics: Diagnostic[] = [];
+    while ((m = pattern.exec(text)) && problems < maxNumberOfProblems) {
+        problems++;
+        const diagnostic: Diagnostic = {
+            severity: DiagnosticSeverity.Warning,
+            range: {
+                start: textDocument.positionAt(m.index),
+                end: textDocument.positionAt(m.index + m[0].length),
+            },
+            message: `${m[0]} is all uppercase.`,
+            source: 'ex',
+        };
+        if (hasDiagnosticRelatedInformationCapability) {
+            diagnostic.relatedInformation = [
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: 'Spelling matters',
+                },
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: 'Particularly for names',
+                },
+            ];
+        }
+        diagnostics.push(diagnostic);
+    }
+
+    // Send the computed diagnostics to VSCode.
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion((_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    // The pass parameter contains the position of the text document in
+    // which code complete got requested. For the example we ignore this
+    // info and always provide the same completion items.
+    return [
+        {
+            label: 'TypeScript',
+            kind: CompletionItemKind.Text,
+            data: 1,
+        },
+        {
+            label: 'JavaScript',
+            kind: CompletionItemKind.Text,
+            data: 2,
+        },
+    ];
+});
+
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+    if (item.data === 1) {
+        item.detail = 'TypeScript details';
+        item.documentation = 'TypeScript documentation';
+    } else if (item.data === 2) {
+        item.detail = 'JavaScript details';
+        item.documentation = 'JavaScript documentation';
+    }
+    return item;
+});
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
