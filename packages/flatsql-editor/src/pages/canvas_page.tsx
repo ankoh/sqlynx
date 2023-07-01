5d7c95025584b0c5a6e34eda2a5f507eab7635ad
André Kohn <kohn.a@outlook.com>
2023-07-01 13:51:42 +0200

    Draw logo on top of flow graph

--- packages/flatsql-editor/src/pages/canvas_page.tsx
+++ packages/flatsql-editor/src/pages/canvas_page.tsx
@@ -12,13 +12,13 @@ interface Props {}
 export const CanvasPage: React.FC<Props> = (props: Props) => {
     return (
         <div className={styles.page}>
-            <div className={styles.header_container}>
-                <img src={logo} className={styles.header_logo} />
-            </div>
             <div className={styles.modelgraph_container}>
                 <ReactFlow nodes={[]} edges={[]} fitView attributionPosition="bottom-right">
                     <FlowBackground color="#aaa" variant={BackgroundVariant.Dots} gap={16} />
                 </ReactFlow>
+            </div>
+            <div className={styles.header_container}>
+                <img src={logo} className={styles.header_logo} />
             </div>
             <div className={styles.editor_section} style={{ height: '66%' }}>
                 <div className={styles.editor_card}>
