eb20d9424d153eeab567d82080391e7238c39a6c
André Kohn <kohn.a@outlook.com>
2023-07-01 13:44:22 +0200

    Add react-flow background

--- packages/flatsql-editor/src/pages/canvas_page.tsx
+++ packages/flatsql-editor/src/pages/canvas_page.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import ReactFlow, { BackgroundVariant, Background as FlowBackground } from 'reactflow';
 import { ScriptEditor } from '../editor/script_editor';
 
+import 'reactflow/dist/style.css';
 import styles from './canvas_page.module.css';
 
 interface Props {}
@@ -9,6 +11,11 @@ export const CanvasPage: React.FC<Props> = (props: Props) => {
     return (
         <div className={styles.page}>
             <div className={styles.header_container}>FlatSQL Logo</div>
+            <div className={styles.modelgraph_container}>
+                <ReactFlow nodes={[]} edges={[]} fitView attributionPosition="bottom-right">
+                    <FlowBackground color="#aaa" variant={BackgroundVariant.Dots} gap={16} />
+                </ReactFlow>
+            </div>
             <div className={styles.editor_section} style={{ height: '66%' }}>
                 <div className={styles.editor_card}>
                     <ScriptEditor />
