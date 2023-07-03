3b0885bf36ccad7d5a1336e487e693ab2cb2d3a8
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-07-03 10:57:50 +0200

    LSP: Use `dom` types
    TypeScript seems rather broken, so I give up on correctly typing
    the LSP server. It will run in Node, hence shouldn't have the DOM
    APIs available. However, the recent installation of `reactflow`
    to `flatsql-editor` broke the types in LSP server again. It seems
    nearly impossible to keep types contained to a single subproject.
    
    Hence, I now just use the `dom` library also in the tsconfig of LSP

--- packages/flatsql-lsp/types/globals.d.ts
+++
@@ -1,8 +0,0 @@
-// Those types are not declared by TypeScript for node
-// environments, but they do exist.
-// We don't actually use them in this module, but we
-// need their declarations so the `.d.ts` files from the
-// main `flatsql` package can be loaded successfully.
-
-interface TextEncoder {}
-interface TextDecoder {}
