fa78825289c6746c8e9c180eac50a6d8d13bfc90
André Kohn <kohn.a@outlook.com>
2023-07-05 17:22:32 +0200

    Simplify initialization of backend provider

--- packages/flatsql-editor/src/pages/canvas_page.tsx
+++ packages/flatsql-editor/src/pages/canvas_page.tsx
@@ -1,10 +1,9 @@
-import * as flatsql from '@ankoh/flatsql';
 import * as React from 'react';
 
-import ReactFlow, { BackgroundVariant, Background as FlowBackground } from 'reactflow';
-import { useBackend, useBackendResolver } from '../backend';
+import { useBackend } from '../backend';
 import { ScriptEditor } from '../editor/editor';
-import { useEditorContext } from '../editor/editor_context';
+import { SchemaGraph } from '../schema_graph/schema_graph';
+import { RESULT_OK } from '../utils/result';
 
 import 'reactflow/dist/style.css';
 import styles from './canvas_page.module.css';
@@ -17,88 +16,15 @@ import logo from '../../static/svg/logo/logo.svg';
 interface Props {}
 
 export const CanvasPage: React.FC<Props> = (props: Props) => {
-    const context = useEditorContext();
     const backend = useBackend();
-    const backendResolver = useBackendResolver();
-    if (backend.unresolved()) {
-        backendResolver();
-    }
-
-    const instance = backend.value?.instance;
     const version = React.useMemo(() => {
-        if (!instance) return 'unknown';
-        return instance!.getVersionText();
-    }, [instance]);
-
-    if (context) {
-        const parsed = context.mainParsed?.read(new flatsql.proto.ParsedScript())!;
-        const analyzed = context.mainAnalyzed?.read(new flatsql.proto.AnalyzedScript())!;
-        if (analyzed) {
-            // Collect tables
-            const tables = [];
-            const tableCount = analyzed.tablesLength();
-            let tableName = new flatsql.proto.QualifiedTableName();
-            let table = new flatsql.proto.Table();
-            let tableColumn = new flatsql.proto.TableColumn();
-            for (let i = 0; i < tableCount; ++i) {
-                table = analyzed.tables(i)!;
-                const columnCount = table.columnCount();
-                const columnsBegin = table.columnsBegin();
-                const columns = [];
-                for (let j = 0; j < columnCount; ++j) {
-                    tableColumn = analyzed.tableColumns(columnsBegin + j)!;
-                    columns.push(flatsql.FlatID.readName(tableColumn.columnName(), parsed));
-                }
-                tableName = table.tableName(tableName)!;
-                tables.push({
-                    name: flatsql.FlatID.readTableName(tableName, parsed),
-                    columns,
-                });
-            }
-            console.log(tables);
-
-            // Collect query edges
-            const edgeCount = analyzed.graphEdgesLength();
-            let edge = new flatsql.proto.QueryGraphEdge();
-            let node = new flatsql.proto.QueryGraphEdgeNode();
-            for (let i = 0; i < edgeCount; ++i) {
-                edge = analyzed.graphEdges(i, edge)!;
-                let reader = edge?.nodesBegin()!;
-                const countLeft = edge?.nodeCountLeft()!;
-                const countRight = edge?.nodeCountRight()!;
-                let left = [],
-                    right = [];
-                for (let j = 0; j < countLeft; ++j) {
-                    node = analyzed.graphEdgeNodes(reader++, node)!;
-                    left.push(node.columnReferenceId());
-                }
-                for (let j = 0; j < countRight; ++j) {
-                    node = analyzed.graphEdgeNodes(reader++, node)!;
-                    right.push(node.columnReferenceId());
-                }
-                console.log({
-                    edgeId: i,
-                    left,
-                    right,
-                });
-            }
-        }
-    }
+        if (!backend || backend.type != RESULT_OK) return 'unknown';
+        return backend.value.getVersionText();
+    }, [backend]);
 
     return (
         <div className={styles.page}>
-            <div className={styles.modelgraph_container}>
-                <ReactFlow
-                    nodes={[]}
-                    edges={[]}
-                    zoomOnScroll={false}
-                    zoomOnPinch={false}
-                    zoomOnDoubleClick={false}
-                    proOptions={{ hideAttribution: true }}
-                >
-                    <FlowBackground color="#aaa" variant={BackgroundVariant.Dots} gap={16} />
-                </ReactFlow>
-            </div>
+            <SchemaGraph className={styles.schemagraph_container} />
             <div className={styles.header_left_container}>
                 <img className={styles.header_logo} src={logo} />
                 <div className={styles.header_version}>Version: {version}</div>
