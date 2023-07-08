3147ee9acd9ba661f31cc55471a92e7b5f3e12e7
André Kohn <kohn.a@outlook.com>
2023-07-08 11:27:15 +0200

    Draft SchemaGraph api

--- packages/flatsql/include/flatsql/vis/adjacency_map.h
+++ packages/flatsql/include/flatsql/vis/adjacency_map.h
@@ -5,9 +5,8 @@
 
 namespace flatsql {
 
-struct Graph {
-    /// The vertex count
-    size_t vertex_count;
+/// Adjecency list as compressed sparse rows
+struct AdjacencyMap {
     /// The adjacency nodes
     std::vector<size_t> adjacency_nodes;
     /// The adjacency offsets
@@ -20,8 +19,6 @@ struct Graph {
         return std::span<const size_t>{adjacency_nodes}.subspan(
             adjacency_offsets[vertex_id], adjacency_offsets[vertex_id + 1] - adjacency_offsets[vertex_id]);
     }
-    /// Get the vertex count
-    inline size_t GetVertexCount() const { return vertex_count; }
     /// Get the edge count
     inline size_t GetEdgeCount() const { return adjacency_offsets.size() - 1; }
 };
