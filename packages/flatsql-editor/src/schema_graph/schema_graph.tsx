fffcb7178af0bc29017c722e223622a0eafbf978
André Kohn <kohn.a@outlook.com>
2023-07-24 08:44:54 +0200

    Layer schema graph rendering

--- packages/flatsql-editor/src/schema_graph/schema_graph.tsx
+++ packages/flatsql-editor/src/schema_graph/schema_graph.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import { TableNode, layoutSchema } from './schema_graph_node';
+import { NodeLayer } from './node_layer';
+import { EdgeLayer } from './edge_layer';
+import { BackgroundLayer } from './background_layer';
+import { layoutSchemaGraph } from './schema_graph_layout';
 import { RESIZE_SCHEMA_GRAPH, useAppStateDispatch, useAppState } from '../app_state_reducer';
 import cn from 'classnames';
 
@@ -10,23 +13,6 @@ interface Props {
     width: number;
     height: number;
 }
-
-const Background = () => (
-    <svg data-testid="rf__background" className={styles.graph_background}>
-        <pattern
-            id="pattern-1undefined"
-            x="0"
-            y="0"
-            width="16"
-            height="16"
-            patternUnits="userSpaceOnUse"
-            patternTransform="translate(-0.5,-0.5)"
-        >
-            <circle cx="0.5" cy="0.5" r="0.5" fill="#aaa"></circle>
-        </pattern>
-        <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-1undefined)"></rect>
-    </svg>
-);
 
 export const SchemaGraph: React.FC<Props> = (props: Props) => {
     const state = useAppState();
@@ -42,20 +28,29 @@ export const SchemaGraph: React.FC<Props> = (props: Props) => {
     // Render placeholder if context is not available
     if (!state) {
         <div className={props.className}>
-            <Background />
+            <BackgroundLayer />
         </div>;
     }
 
-    const [nodes, edges] = layoutSchema(state);
+    const [nodes, edges] = layoutSchemaGraph(state);
     // console.log([nodes, edges]); XXX
     return (
         <div className={cn(styles.graph_container, props.className)}>
-            <Background />
-            <div className={styles.graph_nodes}>
-                {nodes.map(n => (
-                    <TableNode key={n.tableId} {...n} />
-                ))}
-            </div>
+            <BackgroundLayer className={styles.graph_background} />
+            <EdgeLayer
+                className={styles.graph_edges}
+                width={props.width}
+                height={props.height}
+                nodes={nodes}
+                edges={edges}
+            />
+            <NodeLayer
+                className={styles.graph_nodes}
+                width={props.width}
+                height={props.height}
+                nodes={nodes}
+                edges={edges}
+            />
         </div>
     );
 };
