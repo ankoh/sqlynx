c0abf7f354e6f7cda7e2f49720ff615732b23d7f
André Kohn <kohn.a@outlook.com>
2023-07-24 21:08:28 +0200

    Two-phase force-directed layout to disentangle nodes

--- packages/flatsql-editor/src/schema_graph/edge_layer.tsx
+++ packages/flatsql-editor/src/schema_graph/edge_layer.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 
+import { NodeLayout } from './schema_graph_layout';
+import { EdgeLayout } from './schema_graph_layout';
+
 interface Props {
     className?: string;
     width: number;
     height: number;
-    nodes: any[];
-    edges: any[];
+    nodes: NodeLayout[];
+    edges: EdgeLayout[];
 }
 
 export function EdgeLayer(props: Props) {
     console.log(props.edges);
-    return <svg className={props.className} viewBox={'0 0 ' + props.width + ' ' + props.height}></svg>;
+
+    return (
+        <svg className={props.className} viewBox={'0 0 ' + props.width + ' ' + props.height}>
+            {props.edges.map((e, i) => {
+                let path = `M ${e.path[0]} ${e.path[1]} L ${e.path[2]} ${e.path[3]}`;
+                return <path key={i} d={path} stroke="black" />;
+            })}
+        </svg>
+    );
 }
