fb6f7dcffa280407750daae46cf712c119dd33b2
André Kohn <kohn.a@outlook.com>
2023-07-24 08:44:54 +0200

    Layer schema graph rendering

---
+++ packages/flatsql-editor/src/schema_graph/edge_layer.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface Props {
+    className?: string;
+    width: number;
+    height: number;
+    nodes: any[];
+    edges: any[];
+}
+
+export function EdgeLayer(props: Props) {
+    return <svg className={props.className} viewBox={'0 0 ' + props.width + ' ' + props.height}></svg>;
+}
---
+++ packages/flatsql-editor/src/schema_graph/node_layer.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+
+import { NodeLayout } from './schema_graph_layout';
+
+import iconTable from '../../static/svg/icons/table.svg';
+import iconTableView from '../../static/svg/icons/table_border.svg';
+
+import styles from './node_layer.module.css';
+
+function TableNode(props: NodeLayout) {
+    return (
+        <div
+            className={styles.table_node}
+            style={{
+                position: 'absolute',
+                top: props.y,
+                left: props.x,
+                width: props.width,
+                height: props.height,
+            }}
+        >
+            <div className={styles.table_icon}>
+                <svg width="20px" height="20px">
+                    <use xlinkHref={`${iconTable}#sym`} />
+                </svg>
+            </div>
+            <div className={styles.table_name}>{props.name}</div>
+            <div className={styles.table_column_count}>{props.columns.length}</div>
+        </div>
+    );
+}
+
+interface NodeLayerProps {
+    className?: string;
+    width: number;
+    height: number;
+    nodes: any[];
+    edges: any[];
+}
+
+export function NodeLayer(props: NodeLayerProps) {
+    return (
+        <div className={styles.graph_nodes}>
+            {props.nodes.map(n => (
+                <TableNode key={n.tableId} {...n} />
+            ))}
+        </div>
+    );
+}
