7d4e74d0cebae420ba56c6054560ab8e85d23a7b
Andre Kohn <kohn.a@outlook.com>
2023-07-29 16:49:12 +0200

    Refactor graph edge construction

--- packages/flatsql-editor/src/schema_graph/schema_graph.tsx
+++ packages/flatsql-editor/src/schema_graph/schema_graph.tsx
@@ -3,7 +3,8 @@ import { NodeLayer } from './node_layer';
 import { EdgeLayer } from './edge_layer';
 import { DebugLayer } from './debug_layer';
 import { BackgroundLayer } from './background_layer';
-import { layoutDebugInfo, layoutSchemaGraph } from './graph_layout';
+import { buildSchemaGraphLayout } from './graph_layout';
+import { buildDebugInfo } from './debug_layer';
 import { RESIZE_SCHEMA_GRAPH, useAppStateDispatch, useAppState } from '../app_state_reducer';
 import cn from 'classnames';
 
@@ -33,8 +34,8 @@ export const SchemaGraph: React.FC<Props> = (props: Props) => {
         </div>;
     }
 
-    const [nodes, edges] = layoutSchemaGraph(state);
-    const debugInfo = layoutDebugInfo(state, nodes);
+    const schemaGraphLayout = buildSchemaGraphLayout(state);
+    const debugInfo = buildDebugInfo(state, schemaGraphLayout.nodes);
     return (
         <div className={cn(styles.graph_container, props.className)}>
             <BackgroundLayer className={styles.graph_background} />
@@ -42,19 +43,14 @@ export const SchemaGraph: React.FC<Props> = (props: Props) => {
                 className={styles.graph_edges}
                 boardWidth={props.width}
                 boardHeight={props.height}
-                nodes={nodes}
-                edges={edges}
-                nodeHeight={state.graphConfig.tableHeight}
-                nodeWidth={state.graphConfig.tableWidth}
-                gridSize={state.graphConfig.gridSize}
-                cornerRadius={8}
+                edges={schemaGraphLayout.edges}
             />
             <NodeLayer
                 className={styles.graph_nodes}
                 width={props.width}
                 height={props.height}
-                nodes={nodes}
-                edges={edges}
+                nodes={schemaGraphLayout.nodes}
+                edges={schemaGraphLayout.edges}
             />
             {state.graphDebugMode && (
                 <DebugLayer className={styles.graph_edges} width={props.width} height={props.height} info={debugInfo} />
