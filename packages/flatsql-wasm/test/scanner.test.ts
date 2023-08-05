3924c1cde7a8cbcd77fc459b7661608e7e12f1b2
André Kohn <kohn.a@outlook.com>
2023-08-05 17:02:42 +0200

    Use new token encoding in js scanner tests

--- packages/flatsql-wasm/test/scanner.test.ts
+++ packages/flatsql-wasm/test/scanner.test.ts
@@ -28,6 +28,7 @@ describe('FlatSQL Scanner', () => {
         const add = (
             t: string,
             expectedOffsets: number[],
+            expectedLengths: number[],
             expectedTypes: flatsql.proto.ScannerTokenType[],
             expectedBreaks: number[],
         ) => {
@@ -38,20 +39,22 @@ describe('FlatSQL Scanner', () => {
             expect(scanned.tokens()).toBeTruthy();
             let hl = scanned.tokens()!;
             expect(hl.tokenOffsetsArray()).toBeTruthy();
+            expect(hl.tokenLengthsArray()).toBeTruthy();
             expect(hl.tokenTypesArray()).toBeTruthy();
             expect(Array.from(hl.tokenOffsetsArray()!)).toEqual(expectedOffsets);
+            expect(Array.from(hl.tokenLengthsArray()!)).toEqual(expectedLengths);
             expect(Array.from(hl.tokenTypesArray()!)).toEqual(expectedTypes);
             expect(Array.from(hl.tokenBreaksArray() ?? new Uint32Array())).toEqual(expectedBreaks);
         };
 
-        add('s', [0, 1], [Token.IDENTIFIER, Token.NONE], []);
-        add('e', [0, 2], [Token.IDENTIFIER, Token.NONE], []);
-        add('l', [0, 3], [Token.IDENTIFIER, Token.NONE], []);
-        add('e', [0, 4], [Token.IDENTIFIER, Token.NONE], []);
-        add('c', [0, 5], [Token.IDENTIFIER, Token.NONE], []);
-        add('t', [0, 6], [Token.KEYWORD, Token.NONE], []);
-        add('\n', [0, 6], [Token.KEYWORD, Token.NONE], [1]);
-        add('1', [0, 6, 7, 8], [Token.KEYWORD, Token.NONE, Token.LITERAL_INTEGER, Token.NONE], [1]);
+        add('s', [0], [1], [Token.IDENTIFIER], []);
+        add('e', [0], [2], [Token.IDENTIFIER], []);
+        add('l', [0], [3], [Token.IDENTIFIER], []);
+        add('e', [0], [4], [Token.IDENTIFIER], []);
+        add('c', [0], [5], [Token.IDENTIFIER], []);
+        add('t', [0], [6], [Token.KEYWORD], []);
+        add('\n', [0], [6], [Token.KEYWORD], [1]);
+        add('1', [0, 7], [6, 1], [Token.KEYWORD, Token.LITERAL_INTEGER], [1]);
 
         script.delete();
     });
@@ -61,6 +64,7 @@ describe('FlatSQL Scanner', () => {
             const test_tokens = (
                 text: string,
                 expectedOffsets: number[],
+                expectedLengths: number[],
                 expectedTypes: flatsql.proto.ScannerTokenType[],
                 textRange: [number, number],
                 expectedFiltered: [number, number],
@@ -74,8 +78,10 @@ describe('FlatSQL Scanner', () => {
                 const hl = scannedScript.tokens();
                 expect(hl).toBeTruthy();
                 expect(hl!.tokenOffsetsArray()).toBeTruthy();
+                expect(hl!.tokenLengthsArray()).toBeTruthy();
                 expect(hl!.tokenTypesArray()).toBeTruthy();
                 expect(Array.from(hl!.tokenOffsetsArray()!)).toEqual(expectedOffsets);
+                expect(Array.from(hl!.tokenLengthsArray()!)).toEqual(expectedLengths);
                 expect(Array.from(hl!.tokenTypesArray()!)).toEqual(expectedTypes);
 
                 const [textBegin, textEnd] = textRange;
@@ -86,45 +92,15 @@ describe('FlatSQL Scanner', () => {
             };
 
             // Full text
-            test_tokens(
-                'select 1',
-                [0, 6, 7, 8],
-                [Token.KEYWORD, Token.NONE, Token.LITERAL_INTEGER, Token.NONE],
-                [0, 8],
-                [0, 3],
-            );
+            test_tokens('select 1', [0, 7], [6, 1], [Token.KEYWORD, Token.LITERAL_INTEGER], [0, 8], [0, 2]);
             // Expand left
-            test_tokens(
-                'select 1',
-                [0, 6, 7, 8],
-                [Token.KEYWORD, Token.NONE, Token.LITERAL_INTEGER, Token.NONE],
-                [3, 8],
-                [0, 3],
-            );
+            test_tokens('select 1', [0, 7], [6, 1], [Token.KEYWORD, Token.LITERAL_INTEGER], [3, 8], [0, 2]);
             // Not begin, not end
-            test_tokens(
-                'select 111111',
-                [0, 6, 7, 13],
-                [Token.KEYWORD, Token.NONE, Token.LITERAL_INTEGER, Token.NONE],
-                [7, 9],
-                [2, 3],
-            );
+            test_tokens('select 111111', [0, 7], [6, 6], [Token.KEYWORD, Token.LITERAL_INTEGER], [7, 9], [1, 2]);
             // Begin, expand right
-            test_tokens(
-                'select 111111',
-                [0, 6, 7, 13],
-                [Token.KEYWORD, Token.NONE, Token.LITERAL_INTEGER, Token.NONE],
-                [0, 9],
-                [0, 3],
-            );
+            test_tokens('select 111111', [0, 7], [6, 6], [Token.KEYWORD, Token.LITERAL_INTEGER], [0, 9], [0, 2]);
             // Begin, don't expand right
-            test_tokens(
-                'select 111111',
-                [0, 6, 7, 13],
-                [Token.KEYWORD, Token.NONE, Token.LITERAL_INTEGER, Token.NONE],
-                [0, 3],
-                [0, 1],
-            );
+            test_tokens('select 111111', [0, 7], [6, 6], [Token.KEYWORD, Token.LITERAL_INTEGER], [0, 3], [0, 1]);
         });
     });
 });
