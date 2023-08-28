02079c9c2d34cba2adbb89e6a92a4f14d1e9b444
André Kohn <kohn.a@outlook.com>
2023-08-28 22:24:40 +0200

    Add static sorted keywords

--- packages/flatsql/src/parser/grammar/keywords.cc
+++ packages/flatsql/src/parser/grammar/keywords.cc
@@ -24,6 +24,20 @@ constexpr frozen::unordered_map<frozen::string, Keyword, KEYWORD_COUNT> KEYWORD_
 #undef X
 };
 
+const constexpr std::array<Keyword, KEYWORD_COUNT> SortKeywords() {
+    std::array<Keyword, KEYWORD_COUNT> keywords{
+#define X(CATEGORY, NAME, TOKEN) Keyword{NAME, Parser::token::FQL_##TOKEN, KeywordCategory::CATEGORY},
+#include "../../../grammar/lists/sql_column_name_keywords.list"
+#include "../../../grammar/lists/sql_reserved_keywords.list"
+#include "../../../grammar/lists/sql_type_func_keywords.list"
+#include "../../../grammar/lists/sql_unreserved_keywords.list"
+#undef X
+    };
+    std::sort(keywords.begin(), keywords.end(), [](auto& l, auto& r) { return l.name < r.name; });
+    return keywords;
+};
+static const std::array<Keyword, KEYWORD_COUNT> SORTED_KEYWORDS = SortKeywords();
+
 constexpr size_t MAX_KEYWORD_LENGTH = std::max<size_t>({
 #define X(CATEGORY, NAME, TOKEN) Keyword::ConstLength(NAME),
 #include "../../../grammar/lists/sql_column_name_keywords.list"
@@ -32,6 +46,9 @@ constexpr size_t MAX_KEYWORD_LENGTH = std::max<size_t>({
 #include "../../../grammar/lists/sql_unreserved_keywords.list"
 #undef X
 });
+
+/// Get sorted keywords
+std::span<const Keyword> Keyword::GetKeywords() { return {SORTED_KEYWORDS.begin(), SORTED_KEYWORDS.size()}; }
 
 /// Find a keyword
 const Keyword* Keyword::Find(std::string_view text) {
