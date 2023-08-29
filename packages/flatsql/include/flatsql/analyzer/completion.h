6f6b4b8c5521eecab6b84efa6ac41ec4435c5350
André Kohn <kohn.a@outlook.com>
2023-08-30 00:14:05 +0200

    Work towards completion

--- packages/flatsql/include/flatsql/analyzer/completion.h
+++ packages/flatsql/include/flatsql/analyzer/completion.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include "flatsql/analyzer/analyzer.h"
-#include "flatsql/parser/grammar/keywords.h"
-#include "flatsql/script.h"
 #include "flatsql/utils/suffix_trie.h"
 
 namespace flatsql {
+
+struct AnalyzedScript;
 
 struct CompletionIndex {
     /// The suffix trie
@@ -16,8 +15,6 @@ struct CompletionIndex {
     /// Constructor
     CompletionIndex(std::unique_ptr<SuffixTrie> trie, std::shared_ptr<AnalyzedScript> script = nullptr);
 
-    /// Construct completion index from keywords
-    static std::unique_ptr<CompletionIndex> Build(std::span<const parser::Keyword> keywords);
     /// Construct completion index from script
     static std::unique_ptr<CompletionIndex> Build(std::shared_ptr<AnalyzedScript> script);
 };
