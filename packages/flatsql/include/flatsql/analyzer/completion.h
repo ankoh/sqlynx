8e01e17482ced760d56f4e1b8b0c5bb2c2628858
André Kohn <kohn.a@outlook.com>
2023-08-29 23:29:11 +0200

    Construct completion index from script or keywords

--- packages/flatsql/include/flatsql/analyzer/completion.h
+++ packages/flatsql/include/flatsql/analyzer/completion.h
@@ -2,6 +2,7 @@
 
 #include "flatsql/analyzer/analyzer.h"
 #include "flatsql/parser/grammar/keywords.h"
+#include "flatsql/script.h"
 #include "flatsql/utils/suffix_trie.h"
 
 namespace flatsql {
@@ -11,6 +12,14 @@ struct CompletionIndex {
     std::unique_ptr<SuffixTrie> suffix_trie = nullptr;
     /// The analyzed script
     std::shared_ptr<AnalyzedScript> analyzed_script = nullptr;
+
+    /// Constructor
+    CompletionIndex(std::unique_ptr<SuffixTrie> trie, std::shared_ptr<AnalyzedScript> script = nullptr);
+
+    /// Construct completion index from keywords
+    static std::unique_ptr<CompletionIndex> Build(std::span<const parser::Keyword> keywords);
+    /// Construct completion index from script
+    static std::unique_ptr<CompletionIndex> Build(std::shared_ptr<AnalyzedScript> script);
 };
 
 }  // namespace flatsql
