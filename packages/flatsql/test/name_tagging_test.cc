5e6e9ebc6cc299e16041ae6c85985ae0e8f5da7c
André Kohn <kohn.a@outlook.com>
2023-09-03 22:38:46 +0200

    Refine name tagging tests

--- packages/flatsql/test/name_tagging_test.cc
+++ packages/flatsql/test/name_tagging_test.cc
@@ -13,18 +13,6 @@ using namespace flatsql;
 
 namespace {
 
-void analyze(std::shared_ptr<AnalyzedScript>& out, rope::Rope& buffer, uint32_t context_id, std::string_view text) {
-    buffer.Remove(0, buffer.GetStats().utf8_codepoints);
-    buffer.Insert(0, text);
-    auto [scanned, scan_status] = parser::Scanner::Scan(buffer, context_id);
-    ASSERT_EQ(scan_status, proto::StatusCode::OK);
-    auto [parsed, parser_status] = parser::ParseContext::Parse(scanned);
-    ASSERT_EQ(parser_status, proto::StatusCode::OK);
-    auto [analyzed, analyzer_status] = Analyzer::Analyze(parsed, nullptr);
-    ASSERT_EQ(analyzer_status, proto::StatusCode::OK);
-    out = analyzed;
-}
-
 std::string dump(std::span<const ScannedScript::Name> names) {
     std::stringstream out;
     size_t i = 0;
@@ -39,27 +27,55 @@ std::string dump(std::span<const ScannedScript::Name> names) {
     return out.str();
 }
 
-void test_name_tags(const AnalyzedScript& analyzed,
-                    std::initializer_list<std::pair<std::string_view, NameTags>> expected) {
-    auto& scanned = *analyzed.parsed_script->scanned_script;
-    ASSERT_EQ(scanned.name_dictionary.size(), expected.size()) << dump(scanned.name_dictionary);
+struct NameTaggingTest {
+    std::string_view title;
+    std::string_view script;
+    std::vector<std::pair<std::string_view, NameTags>> expected;
+
+    NameTaggingTest(std::string_view title, std::string_view script,
+                    std::initializer_list<std::pair<std::string_view, NameTags>> expected)
+        : title(title), script(script), expected(expected) {}
+};
+
+struct NameTaggingTestPrinter {
+    std::string operator()(const ::testing::TestParamInfo<NameTaggingTest>& info) const {
+        return std::string{info.param.title};
+    }
+};
+
+struct TestNameTags : public ::testing::TestWithParam<NameTaggingTest> {};
+
+TEST_P(TestNameTags, Test) {
+    rope::Rope buffer{128};
+    buffer.Insert(0, GetParam().script);
+
+    auto [scanned, scan_status] = parser::Scanner::Scan(buffer, 0);
+    ASSERT_EQ(scan_status, proto::StatusCode::OK);
+    auto [parsed, parser_status] = parser::ParseContext::Parse(scanned);
+    ASSERT_EQ(parser_status, proto::StatusCode::OK);
+    auto [analyzed, analyzer_status] = Analyzer::Analyze(parsed, nullptr);
+    ASSERT_EQ(analyzer_status, proto::StatusCode::OK);
+
+    ASSERT_EQ(scanned->name_dictionary.size(), GetParam().expected.size()) << dump(scanned->name_dictionary);
     size_t i = 0;
-    for (auto [name, tags] : expected) {
-        auto& have = scanned.name_dictionary[i++];
+    for (auto [name, tags] : GetParam().expected) {
+        auto& have = scanned->name_dictionary[i++];
         ASSERT_EQ(have.text, name);
         ASSERT_EQ(static_cast<uint64_t>(have.tags), static_cast<uint64_t>(tags));
     }
 }
 
-TEST(NameTaggingTest, Select1) {
-    std::shared_ptr<AnalyzedScript> analyzed;
-    rope::Rope buffer{128};
+std::vector<NameTaggingTest> TESTS_SIMPLE{
+    {"select_1", "select 1", {}},
+    {"select_foo", "select foo", {{"foo", NameTags() | proto::NameTag::COLUMN_NAME}}},
+    {"select_foo_from_bar",
+     "select foo from bar",
+     {
+         {"foo", NameTags() | proto::NameTag::COLUMN_NAME},
+         {"bar", NameTags() | proto::NameTag::TABLE_NAME},
+     }},
+};
 
-    ASSERT_NO_FATAL_FAILURE(analyze(analyzed, buffer, 0, "select 1"));
-    ASSERT_NO_FATAL_FAILURE(test_name_tags(*analyzed, {}));
-
-    ASSERT_NO_FATAL_FAILURE(analyze(analyzed, buffer, 0, "select foo"));
-    ASSERT_NO_FATAL_FAILURE(test_name_tags(*analyzed, {{"foo", proto::NameTag::COLUMN_NAME}}));
-}
+INSTANTIATE_TEST_SUITE_P(SimpleNameTagging, TestNameTags, ::testing::ValuesIn(TESTS_SIMPLE), NameTaggingTestPrinter());
 
 }  // namespace
