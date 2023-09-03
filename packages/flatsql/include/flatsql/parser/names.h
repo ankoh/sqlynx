e7e8bea127762039211bff1cd20465b3f8e47ea4
André Kohn <kohn.a@outlook.com>
2023-09-03 22:17:51 +0200

    Test first name tags

--- packages/flatsql/include/flatsql/parser/names.h
+++ packages/flatsql/include/flatsql/parser/names.h
@@ -6,30 +6,35 @@
 
 namespace flatsql {
 
-struct NameTagBitmap {
+struct NameTags {
     using ValueType = uint8_t;
 
     /// The value
     ValueType value;
     /// Constructor
-    NameTagBitmap(ValueType value = 0) : value(value) {}
+    NameTags(ValueType value = 0) : value(value) {}
     /// Constructor
-    NameTagBitmap(proto::NameTag value) : value(static_cast<ValueType>(value)) {}
+    NameTags(proto::NameTag value) : value(static_cast<ValueType>(value)) {}
     /// Get the value
     operator ValueType() const { return value; }
     /// Tag a name
-    NameTagBitmap& operator|=(proto::NameTag tag) {
+    NameTags& operator|=(proto::NameTag tag) {
         value |= static_cast<ValueType>(tag);
         return *this;
     }
+    /// Tag a name
+    NameTags operator|(proto::NameTag tag) {
+        ValueType v = value | static_cast<ValueType>(tag);
+        return NameTags{v};
+    }
     /// Untag a name
-    NameTagBitmap& operator^=(proto::NameTag tag) {
+    NameTags& operator^=(proto::NameTag tag) {
         value &= ~static_cast<ValueType>(tag);
         return *this;
     }
 };
-static_assert(std::is_trivially_copyable<NameTagBitmap::ValueType>::value);
-static_assert(sizeof(NameTagBitmap::ValueType) == sizeof(NameTagBitmap));
-static_assert(sizeof(NameTagBitmap::ValueType) == sizeof(proto::NameTag));
+static_assert(std::is_trivially_copyable<NameTags::ValueType>::value);
+static_assert(sizeof(NameTags::ValueType) == sizeof(NameTags));
+static_assert(sizeof(NameTags::ValueType) == sizeof(proto::NameTag));
 
 }  // namespace flatsql
