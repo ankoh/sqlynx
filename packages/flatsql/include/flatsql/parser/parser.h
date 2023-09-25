f110cb023c89ed32a324d10d255771ba5c3b0f84
André Kohn <kohn.a@outlook.com>
2023-09-25 22:40:34 +0200

    Test collecting expected symbols

--- packages/flatsql/include/flatsql/parser/parser.h
+++ packages/flatsql/include/flatsql/parser/parser.h
@@ -12,10 +12,13 @@ namespace parser {
 class Parser : public ParserBase {
     using ParserBase::ParserBase;
 
+    /// Collect all expected symbols
+    std::vector<symbol_kind_type> CollectExpectedSymbols();
+
    public:
     /// Run the parser rules and get the list of expected tokens at a position.
     /// Returns the expected tokens for the NEXT token and substitutes.
-    int CompleteAt(size_t token);
+    std::vector<symbol_kind_type> CompleteAt(size_t token);
 
     /// Parse a module
     static std::pair<std::shared_ptr<ParsedScript>, proto::StatusCode> Parse(std::shared_ptr<ScannedScript> in,
