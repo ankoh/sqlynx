bb9990d3290ef68e69ea3f80a8c242cdfa9d1632
André Kohn <kohn.a@outlook.com>
2023-10-03 23:17:46 +0200

    Setup example autocomplete extension

--- packages/flatsql-editor/src/editor/flatsql_extension.ts
+++ packages/flatsql-editor/src/editor/flatsql_extension.ts
@@ -1,6 +1,15 @@
 import { lineNumbers } from '@codemirror/view';
+import { autocompletion } from '@codemirror/autocomplete';
 
+import { completeFlatSQL } from './flatsql_completion';
 import { FlatSQLDecorations } from './flatsql_decorations';
 import { FlatSQLProcessor } from './flatsql_processor';
 
-export const FlatSQLExtensions = [lineNumbers(), FlatSQLProcessor, ...FlatSQLDecorations];
+export const FlatSQLExtensions = [
+    lineNumbers(),
+    FlatSQLProcessor,
+    ...FlatSQLDecorations,
+    autocompletion({
+        override: [completeFlatSQL],
+    }),
+];
