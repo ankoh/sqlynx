a391015d51cfb83da2299205a515c194fe3bb914
André Kohn <kohn.a@outlook.com>
2023-10-03 23:34:17 +0200

    Return first completion results from wasm module

--- packages/flatsql-editor/src/editor/flatsql_completion.ts
+++ packages/flatsql-editor/src/editor/flatsql_completion.ts
@@ -1,22 +1,39 @@
-import { CompletionContext, CompletionResult } from '@codemirror/autocomplete';
+import * as flatsql from '@ankoh/flatsql';
+
+import { CompletionContext, CompletionResult, Completion } from '@codemirror/autocomplete';
+import { FlatSQLProcessor } from './flatsql_processor';
 
 function updateCompletions(
-    current: CompletionResult,
-    from: number,
-    to: number,
-    context: CompletionContext,
+    _current: CompletionResult,
+    _from: number,
+    _to: number,
+    _context: CompletionContext,
 ): CompletionResult | null {
     return null;
 }
 
+/// Derived from this example:
+/// https://codemirror.net/examples/autocompletion/
 export async function completeFlatSQL(context: CompletionContext): Promise<CompletionResult> {
+    const processor = context.state.field(FlatSQLProcessor);
+    const options: Completion[] = [];
+
+    if (processor.mainScript !== null) {
+        const completionBuffer = processor.mainScript.completeAtCursor(10);
+        const completion = completionBuffer.read(new flatsql.proto.Completion());
+        const candidateObj = new flatsql.proto.CompletionCandidate();
+        for (let i = 0; i < completion.candidatesLength(); ++i) {
+            const candidate = completion.candidates(i, candidateObj)!;
+            options.push({
+                label: candidate.nameText() ?? '',
+                type: 'keyword',
+            });
+        }
+    }
+
     return {
         from: context.pos,
-        options: [
-            { label: 'match', type: 'keyword' },
-            { label: 'hello', type: 'variable', info: '(World)' },
-            { label: 'magic', type: 'text', apply: '⠁⭒*.✩.*⭒⠁', detail: 'macro' },
-        ],
+        options,
         filter: false,
         update: updateCompletions,
     };
