f1b6cef0dd969e0e6d20e04cc620b4fef61bb30e
André Kohn <kohn.a@outlook.com>
2023-11-24 23:46:12 +0000

    Connections page scaffold

--- packages/sqlynx-app/src/view/pages/connection_manager_page.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-interface Props {}
-
-export const ConnectionManagerPage: React.FC<Props> = (props: Props) => {
-    return <div></div>;
-};
---
+++ packages/sqlynx-app/src/view/pages/connections_page.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import symbols from '../../../static/svg/symbols.generated.svg';
+import styles from './connections_page.module.css';
+
+interface Props {}
+
+export const ConnectionsPage: React.FC<Props> = (props: Props) => {
+    return (
+        <div className={styles.page}>
+            <div className={styles.header_container}>
+                <div className={styles.header_left_container}>
+                    <div className={styles.page_title}>Connections</div>
+                </div>
+            </div>
+            <div className={styles.body_container}>
+                <div className={styles.card_container}>
+                    <svg width="32px" height="32px">
+                        <use xlinkHref={`${symbols}#salesforce-notext`} />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    );
+};
