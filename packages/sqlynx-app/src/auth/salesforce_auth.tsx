ddece37b8ae6f616b5b3b67383a14f7091f97c8f
André Kohn <kohn.a@outlook.com>
2023-11-25 21:30:12 +0000

    Use salesforce oauth provider

---
+++ packages/sqlynx-app/src/auth/salesforce_auth.tsx
@@ -0,0 +1,229 @@
+// Copyright (c) 2020 The DashQL Authors
+
+import React from 'react';
+import './oauth_callback.html';
+import * as utils from '../utils';
+
+// Docs: https://help.salesforce.com/s/articleView?id=sf.remoteaccess_oauth_web_server_flow.htm&type=5
+
+const OAUTH_POPUP_NAME = 'SQLynx OAuth';
+const OAUTH_POPUP_SETTINGS = 'toolbar=no, menubar=no, width=600, height=700, top=100, left=100';
+
+type Props = {
+    /// The children
+    children: React.ReactElement;
+};
+
+type AccessToken = {
+    /// The token
+    token: string | null;
+    /// The token type
+    tokenType: string | null;
+    /// The scope
+    scope: string | null;
+};
+
+type State = {
+    /// The popup window URL
+    pendingAuth: string | null;
+    /// The expected oauth state
+    expectedAuthSig: string | null;
+    /// The popup window
+    openAuthWindow: Window | null;
+    /// The authentication error
+    authError: string | null;
+    /// The code
+    authCode: string | null;
+    /// The github access token
+    accessToken: AccessToken | null;
+};
+
+type AuthParams = {
+    /// The oauth redirect
+    oauthRedirect: URL;
+    /// The base URL
+    instanceUrl: URL;
+    /// The client id
+    clientId: string;
+    /// The client secret
+    clientSecret: string;
+};
+
+export interface SalesforceAccountAuth {
+    /// Login into an account
+    login: (config: AuthParams) => void;
+    /// Logout of an account
+    logout: () => void;
+}
+
+export interface SalesforceAPIClient {
+    /// Is authenticated?
+    isAuthenticated: boolean;
+}
+
+const accountAuthCtx = React.createContext<SalesforceAccountAuth | null>(null);
+const apiClientCtx = React.createContext<SalesforceAPIClient | null>(null);
+
+export const SalesforceAuthProvider: React.FC<Props> = (props: Props) => {
+    const [state, setState] = React.useState<State>({
+        pendingAuth: null,
+        expectedAuthSig: null,
+        openAuthWindow: null,
+        authCode: null,
+        authError: null,
+        accessToken: null,
+    });
+
+    // Maintain mount flag
+    const isMountedRef = React.useRef<boolean>(true);
+    React.useEffect(() => {
+        isMountedRef.current = true;
+        return () => void (isMountedRef.current = false);
+    }, []);
+
+    // Register a receive for the oauth code from the window
+    React.useEffect(() => {
+        const handler = (event: any) => {
+            const params = new URLSearchParams(event?.data);
+            const code = params.get('code');
+            if (!code) return;
+            if (!isMountedRef.current) return;
+            setState(s => {
+                console.log(`oauth code=${code}`);
+                return {
+                    ...s,
+                    authCode: code,
+                };
+            });
+        };
+        window.addEventListener('message', handler);
+        return () => {
+            window.removeEventListener('message', handler);
+        };
+    }, [setState]);
+
+    // Effect to forget about the auth window when it closes
+    React.useEffect(() => {
+        if (!state.openAuthWindow) return () => {};
+        const loop = setInterval(function () {
+            if (state.openAuthWindow?.closed) {
+                clearInterval(loop);
+                setState(s => {
+                    if (!s.openAuthWindow) return s;
+                    return {
+                        ...s,
+                        pendingAuth: null,
+                        expectedAuthSig: null,
+                        openAuthWindow: null,
+                    };
+                });
+            }
+        }, 1000);
+        return () => {
+            clearInterval(loop);
+        };
+    }, [state.openAuthWindow]);
+
+    // Effect to open the auth window when there is a pending auth
+    React.useEffect(() => {
+        // Already done?
+        if (!state.pendingAuth || state.openAuthWindow) return;
+        // Open popup window
+        const popup = window.open(state.pendingAuth, OAUTH_POPUP_NAME, OAUTH_POPUP_SETTINGS);
+        if (!popup) {
+            // Something went wrong, Browser might prevent the popup.
+            // (E.g. FF blocks by default)
+            setState(s => ({
+                ...s,
+                pendingAuth: null,
+                expectedAuthSig: null,
+                openAuthWindow: null,
+                error: 'could not open oauth window',
+            }));
+            return;
+        }
+        popup.focus();
+        setState(s => ({
+            ...s,
+            openAuthWindow: popup,
+        }));
+    }, [state.pendingAuth, state.openAuthWindow]);
+
+    // Login function initiated the OAuth login
+    const login = React.useCallback(
+        (params: AuthParams) => {
+            if (!isMountedRef.current) return;
+
+            // Construct the URI
+            const authSig = utils.generateRandomString(20);
+            const p = [
+                `client_id=${params.clientId}`,
+                `redirect_uri=${params.oauthRedirect}`,
+                `code_challenge=${authSig}`,
+                `response_type=code`,
+            ].join('&');
+            const url = `${params.instanceUrl}/services/oauth2/authorize?${p}`;
+
+            // Set the pending popup URL
+            setState(s => {
+                if (s.pendingAuth || s.openAuthWindow) return s;
+                return {
+                    pendingAuth: url,
+                    expectedAuthSig: authSig,
+                    openAuthWindow: null,
+                    authError: null,
+                    authCode: null,
+                    accessToken: null,
+                };
+            });
+        },
+        [setState],
+    );
+
+    // Logout function that clears any pending login
+    const logout = React.useCallback(() => {
+        if (!isMountedRef.current) return;
+        setState(s => {
+            if (s.openAuthWindow) s.openAuthWindow.close();
+            return {
+                pendingAuth: null,
+                expectedAuthSig: null,
+                openAuthWindow: null,
+                authError: null,
+                authCode: null,
+                accessToken: null,
+            };
+        });
+    }, [setState]);
+
+    // Build the account login methods
+    const auth = React.useMemo(
+        (): SalesforceAccountAuth => ({
+            login,
+            logout,
+        }),
+        [login, logout],
+    );
+
+    // Build the graphql client
+    const apiClient = React.useMemo<SalesforceAPIClient>(() => {
+        if (state.accessToken) {
+            return {
+                isAuthenticated: true,
+            };
+        } else {
+            return {
+                isAuthenticated: false,
+            };
+        }
+    }, [state.accessToken]);
+
+    return (
+        <accountAuthCtx.Provider value={auth}>
+            <apiClientCtx.Provider value={apiClient}>{props.children}</apiClientCtx.Provider>
+        </accountAuthCtx.Provider>
+    );
+};
+
+export const useSalesforceAuth = (): SalesforceAccountAuth => React.useContext(accountAuthCtx)!;
+export const useSalesforceAPI = (): SalesforceAPIClient => React.useContext(apiClientCtx)!;
