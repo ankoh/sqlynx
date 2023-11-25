bb9ec49daf5395b8e6181e5c560dff1bb2e8c830
André Kohn <kohn.a@outlook.com>
2023-11-25 10:00:10 +0000

    Data cloud connection card

--- packages/sqlynx-app/src/view/pages/connections_page.tsx
+++ packages/sqlynx-app/src/view/pages/connections_page.tsx
@@ -3,6 +3,28 @@ import * as React from 'react';
 import symbols from '../../../static/svg/symbols.generated.svg';
 import styles from './connections_page.module.css';
 
+interface SalesforceConnectionCardProps {}
+
+export const SalesforceConnectionCard: React.FC<SalesforceConnectionCardProps> = (
+    props: SalesforceConnectionCardProps,
+) => {
+    return (
+        <div className={styles.card_container}>
+            <div className={styles.card_header_container}>
+                <div className={styles.platform_logo}>
+                    <svg width="32px" height="32px">
+                        <use xlinkHref={`${symbols}#salesforce-notext`} />
+                    </svg>
+                </div>
+                <div className={styles.platform_name}>Salesforce Data Cloud</div>
+            </div>
+            <div className={styles.card_body_container}>
+                <button>Test</button>
+            </div>
+        </div>
+    );
+};
+
 interface Props {}
 
 export const ConnectionsPage: React.FC<Props> = (props: Props) => {
@@ -14,19 +36,7 @@ export const ConnectionsPage: React.FC<Props> = (props: Props) => {
                 </div>
             </div>
             <div className={styles.body_container}>
-                <div className={styles.card_container}>
-                    <div className={styles.card_header_container}>
-                        <div className={styles.platform_logo}>
-                            <svg width="32px" height="32px">
-                                <use xlinkHref={`${symbols}#salesforce-notext`} />
-                            </svg>
-                        </div>
-                        <div className={styles.platform_name}>Salesforce Data Cloud</div>
-                    </div>
-                    <div className={styles.card_body_container}>
-                        <button>Test</button>
-                    </div>
-                </div>
+                <SalesforceConnectionCard />
             </div>
         </div>
     );
