66bee0247be9fd9dced72fd53183fe245985bbb7
André Kohn <kohn.a@outlook.com>
2023-11-25 10:28:38 +0000

    Log oauth query params

--- packages/sqlynx-app/src/view/pages/oauth_callback_page.tsx
+++ packages/sqlynx-app/src/view/pages/oauth_callback_page.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 
+import { useSearchParams } from 'react-router-dom';
+
 import styles from './oauth_callback_page.module.css';
 
 interface Props {}
 
 export const OAuthCallbackPage: React.FC<Props> = (props: Props) => {
+    const [searchParams, _setSearchParams] = useSearchParams();
+    const flatParams = new Map();
+    for (const [key, value] of searchParams.entries()) {
+        flatParams.set(key, value);
+    }
+    console.log(flatParams);
     return <div className={styles.container}>Oauth Callback Page</div>;
 };
