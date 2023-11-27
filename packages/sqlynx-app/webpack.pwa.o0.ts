24914d3ea7b85ea19fe31c790dc710c11669a9f2
André Kohn <kohn.a@outlook.com>
2023-11-27 10:57:01 +0000

    OAuth tests with orgfarm org

--- packages/sqlynx-app/webpack.pwa.o0.ts
+++ packages/sqlynx-app/webpack.pwa.o0.ts
@@ -40,11 +40,6 @@ const config: Configuration = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
             'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-            // This will enable SharedArrayBuffers in Firefox but will block most requests to third-party sites.
-            //
-            // "Cross-Origin-Resource-Policy": "cross-origin",
-            // "Cross-Origin-Embedder-Policy": "require-corp",
-            // "Cross-Origin-Opener-Policy": "same-origin"
         },
     },
 };
