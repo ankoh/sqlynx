b157dc9a2ec94d70b1fb201907a071fb5e3b60a5
André Kohn <kohn.a@outlook.com>
2023-12-11 10:16:28 +0100

    Only store tables and table columns in schema

--- packages/sqlynx/include/sqlynx/script.h
+++ packages/sqlynx/include/sqlynx/script.h
@@ -174,10 +174,124 @@ class ParsedScript {
 
 class AnalyzedScript : public Schema {
    public:
+    /// A table reference
+    struct TableReference {
+        /// The AST node id in the target script
+        std::optional<uint32_t> ast_node_id;
+        /// The AST statement id in the target script
+        std::optional<uint32_t> ast_statement_id;
+        /// The AST scope root in the target script
+        std::optional<uint32_t> ast_scope_root;
+        /// The table name, may refer to different context
+        QualifiedTableName table_name;
+        /// The alias name, may refer to different context
+        QualifiedID alias_name;
+        /// The table id, may refer to different context
+        QualifiedID table_id;
+        /// Constructor
+        TableReference(std::optional<uint32_t> ast_node_id = std::nullopt,
+                       std::optional<uint32_t> ast_statement_id = {}, std::optional<uint32_t> ast_scope_root = {},
+                       QualifiedTableName table_name = {}, QualifiedID alias_name = {}, QualifiedID table_id = {})
+            : ast_node_id(ast_node_id),
+              ast_statement_id(ast_statement_id),
+              ast_scope_root(ast_scope_root),
+              table_name(table_name),
+              alias_name(alias_name),
+              table_id(table_id) {}
+        /// Create FlatBuffer
+        operator proto::TableReference() {
+            return proto::TableReference{ast_node_id.value_or(PROTO_NULL_U32),
+                                         ast_statement_id.value_or(PROTO_NULL_U32),
+                                         ast_scope_root.value_or(PROTO_NULL_U32),
+                                         table_name,
+                                         alias_name.Pack(),
+                                         table_id.Pack()};
+        }
+    };
+    /// A column reference
+    struct ColumnReference {
+        /// The AST node id in the target script
+        std::optional<uint32_t> ast_node_id;
+        /// The AST statement id in the target script
+        std::optional<uint32_t> ast_statement_id;
+        /// The AST scope root in the target script
+        std::optional<uint32_t> ast_scope_root;
+        /// The column name, may refer to different context
+        QualifiedColumnName column_name;
+        /// The table id, may refer to different context
+        QualifiedID table_id;
+        /// The column index
+        std::optional<uint32_t> column_id;
+        /// Constructor
+        ColumnReference(std::optional<uint32_t> ast_node_id = std::nullopt,
+                        std::optional<uint32_t> ast_statement_id = {}, std::optional<uint32_t> ast_scope_root = {},
+                        QualifiedColumnName column_name = {}, QualifiedID table_id = {},
+                        std::optional<uint32_t> column_id = std::nullopt)
+            : ast_node_id(ast_node_id),
+              ast_statement_id(ast_statement_id),
+              ast_scope_root(ast_scope_root),
+              column_name(column_name),
+              table_id(table_id),
+              column_id(column_id) {}
+        /// Create FlatBuffer
+        operator proto::ColumnReference() {
+            return proto::ColumnReference{ast_node_id.value_or(PROTO_NULL_U32),
+                                          ast_statement_id.value_or(PROTO_NULL_U32),
+                                          ast_scope_root.value_or(PROTO_NULL_U32),
+                                          column_name,
+                                          table_id.Pack(),
+                                          column_id.value_or(PROTO_NULL_U32)};
+        }
+    };
+    /// A query graph edge
+    struct QueryGraphEdge {
+        /// The AST node id in the target script
+        std::optional<uint32_t> ast_node_id;
+        /// The begin of the nodes
+        uint32_t nodes_begin;
+        /// The number of nodes on the left
+        uint16_t node_count_left;
+        /// The number of nodes on the right
+        uint16_t node_count_right;
+        /// The expression operator
+        proto::ExpressionOperator expression_operator;
+        /// Constructor
+        QueryGraphEdge(std::optional<uint32_t> ast_node_id = std::nullopt, uint32_t nodes_begin = 0,
+                       uint16_t node_count_left = 0, uint16_t node_count_right = 0,
+                       proto::ExpressionOperator op = proto::ExpressionOperator::DEFAULT)
+            : ast_node_id(ast_node_id),
+              nodes_begin(nodes_begin),
+              node_count_left(node_count_left),
+              node_count_right(node_count_right),
+              expression_operator(op) {}
+        /// Create FlatBuffer
+        operator proto::QueryGraphEdge() {
+            return proto::QueryGraphEdge{ast_node_id.value_or(PROTO_NULL_U32), nodes_begin, node_count_left,
+                                         node_count_right, expression_operator};
+        }
+    };
+    /// A query graph edge node
+    struct QueryGraphEdgeNode {
+        /// The column reference id
+        uint32_t column_reference_id;
+        /// Constructor
+        QueryGraphEdgeNode(uint32_t column_ref_id = 0) : column_reference_id(column_ref_id) {}
+        /// Create FlatBuffer
+        operator proto::QueryGraphEdgeNode() { return proto::QueryGraphEdgeNode{column_reference_id}; }
+    };
+
     /// The parsed script
     std::shared_ptr<ParsedScript> parsed_script;
     /// The external script
     std::shared_ptr<AnalyzedScript> external_script;
+    /// The table references
+    std::vector<TableReference> table_references;
+    /// The column references
+    std::vector<ColumnReference> column_references;
+    /// The join edges
+    std::vector<QueryGraphEdge> graph_edges;
+    /// The join edge nodes
+    std::vector<QueryGraphEdgeNode> graph_edge_nodes;
 
    public:
     /// Constructor
