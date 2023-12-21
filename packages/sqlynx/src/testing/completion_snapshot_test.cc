538a091097c41ef035ef5993c324ec0241967e58
André Kohn <kohn.a@outlook.com>
2023-12-21 19:56:03 +0100

    Rename completion test flag

--- packages/sqlynx/src/testing/completion_snapshot_test.cc
+++ packages/sqlynx/src/testing/completion_snapshot_test.cc
@@ -46,7 +46,7 @@ void CompletionSnapshotTest::EncodeCompletion(pugi::xml_node root, const Complet
         std::string text{iter->name.text.data(), iter->name.text.size()};
         xml_entry.append_attribute("value").set_value(text.c_str());
         xml_entry.append_attribute("score").set_value(iter->GetScore());
-        xml_entry.append_attribute("in-scope").set_value(iter->near_cursor);
+        xml_entry.append_attribute("near").set_value(iter->near_cursor);
         std::stringstream tags;
         size_t i = 0;
         iter->combined_tags.ForEach([&](proto::NameTag tag) {
