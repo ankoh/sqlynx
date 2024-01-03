2fd2c98365316846cb912d5afc5ecd33d91e4a3d
André Kohn <kohn.a@outlook.com>
2024-01-03 23:22:30 +0100

    Store the data cloud metadata in the catalog

--- packages/sqlynx-app/src/connectors/salesforce_catalog_loader.tsx
+++ packages/sqlynx-app/src/connectors/salesforce_catalog_loader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { SQLynxCatalog } from '@ankoh/sqlynx';
+import * as sqlynx from '@ankoh/sqlynx';
+
 import { SalesforceConnectorInterface, SalesforceDataCloudAccessToken } from './salesforce_api_client';
 import { useSalesforceAuthState } from './salesforce_auth_state';
 import { useSalesforceConnector } from './salesforce_connector';
@@ -8,18 +9,34 @@ import { useCatalogLoader } from './catalog_loader';
 
 export const UPDATE_SALESFORCE_DATA_CLOUD_METADATA = Symbol('UPDATE_SALESFORCE_DATA_CLOUD_METADATA');
 
+const METADATA_DESCRIPTOR_POOL_ID = 100;
+const METADATA_DESCRIPTOR_POOL_RANK = 1e9;
+
 export interface UpdateDataCloudMetadataTask {
     api: SalesforceConnectorInterface;
     accessToken: SalesforceDataCloudAccessToken;
 }
 
 export async function updateDataCloudMetadata(
-    catalog: SQLynxCatalog,
+    catalog: sqlynx.SQLynxCatalog,
     task: UpdateDataCloudMetadataTask,
     cancellation: AbortController,
 ): Promise<void> {
+    // Get the data cloud metadata
     const result = await task.api.getDataCloudMetadata(task.accessToken, cancellation.signal);
+    // Build the descriptor
+    const schema = new sqlynx.proto.SchemaDescriptorT();
+    for (const entity of result.metadata) {
+        const table = new sqlynx.proto.SchemaTableT(entity.name);
+        for (const column of entity.fields) {
+            table.columns.push(new sqlynx.proto.SchemaTableColumnT(column.name));
+        }
+        schema.tables.push(table);
+    }
+    // Store the metadata in the descriptor pool
     catalog.clear();
+    catalog.addDescriptorPool(METADATA_DESCRIPTOR_POOL_ID, METADATA_DESCRIPTOR_POOL_RANK);
+    catalog.addSchemaDescriptorT(METADATA_DESCRIPTOR_POOL_ID, schema);
 }
 
 interface Props {
