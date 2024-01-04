faf771186a3a6b0070c2353eb2e7a7dd90396293
André Kohn <kohn.a@outlook.com>
2024-01-05 00:23:46 +0100

    Use anchored overlay for connector selection

--- packages/sqlynx-app/src/view/pages/editor_page.tsx
+++ packages/sqlynx-app/src/view/pages/editor_page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { ActionList, IconButton, ButtonGroup } from '@primer/react';
+import { ActionList, IconButton, ButtonGroup, SelectPanel, AnchoredOverlay, Box, Button } from '@primer/react';
 import { TriangleDownIcon, SyncIcon, PaperAirplaneIcon, LinkIcon, DownloadIcon } from '@primer/octicons-react';
 
 import { ConnectorInfo, useActiveConnector } from '../../connectors/connector_switch';
@@ -29,76 +29,115 @@ const SalesforceIcon = () => (
         <use xlinkHref={`${icons}#salesforce-notext`} />
     </svg>
 );
-const ActionsPanel = (props: { icon: React.ReactElement; name: string; connectorInfo: ConnectorInfo }) => (
-    <div className={styles.action_container}>
-        <ActionList className={styles.data_actions}>
-            <ActionList.Item>
-                <ActionList.LeadingVisual>{props.icon}</ActionList.LeadingVisual>
-                {props.name}
-                <ActionList.TrailingVisual>
-                    <TriangleDownIcon />
-                </ActionList.TrailingVisual>
-            </ActionList.Item>
-            <ActionList.Divider />
-            <ActionList.Item disabled={!props.connectorInfo.features.executeQueryAction}>
-                <ActionList.LeadingVisual>
-                    <PaperAirplaneIcon />
-                </ActionList.LeadingVisual>
-                Execute Query
-                <ActionList.TrailingVisual>Ctrl + E</ActionList.TrailingVisual>
-            </ActionList.Item>
-            <ActionList.Item disabled={!props.connectorInfo.features.refreshSchemaAction}>
-                <ActionList.LeadingVisual>
-                    <SyncIcon />
-                </ActionList.LeadingVisual>
-                Refresh Schema
-                <ActionList.TrailingVisual>Ctrl + R</ActionList.TrailingVisual>
-            </ActionList.Item>
-            <ActionList.Divider />
-            <ActionList.Item>
-                <ActionList.LeadingVisual>
-                    <LinkIcon />
-                </ActionList.LeadingVisual>
-                Save Query as Link
-                <ActionList.TrailingVisual>Ctrl + L</ActionList.TrailingVisual>
-            </ActionList.Item>
-            <ActionList.Item>
-                <ActionList.LeadingVisual>
-                    <DownloadIcon />
-                </ActionList.LeadingVisual>
-                Save Query as .sql
-                <ActionList.TrailingVisual>Ctrl + S</ActionList.TrailingVisual>
-            </ActionList.Item>
-            <ActionList.Item disabled={!props.connectorInfo.features.executeQueryAction}>
-                <ActionList.LeadingVisual>
-                    <DownloadIcon />
-                </ActionList.LeadingVisual>
-                Save Results as .arrow
-                <ActionList.TrailingVisual>Ctrl + A</ActionList.TrailingVisual>
-            </ActionList.Item>
-        </ActionList>
-        <ActionList className={styles.project_actions}>
-            <ActionList.Item>
-                <ActionList.LeadingVisual>
-                    <GitHubIcon />
-                </ActionList.LeadingVisual>
-                Open-source project
-            </ActionList.Item>
-            <ActionList.Item>
-                <ActionList.LeadingVisual>
-                    <GitHubIcon />
-                </ActionList.LeadingVisual>
-                Report a bug
-            </ActionList.Item>
-            <ActionList.Item>
-                <ActionList.LeadingVisual>
-                    <GitHubIcon />
-                </ActionList.LeadingVisual>
-                View discussions
-            </ActionList.Item>
-        </ActionList>
-    </div>
-);
+
+const ActionsPanel = (props: {
+    icon: React.ReactElement;
+    name: string;
+    connectorInfo: ConnectorInfo;
+    isOpen: boolean;
+    setIsOpen: (open: boolean) => void;
+}) => {
+    const anchorRef = React.useRef(null);
+    return (
+        <div className={styles.action_container}>
+            <ActionList className={styles.data_actions}>
+                <ActionList.Item
+                    onClick={() => {
+                        console.log('foo');
+                        props.setIsOpen(true);
+                    }}
+                >
+                    <ActionList.LeadingVisual>{props.icon}</ActionList.LeadingVisual>
+                    {props.name}
+                    <ActionList.TrailingVisual>
+                        <AnchoredOverlay
+                            renderAnchor={anchorProps => (
+                                <div ref={anchorRef}>
+                                    <TriangleDownIcon />
+                                </div>
+                            )}
+                            open={props.isOpen}
+                            onClose={() => props.setIsOpen(false)}
+                            anchorRef={anchorRef}
+                            anchorOffset={8}
+                            align="end"
+                        >
+                            <Box>
+                                <ActionList>
+                                    <ActionList.Item>
+                                        <ActionList.LeadingVisual>{props.icon}</ActionList.LeadingVisual>
+                                        Offline
+                                    </ActionList.Item>
+                                    <ActionList.Item>
+                                        <ActionList.LeadingVisual>{props.icon}</ActionList.LeadingVisual>
+                                        Salesforce Data Cloud
+                                    </ActionList.Item>
+                                </ActionList>
+                            </Box>
+                        </AnchoredOverlay>
+                    </ActionList.TrailingVisual>
+                </ActionList.Item>
+                <ActionList.Divider />
+                <ActionList.Item disabled={!props.connectorInfo.features.executeQueryAction}>
+                    <ActionList.LeadingVisual>
+                        <PaperAirplaneIcon />
+                    </ActionList.LeadingVisual>
+                    Execute Query
+                    <ActionList.TrailingVisual>Ctrl + E</ActionList.TrailingVisual>
+                </ActionList.Item>
+                <ActionList.Item disabled={!props.connectorInfo.features.refreshSchemaAction}>
+                    <ActionList.LeadingVisual>
+                        <SyncIcon />
+                    </ActionList.LeadingVisual>
+                    Refresh Schema
+                    <ActionList.TrailingVisual>Ctrl + R</ActionList.TrailingVisual>
+                </ActionList.Item>
+                <ActionList.Divider />
+                <ActionList.Item>
+                    <ActionList.LeadingVisual>
+                        <LinkIcon />
+                    </ActionList.LeadingVisual>
+                    Save Query as Link
+                    <ActionList.TrailingVisual>Ctrl + L</ActionList.TrailingVisual>
+                </ActionList.Item>
+                <ActionList.Item>
+                    <ActionList.LeadingVisual>
+                        <DownloadIcon />
+                    </ActionList.LeadingVisual>
+                    Save Query as .sql
+                    <ActionList.TrailingVisual>Ctrl + S</ActionList.TrailingVisual>
+                </ActionList.Item>
+                <ActionList.Item disabled={!props.connectorInfo.features.executeQueryAction}>
+                    <ActionList.LeadingVisual>
+                        <DownloadIcon />
+                    </ActionList.LeadingVisual>
+                    Save Results as .arrow
+                    <ActionList.TrailingVisual>Ctrl + A</ActionList.TrailingVisual>
+                </ActionList.Item>
+            </ActionList>
+            <ActionList className={styles.project_actions}>
+                <ActionList.Item>
+                    <ActionList.LeadingVisual>
+                        <GitHubIcon />
+                    </ActionList.LeadingVisual>
+                    Open-source project
+                </ActionList.Item>
+                <ActionList.Item>
+                    <ActionList.LeadingVisual>
+                        <GitHubIcon />
+                    </ActionList.LeadingVisual>
+                    Report a bug
+                </ActionList.Item>
+                <ActionList.Item>
+                    <ActionList.LeadingVisual>
+                        <GitHubIcon />
+                    </ActionList.LeadingVisual>
+                    View discussions
+                </ActionList.Item>
+            </ActionList>
+        </div>
+    );
+};
 
 const connections = [
     { icon: SalesforceIcon, name: 'No Database' },
@@ -111,6 +150,8 @@ export const EditorPage: React.FC<Props> = (props: Props) => {
     const [selectedConnIdx, selectConn] = React.useState(1);
     const selectedConn = connections[selectedConnIdx];
     const activeConnector = useActiveConnector();
+
+    const [isOpen, setIsOpen] = React.useState<boolean>(false);
 
     return (
         <div className={styles.page}>
@@ -143,7 +184,13 @@ export const EditorPage: React.FC<Props> = (props: Props) => {
                 />
                 <ScriptEditor className={styles.editor_card} />
                 {appConfig.value?.features?.editorActions && (
-                    <ActionsPanel icon={selectedConn.icon()} name={selectedConn.name} connectorInfo={activeConnector} />
+                    <ActionsPanel
+                        icon={selectedConn.icon()}
+                        name={selectedConn.name}
+                        connectorInfo={activeConnector}
+                        isOpen={isOpen}
+                        setIsOpen={setIsOpen}
+                    />
                 )}
             </div>
         </div>
