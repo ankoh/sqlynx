af9d91d13ac5d7cc89d5fe7543840e303bb7649f
André Kohn <kohn.a@outlook.com>
2024-01-06 22:00:19 +0100

    Display active connector in editor

---
+++ packages/sqlynx-app/src/view/connector_icons.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import { ConnectorInfo, ConnectorType } from '../connectors/connector_switch';
+
+import icons from '../../static/svg/symbols.generated.svg';
+
+const SalesforceIcon = () => (
+    <svg width="20px" height="20px">
+        <use xlinkHref={`${icons}#salesforce-notext`} />
+    </svg>
+);
+const CloudOfflineIcon = () => (
+    <svg width="20px" height="20px">
+        <use xlinkHref={`${icons}#cloud_offline`} />
+    </svg>
+);
+const HyperIcon = () => (
+    <svg width="20px" height="20px">
+        <use xlinkHref={`${icons}#hyper`} />
+    </svg>
+);
+
+export function getConnectorIcon(connector: ConnectorInfo): React.ReactElement {
+    switch (connector.connectorType) {
+        case ConnectorType.LOCAL_SCRIPT:
+            return <CloudOfflineIcon />;
+        case ConnectorType.SALESFORCE_DATA_CLOUD_CONNECTOR:
+            return <SalesforceIcon />;
+        case ConnectorType.HYPER_DATABASE:
+            return <HyperIcon />;
+    }
+}
