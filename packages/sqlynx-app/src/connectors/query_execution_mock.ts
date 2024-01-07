7c12c5449ed15089d9ede9fc31f4d51b46efb445
André Kohn <kohn.a@outlook.com>
2024-01-07 20:31:55 +0100

    Sketch first query executor mock

--- packages/sqlynx-app/src/connectors/query_execution_mock.ts
+++ packages/sqlynx-app/src/connectors/query_execution_mock.ts
@@ -1,17 +1,33 @@
 import * as arrow from 'apache-arrow';
 import { QueryExecutionProgress, QueryExecutionResponseStream } from './query_execution';
+import { sleep } from '../utils/sleep';
 
 export class QueryExecutorMock implements QueryExecutionResponseStream {
+    schema: arrow.Schema;
+
+    constructor() {
+        this.schema = new arrow.Schema([
+            new arrow.Field('A', new arrow.Int32()),
+            new arrow.Field('B', new arrow.Int32()),
+            new arrow.Field('C', new arrow.Int32()),
+            new arrow.Field('D', new arrow.Utf8()),
+            new arrow.Field('F', new arrow.Utf8()),
+        ]);
+    }
+
     /// Get the arrow schema
     async getSchema(): Promise<arrow.Schema | null> {
-        return null;
+        await sleep(200);
+        return this.schema;
     }
     /// Await the next progress update
     async nextProgressUpdate(): Promise<QueryExecutionProgress | null> {
-        return null;
+        await sleep(100);
+        return {};
     }
     /// Await the next record batch
     async nextRecordBatch(): Promise<arrow.RecordBatch | null> {
-        return null;
+        await sleep(400);
+        return new arrow.RecordBatch(this.schema);
     }
 }
