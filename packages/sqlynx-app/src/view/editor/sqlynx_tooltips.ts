13231f52810bc8eb254460e80e19e4c531a9eb8b
André Kohn <kohn.a@outlook.com>
2024-01-11 12:11:13 -0800

    Buid error tooltip

--- packages/sqlynx-app/src/view/editor/sqlynx_tooltips.ts
+++ packages/sqlynx-app/src/view/editor/sqlynx_tooltips.ts
@@ -1,20 +1,90 @@
+import * as sqlynx from '@ankoh/sqlynx';
+
 import { Tooltip, showTooltip } from '@codemirror/view';
-import { Transaction, EditorState } from '@codemirror/state';
+import { Transaction } from '@codemirror/state';
 import { StateField } from '@codemirror/state';
 
-function getCursorTooltips(state: EditorState) {
-    return [];
+import { SQLynxProcessor } from './sqlynx_processor';
+
+function updateCursorTooltip(state: CursorTooltipState, transaction: Transaction): CursorTooltipState {
+    const processor = transaction.state.field(SQLynxProcessor);
+    if ((transaction.selection?.ranges.length ?? 0) == 0) {
+        return state;
+    }
+    let nextTooltips = [];
+
+    const tmpError = new sqlynx.proto.Error();
+    const findErrorAtLocation = (
+        buffer: {
+            errors: (index: number, obj?: sqlynx.proto.Error) => sqlynx.proto.Error | null;
+            errorsLength: () => number;
+        },
+        cursor: number,
+    ) => {
+        for (let i = 0; i < buffer.errorsLength(); ++i) {
+            const error = buffer.errors(i, tmpError)!;
+            const loc = error.location()!;
+            if (loc.offset() <= cursor && loc.offset() + loc.length() > cursor) {
+                return error;
+            }
+            return null;
+        }
+    };
+
+    const cursorPos = transaction.selection!.ranges[0].head;
+    if (processor.scriptBuffers.scanned) {
+        const scanned = processor.scriptBuffers.scanned.read(new sqlynx.proto.ScannedScript());
+        const error = findErrorAtLocation(scanned, cursorPos);
+        if (error != null) {
+            nextTooltips.push({
+                pos: cursorPos,
+                arrow: true,
+                create: () => {
+                    let dom = document.createElement('div');
+                    dom.className = 'cm-tooltip-cursor';
+                    dom.textContent = error.message();
+                    return { dom };
+                },
+            });
+        }
+    }
+    if (processor.scriptBuffers.parsed) {
+        const parsed = processor.scriptBuffers.parsed.read(new sqlynx.proto.ParsedScript());
+        const error = findErrorAtLocation(parsed, cursorPos);
+        if (error != null) {
+            nextTooltips.push({
+                pos: cursorPos,
+                arrow: true,
+                create: () => {
+                    let dom = document.createElement('div');
+                    dom.className = 'cm-tooltip-cursor';
+                    dom.textContent = error.message();
+                    return { dom };
+                },
+            });
+        }
+    }
+    return {
+        tooltips: nextTooltips,
+    };
 }
 
-type StateType = readonly Tooltip[];
+interface CursorTooltipState {
+    tooltips: Tooltip[];
+}
 
-const CursorTooltipField = StateField.define<StateType>({
-    create: getCursorTooltips,
-    update: (state: StateType, transaction: Transaction) => {
-        if (!transaction.docChanged && !transaction.selection) return state;
-        return getCursorTooltips(transaction.state);
+const CursorTooltipField = StateField.define<CursorTooltipState>({
+    create: () => {
+        const config: CursorTooltipState = {
+            tooltips: [],
+        };
+        return config;
     },
-    provide: f => showTooltip.computeN([f], state => state.field(f)),
+    update: (state: CursorTooltipState, transaction: Transaction) => {
+        if (!transaction.docChanged && !transaction.selection) return state;
+        return updateCursorTooltip(state, transaction);
+    },
+    provide: f => showTooltip.computeN([f], state => state.field(f).tooltips),
 });
 
 export const SQLynxTooltips = [CursorTooltipField];
