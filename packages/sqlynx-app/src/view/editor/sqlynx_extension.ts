4695ddef9ee9dd20a87e81197175ca6c7bbb8fb1
André Kohn <kohn.a@outlook.com>
2024-01-11 10:27:03 -0800

    Consult configured codemirror theme for scanner token decorations

--- packages/sqlynx-app/src/view/editor/sqlynx_extension.ts
+++ packages/sqlynx-app/src/view/editor/sqlynx_extension.ts
@@ -5,7 +5,12 @@ import { completeSQLynx } from './sqlynx_completion';
 import { SQLynxDecorations } from './sqlynx_decorations';
 import { SQLynxProcessor } from './sqlynx_processor';
 
+import { basicLight } from 'cm6-theme-basic-light';
+
+const THEME = basicLight;
+
 export const SQLynxExtensions = [
+    THEME,
     lineNumbers(),
     SQLynxProcessor,
     ...SQLynxDecorations,
