4134e03e3e5221a6dabd7d2b8cb40fd198c83585
André Kohn <kohn.a@outlook.com>
2024-01-11 16:41:38 -0800

    Add first error gutters

--- packages/sqlynx-app/src/view/editor/sqlynx_extension.ts
+++ packages/sqlynx-app/src/view/editor/sqlynx_extension.ts
@@ -4,6 +4,7 @@ import { autocompletion } from '@codemirror/autocomplete';
 import { SQLynxDecorations } from './sqlynx_decorations';
 import { SQLynxProcessor } from './sqlynx_processor';
 import { SQLynxTooltips } from './sqlynx_tooltips';
+import { SQLynxGutters } from './sqlynx_gutters';
 import { completeSQLynx } from './sqlynx_completion';
 
 import { xcodeLight as THEME } from '@uiw/codemirror-themes-all';
@@ -14,6 +15,7 @@ export const SQLynxExtensions = [
     SQLynxProcessor,
     SQLynxDecorations,
     SQLynxTooltips,
+    SQLynxGutters,
     autocompletion({
         override: [completeSQLynx],
     }),
