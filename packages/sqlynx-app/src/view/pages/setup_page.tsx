51793620bb646f99baacecb30d97168957275d52
André Kohn <kohn.a@outlook.com>
2024-01-13 10:48:06 -0800

    Refine setup page

--- packages/sqlynx-app/src/view/pages/setup_page.tsx
+++ packages/sqlynx-app/src/view/pages/setup_page.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Button, ButtonGroup, IconButton } from '@primer/react';
 import { TriangleDownIcon } from '@primer/octicons-react';
+import { CONNECTOR_INFOS } from '../../connectors/connector_info';
 
 import styles from './setup_page.module.css';
 import primerBugFixes from '../../primer_bugfixes.module.css';
@@ -11,26 +12,30 @@ import classNames from 'classnames';
 interface Props {}
 
 export const SetupPage: React.FC<Props> = (props: Props) => {
-    const TEST_URL = new URL('https://sqlynx.app?main-script=foo&schema-script=bar&connector-type=sfdc');
+    const TEST_URL = new URL('https://sqlynx.app?script=foo&schema=bar&connector=sfdc');
     const searchParams = new URLSearchParams(TEST_URL.search);
-    const mainScript = searchParams.get('main-script') ?? null;
-    const schemaScript = searchParams.get('schema-script') ?? null;
-    const connectorType = searchParams.get('connector-type') ?? null;
-    // const connectorParams = searchParams.get('connector-params') ?? null;
+    const mainScript = searchParams.get('script') ?? null;
+    const schemaScript = searchParams.get('schema') ?? null;
+    const connectorType = searchParams.get('connector') ?? null;
 
-    const UrlDetailEntry = (props: { label: string; value: string | null }) => (
+    let connectorId: number = connectorType == null ? 0 : Number.parseInt(connectorType);
+    connectorId = connectorId < CONNECTOR_INFOS.length ? connectorId : 0;
+    const connectorInfo = CONNECTOR_INFOS[connectorId];
+
+    const DetailEntry = (props: { label: string; children: React.ReactElement }) => (
         <>
-            <div className={styles.url_detail_entry_key}>{props.label}</div>
-            <div className={styles.url_detail_entry_value}>{props.value}</div>
+            <div className={styles.url_detail_key}>{props.label}</div>
+            <div className={styles.url_detail_value}>{props.children}</div>
         </>
     );
+    const Bean = (props: { text: string }) => <div className={styles.bean}>{props.text}</div>;
 
     return (
         <div className={styles.page}>
             <div className={styles.center_container}>
                 <div className={styles.banner_container}>
                     <div className={styles.banner_logo}>
-                        <svg width="80px" height="80px">
+                        <svg width="64px" height="64px">
                             <use xlinkHref={`${symbols}#sqlynx`} />
                         </svg>
                     </div>
@@ -38,25 +43,46 @@ export const SetupPage: React.FC<Props> = (props: Props) => {
                 <div className={styles.card_container}>
                     <div className={styles.card_header}>
                         <div className={styles.link_icon}></div>
-                        <div className={styles.card_title}>SQLynx Setup</div>
+                        <div className={styles.card_title}>Setup</div>
                     </div>
-                    <div className={styles.url_info}>
-                        <div className={styles.url_box}>Some URL</div>
-                        <div className={styles.url_details}>
-                            <UrlDetailEntry label="Inline Script" value={mainScript ?? ''} />
-                            <UrlDetailEntry label="Inline Schema" value={schemaScript ?? ''} />
-                            <UrlDetailEntry label="Connector" value={connectorType ?? ''} />
+                    <div className={styles.wasm_setup}>
+                        <div className={styles.card_section_header}>WebAssembly</div>
+                        <div className={styles.wasm_details}>
+                            <DetailEntry label="Module Size">
+                                <Bean text="102 kB" />
+                            </DetailEntry>
+                            <DetailEntry label="Instantiation Time">
+                                <Bean text="~10 ms" />
+                            </DetailEntry>
+                            <DetailEntry label="Version">
+                                <Bean text="0.0.1-dev1204" />
+                            </DetailEntry>
                         </div>
                     </div>
-                    <div className={styles.context_content}>
-                        <div className={styles.context_content_tabs}>Tabs</div>
-                        <div className={styles.context_content_body}>Body</div>
+                    <div className={styles.url_setup}>
+                        <div className={styles.card_section_header}>URL Parameters</div>
+                        <div className={styles.url_details}>
+                            <DetailEntry label="Connector Type">
+                                <Bean text={connectorInfo.displayName.long} />
+                            </DetailEntry>
+                            <DetailEntry label="Inline Script">
+                                <Bean text="3 kB | 142 symbols" />
+                            </DetailEntry>
+                            <DetailEntry label="Inline Schema">
+                                <Bean text="10 kB | 1.5k symbols" />
+                            </DetailEntry>
+                        </div>
                     </div>
-                    <div className={styles.launch_section}>
-                        <ButtonGroup className={classNames(primerBugFixes.button_group, styles.launch_button)}>
-                            <Button variant="primary">Continue</Button>
-                            <IconButton variant="primary" icon={TriangleDownIcon} aria-labelledby="more-actions" />
-                        </ButtonGroup>
+                    <div className={styles.auth_setup}>
+                        <div className={styles.card_section_header}>Authentiation</div>
+                    </div>
+                    <div className={styles.card_actions}>
+                        <Button className={styles.skip_button} variant="danger">
+                            Skip
+                        </Button>
+                        <Button className={styles.continue_button} variant="primary">
+                            Continue
+                        </Button>
                     </div>
                 </div>
             </div>
