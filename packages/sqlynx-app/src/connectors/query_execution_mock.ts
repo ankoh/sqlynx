b2045ed26870b137f94c8f42765903ac2bdbd54d
André Kohn <kohn.a@outlook.com>
2024-01-21 00:27:45 +0100

    Return arrow batch in query executor mock

--- packages/sqlynx-app/src/connectors/query_execution_mock.ts
+++ packages/sqlynx-app/src/connectors/query_execution_mock.ts
@@ -4,15 +4,17 @@ import { sleep } from '../utils/sleep';
 
 export class QueryExecutorMock implements QueryExecutionResponseStream {
     schema: arrow.Schema;
+    batchCount: number;
+    batchesWritten: number;
 
     constructor() {
         this.schema = new arrow.Schema([
             new arrow.Field('A', new arrow.Int32()),
             new arrow.Field('B', new arrow.Int32()),
             new arrow.Field('C', new arrow.Int32()),
-            new arrow.Field('D', new arrow.Utf8()),
-            new arrow.Field('F', new arrow.Utf8()),
         ]);
+        this.batchCount = 1;
+        this.batchesWritten = 0;
     }
 
     /// Get the arrow schema
@@ -23,11 +25,29 @@ export class QueryExecutorMock implements QueryExecutionResponseStream {
     /// Await the next progress update
     async nextProgressUpdate(): Promise<QueryExecutionProgress | null> {
         await sleep(100);
-        return {};
+        return null;
     }
     /// Await the next record batch
     async nextRecordBatch(): Promise<arrow.RecordBatch | null> {
-        await sleep(400);
-        return new arrow.RecordBatch(this.schema);
+        if (this.batchesWritten >= this.batchCount) {
+            return null;
+        }
+        this.batchesWritten += 1;
+        await sleep(200);
+        const columnA = Int32Array.from({ length: 1000 }, () => Number((Math.random() * 1000).toFixed(0)));
+        const columnB = Int32Array.from({ length: 1000 }, () => Number((Math.random() * 1000).toFixed(0)));
+        const columnC = Int32Array.from({ length: 1000 }, () => Number((Math.random() * 1000).toFixed(0)));
+
+        const vecA = arrow.makeData({ type: new arrow.Int32(), data: columnA });
+        const vecB = arrow.makeData({ type: new arrow.Int32(), data: columnB });
+        const vecC = arrow.makeData({ type: new arrow.Int32(), data: columnC });
+
+        const data = arrow.makeData({
+            type: new arrow.Struct(this.schema.fields),
+            length,
+            children: [vecA, vecB, vecC],
+            nullCount: 0,
+        });
+        return new arrow.RecordBatch(this.schema, data);
     }
 }
