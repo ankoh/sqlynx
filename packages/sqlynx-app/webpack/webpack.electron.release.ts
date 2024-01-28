8a2ef402b0351e507a402ba59a59b8ef6fe80bc9
André Kohn <kohn.a@outlook.com>
2024-01-28 23:24:48 +0100

    Remove pwa config from electron release

--- packages/sqlynx-app/webpack/webpack.electron.release.ts
+++ packages/sqlynx-app/webpack/webpack.electron.release.ts
@@ -1,15 +1,8 @@
 import path from 'path';
 import fs from 'fs';
 
-import pwaConfig from './webpack.pwa.release';
 import { configure as configureMain } from './webpack.electron.common.main';
 import { configure as configurePreload } from './webpack.electron.common.preload';
-
-pwaConfig.output! = {
-    ...pwaConfig.output!,
-    path: path.resolve(__dirname, '../build/electron/release/app'),
-    publicPath: './',
-};
 
 const PWA_DIR_IN = path.resolve(__dirname, '../build/pwa/release');
 const PWA_DIR_OUT = path.resolve(__dirname, '../build/electron/release/app');
@@ -17,7 +10,6 @@ fs.rmSync(PWA_DIR_OUT, { recursive: true, force: true });
 fs.cpSync(PWA_DIR_IN, PWA_DIR_OUT, { recursive: true });
 
 export default [
-    pwaConfig,
     configureMain({
         buildDir: path.resolve(__dirname, '../build/electron/release'),
         mode: 'production',
