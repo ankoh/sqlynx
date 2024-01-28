2d92b665408420332bde56032e3fc010d0a4ebc7
André Kohn <kohn.a@outlook.com>
2024-01-28 23:36:15 +0100

    Make sure the electron output dir exists

--- packages/sqlynx-app/webpack/webpack.electron.release.ts
+++ packages/sqlynx-app/webpack/webpack.electron.release.ts
@@ -4,8 +4,10 @@ import fs from 'fs';
 import { configure as configureMain } from './webpack.electron.common.main';
 import { configure as configurePreload } from './webpack.electron.common.preload';
 
+const ELECTRON_DIR = path.resolve(__dirname, '../build/electron/release');
 const PWA_DIR_IN = path.resolve(__dirname, '../build/pwa/release');
 const PWA_DIR_OUT = path.resolve(__dirname, '../build/electron/release/app');
+fs.mkdirSync(ELECTRON_DIR, { recursive: true });
 fs.rmSync(PWA_DIR_OUT, { recursive: true, force: true });
 fs.cpSync(PWA_DIR_IN, PWA_DIR_OUT, { recursive: true });
 
