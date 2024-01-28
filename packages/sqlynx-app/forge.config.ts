204f8b70e0ce77aa86c2b041e282129771dd3f16
André Kohn <kohn.a@outlook.com>
2024-01-28 01:25:58 +0100

    Try passing destructive mode through extra args

--- packages/sqlynx-app/forge.config.ts
+++ packages/sqlynx-app/forge.config.ts
@@ -14,6 +14,7 @@ export default {
         new MakerSnap(
             {
                 base: 'core20',
+                extraArgs: ['--destructive-mode'],
             },
             ['linux'],
         ),
