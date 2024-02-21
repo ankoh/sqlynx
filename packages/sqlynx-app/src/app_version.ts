ed62eac24f1245bcf51530d4e58c9327ac8d1508
André Kohn <kohn.a@outlook.com>
2024-02-21 08:30:12 +0100

    Just show the user the canary metadata json for now

--- packages/sqlynx-app/src/app_version.ts
+++ packages/sqlynx-app/src/app_version.ts
@@ -3,5 +3,8 @@ export const SQLYNX_VERSION: string = process.env.SQLYNX_VERSION!;
 // Injected through the DefinePlugin in webpack.common.ts
 export const SQLYNX_GIT_COMMIT: string = process.env.SQLYNX_GIT_COMMIT!;
 
+// The URL of the release channel at get.sqlynx.app
+// XXX How to inject release channel into build? config.json?
+export const SQLYNX_GET_URL: string = `https://get.sqlynx.app/canary.json`;
 // The URL of the git repository
 export const SQLYNX_GIT_REPO_URL: string = `https://github.com/ankoh/sqlynx/tree/${SQLYNX_GIT_COMMIT}`;
