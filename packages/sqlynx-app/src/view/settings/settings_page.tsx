68b521283fc35a3e3ebd6ecbdcdf6b9bcfd0d964
André Kohn <kohn.a@outlook.com>
2024-02-22 22:43:42 +0100

    Rough layout for connector settings

--- packages/sqlynx-app/src/view/settings/settings_page.tsx
+++ packages/sqlynx-app/src/view/settings/settings_page.tsx
@@ -28,7 +28,7 @@ export const SettingsPage: React.FC<PageProps> = (_props: PageProps) => {
         <div className={styles.page}>
             <div className={styles.header_container}>
                 <div className={styles.header_left_container}>
-                    <div className={styles.page_title}>More</div>
+                    <div className={styles.page_title}>Settings</div>
                 </div>
             </div>
             <SettingsPageNav
