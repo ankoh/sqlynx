105e9711a1ce49e55e568e60bff180a910328d27
André Kohn <kohn.a@outlook.com>
2024-02-22 21:01:17 +0100

    Setup navigation for settings page

--- packages/sqlynx-app/src/view/settings/settings_page.tsx
+++ packages/sqlynx-app/src/view/settings/settings_page.tsx
@@ -1,14 +1,33 @@
 import * as React from 'react';
 
-import { classNames } from '../../utils/classnames.js';
-import { SalesforceConnectorPanel } from './salesforce_connector_panel.js';
-import { HyperGrpcConnectorPanel } from './hyper_grpc_connector_panel.js';
+import { SectionProps, SectionRenderers, SettingsPageNav } from './settings_page_nav.js';
 
 import styles from './settings_page.module.css';
+import { ConnectorSettings } from './connector_settings.js';
 
 interface PageProps { }
 
 export const SettingsPage: React.FC<PageProps> = (_props: PageProps) => {
+    const [selectedSection, selectSection] = React.useState(0);
+
+    const sections: SectionProps[] = React.useMemo(() => ([
+        {
+            id: 0,
+            label: "Connectors"
+        },
+        {
+            id: 1,
+            label: "Configuration"
+        },
+        {
+            id: 2,
+            label: "About"
+        }
+    ]), []);
+    const sectionRenderers: SectionRenderers = React.useMemo(() => ({
+        [0]: (_props: {}) => <ConnectorSettings />
+    }), []);
+
     return (
         <div className={styles.page}>
             <div className={styles.header_container}>
@@ -16,29 +35,13 @@ export const SettingsPage: React.FC<PageProps> = (_props: PageProps) => {
                     <div className={styles.page_title}>Settings</div>
                 </div>
             </div>
-            <div className={styles.body_container}>
-                <div className={styles.subpage_list_background} />
-                <div className={styles.section_list_container}>
-                    <div className={classNames(styles.section_list_entry, styles.section_list_entry_active)}>
-                        Connnectors
-                    </div>
-                    <div className={styles.section_list_entry}>Configuration</div>
-                    <div className={styles.section_list_entry}>About</div>
-                </div>
-                <div className={styles.connector_list_container}>
-                    <div className={styles.connector_list_entry}>Hyper Database</div>
-                    <div className={styles.connector_list_entry}>Salesforce Data Cloud</div>
-                    <div className={styles.connector_list_entry}>Local Scripts</div>
-                </div>
-                <div className={styles.connector_details_container}>
-                    <div className={styles.card_container}>
-                        <SalesforceConnectorPanel />
-                    </div>
-                    <div className={styles.card_container}>
-                        <HyperGrpcConnectorPanel />
-                    </div>
-                </div>
-            </div>
-        </div>
+            <SettingsPageNav
+                className={styles.body_container}
+                sections={sections}
+                sectionRenderers={sectionRenderers}
+                selectSection={selectSection}
+                selectedSection={selectedSection}
+            />
+        </div >
     );
 };
