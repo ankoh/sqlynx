44dfc65828ec1a00c3b2a4b211cd488114c0e046
André Kohn <kohn.a@outlook.com>
2024-03-02 23:08:55 +0100

    Add test for generic server streaming call

--- packages/sqlynx-native/src/grpc_client.rs
+++ packages/sqlynx-native/src/grpc_client.rs
@@ -7,42 +7,6 @@ type GenericGrpcClientData = Vec<u8>;
 
 #[derive(Debug, Clone, Default)]
 struct PassthroughCodec();
-#[derive(Debug, Clone, Default)]
-struct PassthroughEncoder();
-#[derive(Debug, Clone, Default)]
-struct PassthroughDecoder();
-
-impl Codec for PassthroughCodec {
-    type Encode = GenericGrpcClientData;
-    type Decode = GenericGrpcClientData;
-
-    type Encoder = PassthroughEncoder;
-    type Decoder = PassthroughDecoder;
-
-    fn encoder(&mut self) -> Self::Encoder {
-        PassthroughEncoder()
-    }
-    fn decoder(&mut self) -> Self::Decoder {
-        PassthroughDecoder()
-    }
-}
-impl Encoder for PassthroughEncoder {
-    type Item = GenericGrpcClientData;
-    type Error = Status;
-    fn encode(&mut self, item: Self::Item, buf: &mut EncodeBuf<'_>) -> Result<(), Self::Error> {
-        buf.writer().write(&item)?;
-        Ok(())
-    }
-}
-impl Decoder for PassthroughDecoder {
-    type Item = GenericGrpcClientData;
-    type Error = Status;
-    fn decode(&mut self, buf: &mut DecodeBuf<'_>) -> Result<Option<Self::Item>, Self::Error> {
-        let mut buffer = Vec::new();
-        buf.reader().read_to_end(&mut buffer)?;
-        Ok(Some(buffer))
-    }
-}
 
 pub struct GenericGrpcClient {
     inner: tonic::client::Grpc<tonic::transport::Channel>,
@@ -94,8 +58,47 @@ impl GenericGrpcClient {
                 )
             })?;
         let request = request.into_request();
-        let codec = tonic::codec::ProstCodec::default();
+        let codec = PassthroughCodec::default();
         self.inner.server_streaming(request, path, codec).await
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct PassthroughEncoder();
+#[derive(Debug, Clone, Default)]
+struct PassthroughDecoder();
+
+/// By default, Tonic uses a prost codec.
+/// We use a dedicated passthrough codec that bypasses prost and writes parameter bytes as-is.
+impl Codec for PassthroughCodec {
+    type Encode = GenericGrpcClientData;
+    type Decode = GenericGrpcClientData;
+
+    type Encoder = PassthroughEncoder;
+    type Decoder = PassthroughDecoder;
+
+    fn encoder(&mut self) -> Self::Encoder {
+        PassthroughEncoder()
+    }
+    fn decoder(&mut self) -> Self::Decoder {
+        PassthroughDecoder()
+    }
+}
+impl Encoder for PassthroughEncoder {
+    type Item = GenericGrpcClientData;
+    type Error = Status;
+    fn encode(&mut self, item: Self::Item, buf: &mut EncodeBuf<'_>) -> Result<(), Self::Error> {
+        buf.writer().write(&item)?;
+        Ok(())
+    }
+}
+impl Decoder for PassthroughDecoder {
+    type Item = GenericGrpcClientData;
+    type Error = Status;
+    fn decode(&mut self, buf: &mut DecodeBuf<'_>) -> Result<Option<Self::Item>, Self::Error> {
+        let mut buffer = Vec::new();
+        buf.reader().read_to_end(&mut buffer)?;
+        Ok(Some(buffer))
     }
 }
 
@@ -104,8 +107,9 @@ mod test {
     use super::*;
     use anyhow::Result;
     use prost::Message;
+    use tokio_stream::StreamExt;
 
-    use crate::{proto::sqlynx_test_v1::{TestUnaryRequest, TestUnaryResponse}, test::test_service_mock::{spawn_test_service_mock, TestServiceMock}};
+    use crate::{proto::sqlynx_test_v1::{TestServerStreamingRequest, TestServerStreamingResponse, TestUnaryRequest, TestUnaryResponse}, test::test_service_mock::{spawn_test_service_mock, TestServiceMock}};
 
     #[tokio::test]
     async fn test_unary() -> Result<()> {
@@ -142,11 +146,54 @@ mod test {
 
         // Check received buffer
         let received_buffer = unary_result.into_inner();
-        let received_message = TestUnaryResponse::decode(bytes::Bytes::from(received_buffer))?;
+        let received_message = TestUnaryResponse::decode(received_buffer.as_slice())?;
         assert_eq!(received_message.data, "response data");
 
         shutdown.send(()).unwrap();
         Ok(())
     }
 
+    #[tokio::test]
+    async fn test_server_streaming() -> Result<()> {
+        // Spawn a test service mock
+        let (mock, mut _setup_unary, mut setup_server_streaming) = TestServiceMock::new();
+        let (addr, shutdown) = spawn_test_service_mock(mock).await;
+
+        // Respond single streaming response
+        let streaming_call = tokio::spawn(async move {
+            let (param, result_sender) = setup_server_streaming.recv().await.unwrap();
+            result_sender.send(Ok(TestServerStreamingResponse {
+                data: "response data".to_string()
+            })).await.unwrap();
+            param
+        });
+
+        // Setup the gRPC client
+        let channel = tonic::transport::Endpoint::new(format!("http://{}", addr))?.connect().await?;
+        let mut client = GenericGrpcClient::new(channel);
+
+        // Call the unary test function
+        let streaming_path = tauri::http::uri::PathAndQuery::from_static(
+            "/sqlynx.test.v1.TestService/TestServerStreaming",
+        );
+        let streaming_param = TestServerStreamingRequest {
+            data: "request data".to_string()
+        };
+        let streaming_req = streaming_param.encode_to_vec();
+        let streaming_response = client.call_server_streaming(streaming_req, streaming_path).await?;
+
+        // Check received parameter
+        let received_param = streaming_call.await?;
+        assert_eq!(received_param.data, "request data");
+
+        // Check received buffer
+        let mut stream = streaming_response.into_inner();
+        while let Some(response) = stream.next().await {
+            let received_message = TestUnaryResponse::decode(response.unwrap().as_slice())?;
+            assert_eq!(received_message.data, "response data");
+        }
+
+        shutdown.send(()).unwrap();
+        Ok(())
+    }
 }
