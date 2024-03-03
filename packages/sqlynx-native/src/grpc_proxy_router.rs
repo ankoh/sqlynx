6bb695051ba1223da274b0820721ad1e7797e47b
André Kohn <kohn.a@outlook.com>
2024-03-03 14:36:33 +0100

    Draft router paths with axum

---
+++ packages/sqlynx-native/src/grpc_proxy_router.rs
@@ -0,0 +1,45 @@
+use axum::routing::{delete, get, post};
+use axum::Router;
+use axum::extract::{Path, Request};
+use axum::http::StatusCode;
+use lazy_static::lazy_static;
+
+use crate::grpc_proxy::GrpcProxy;
+
+lazy_static! {
+    static ref GRPC_PROXY: GrpcProxy = GrpcProxy::default();
+}
+
+async fn create_channel(_req: Request) -> Result<(), (StatusCode, String)> {
+    Ok(())
+}
+
+async fn delete_channel(Path(_channel_id): Path<usize>, _req: Request) -> Result<(), (StatusCode, String)> {
+    Ok(())
+}
+
+async fn call_unary(Path(_channel_id): Path<usize>, _req: Request) -> Result<Vec<u8>, (StatusCode, String)> {
+    Ok(Vec::new())
+}
+
+async fn start_server_stream(Path(_channel_id): Path<usize>, _req: Request) -> Result<(), (StatusCode, String)> {
+    Ok(())
+}
+
+async fn read_server_stream(Path(_channel_id): Path<usize>, Path(_stream_id): Path<usize>, _req: Request) -> Result<Vec<u8>, (StatusCode, String)> {
+    Ok(Vec::new())
+}
+
+async fn delete_server_stream(Path(_channel_id): Path<usize>, Path(_stream_id): Path<usize>, _req: Request) -> Result<(), (StatusCode, String)> {
+    Ok(())
+}
+
+pub fn create_grpc_router() -> axum::Router<()> {
+    Router::new()
+        .route("/channel", post(create_channel))
+        .route("/channel/:channel_id", delete(delete_channel))
+        .route("/channel/:channel_id/unary", post(call_unary))
+        .route("/channel/:channel_id/stream", post(start_server_stream))
+        .route("/channel/:channel_id/stream/:stream_id", get(read_server_stream))
+        .route("/channel/:channel_id/stream/:stream_id", delete(delete_server_stream))
+}
--- packages/sqlynx-native/src/ipc_router.rs
+++ packages/sqlynx-native/src/ipc_router.rs
@@ -1,37 +1,20 @@
-use lazy_static::lazy_static;
-use tauri::http::header::{ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE};
-use tauri::http::response::Builder as ResponseBuilder;
+use axum::Router;
+use tauri::http::header::ACCESS_CONTROL_ALLOW_ORIGIN;
+use tauri::http::header::CONTENT_TYPE;
 use tauri::http::Request;
 use tauri::http::Response;
+use tauri::http::response::Builder as ResponseBuilder;
+use lazy_static::lazy_static;
 
-use crate::grpc_proxy::GrpcProxy;
+use crate::grpc_proxy_router::create_grpc_router;
+
 
 lazy_static! {
-    static ref GRPC_PROXY: GrpcProxy = GrpcProxy::default();
+    static ref ROUTER: axum::routing::Router<()> = Router::new()
+        .nest("/grpc", create_grpc_router());
 }
 
-pub async fn route_ipc_request(request: Request<Vec<u8>>) -> Response<Vec<u8>> {
-    let uri = request.uri();
-
-    match uri.path() {
-        "/grpc/call-unary" => {
-            // XXX
-            GRPC_PROXY.call_unary(request).await;
-        }
-        "/grpc/start-server-stream" => {
-            // XXX
-            GRPC_PROXY.start_server_stream(request).await;
-        }
-        "/grpc/read-server-stream" => {
-            // XXX
-            GRPC_PROXY.read_server_stream(request).await;
-        }
-        "/grpc/cancel-server-stream" => {}
-        _ => {
-            // Report unknown
-        }
-    };
-
+pub async fn route_ipc_request(_request: Request<Vec<u8>>) -> Response<Vec<u8>> {
     let resp = ResponseBuilder::new()
         .header(CONTENT_TYPE, "text/plain")
         .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
