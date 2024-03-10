a37c58154c33024ab59f814e2b741b9d9524ddb2
André Kohn <kohn.a@outlook.com>
2024-03-10 13:15:23 +0100

    Work on mocking the native api

--- packages/sqlynx-app/src/platform/native_api_mock.test.ts
+++ packages/sqlynx-app/src/platform/native_api_mock.test.ts
@@ -4,38 +4,51 @@ import { NativeAPIMock } from './native_api_mock.js';
 import { PlatformType } from './platform_api.js';
 
 describe('Native API mock', () => {
-    it("rejects requests that are not targeting sqlynx-native://", async () => {
+    beforeEach(() => {
         const mock = new NativeAPIMock(PlatformType.MACOS);
         jest.spyOn(global, 'fetch').mockImplementation((req) => mock.process(req as Request));
+    });
+    afterEach(() => {
+        (global.fetch as jest.Mock).mockRestore();
+    })
+
+    it("rejects requests that are not targeting sqlynx-native://", async () => {
         const request = new Request(new URL("not-sqlynx-native://foo"), {
             method: 'POST',
             headers: {}
         });
         const response = await fetch(request);
         expect(response.status).toEqual(400);
-        (global.fetch as jest.Mock).mockRestore();
     });
     it("rejects requests with an invalid request path", async () => {
-        const mock = new NativeAPIMock(PlatformType.MACOS);
-        jest.spyOn(global, 'fetch').mockImplementation((req) => mock.process(req as Request));
         const request = new Request(new URL("sqlynx-native://local/invalid-path"), {
             method: 'POST',
             headers: {}
         });
         const response = await fetch(request);
-        expect(response.status).toEqual(400);
         expect(response.statusText).toEqual("unknown request path");
-        (global.fetch as jest.Mock).mockRestore();
+        expect(response.status).toEqual(400);
     });
     it("accepts requests that are targeting the root path /", async () => {
-        const mock = new NativeAPIMock(PlatformType.MACOS);
-        jest.spyOn(global, 'fetch').mockImplementation((req) => mock.process(req as Request));
         const request = new Request(new URL("sqlynx-native://local/"), {
             method: 'POST',
             headers: {}
         });
         const response = await fetch(request);
+        expect(response.statusText).toEqual("OK");
         expect(response.status).toEqual(200);
-        (global.fetch as jest.Mock).mockRestore();
     })
+    it("create channels when POSTing to /grpc/channels", async () => {
+        const request = new Request(new URL("sqlynx-native://local/grpc/channels"), {
+            method: 'POST',
+            headers: {}
+        });
+        const response = await fetch(request);
+        expect(response.statusText).toEqual("OK");
+        expect(response.status).toEqual(200);
+        expect(response.headers.has("sqlynx-channel-id")).toBeTruthy();
+        expect(() => {
+            Number.parseInt(response.headers.get("sqlynx-channel-id")!)
+        }).not.toThrow();
+    });
 });
--- packages/sqlynx-app/src/platform/native_api_mock.ts
+++ packages/sqlynx-app/src/platform/native_api_mock.ts
@@ -3,13 +3,74 @@ import { PlatformType } from "./platform_api.js";
 /// Only process requests that are targeting sqlynx-native://
 const NATIVE_API_SCHEME = "sqlynx-native://";
 
+// const HEADER_PREFIX = "sqlynx-";
+// const HEADER_NAME_HOST = "sqlynx-host";
+// const HEADER_NAME_TLS_CLIENT_KEY = "sqlynx-tls-client-key";
+// const HEADER_NAME_TLS_CLIENT_CERT = "sqlynx-tls-client-cert";
+// const HEADER_NAME_TLS_CACERTS = "sqlynx-tls-cacerts";
+// const HEADER_NAME_PATH = "sqlynx-path";
+const HEADER_NAME_CHANNEL_ID = "sqlynx-channel-id";
+// const HEADER_NAME_STREAM_ID = "sqlynx-stream-id";
+// const HEADER_NAME_READ_TIMEOUT = "sqlynx-read-timeout";
+// const HEADER_NAME_BATCH_TIMEOUT = "sqlynx-batch-timout";
+// const HEADER_NAME_BATCH_BYTES = "sqlynx-batch-bytes";
+// const HEADER_NAME_BATCH_EVENT = "sqlynx-batch-event";
+// const HEADER_NAME_BATCH_MESSAGES = "sqlynx-batch-messages";
+
+class GRPCChannel {
+    id: number;
+    constructor(id: number) {
+        this.id = id;
+    }
+};
+
+interface RouteMatchers {
+    grpcChannels: RegExp;
+    grpcChannel: RegExp;
+    grpcChannelUnary: RegExp;
+    grpcChannelStreams: RegExp;
+    grpcChannelStream: RegExp;
+}
+
 /// The native API mock mimics the api that is provided by tauri.
 export class NativeAPIMock {
+    /// The platform type
     platform: PlatformType;
+    /// The route matchers
+    routeMatchers: RouteMatchers;
+
+    /// The next gRPC channel id
+    nextGrpcChannelId: number;
+    /// All grpc Channels
+    grpcChannels: Map<number, GRPCChannel>;
+
 
     /// The constructor
     constructor(platform: PlatformType) {
         this.platform = platform;
+        this.routeMatchers = {
+            grpcChannels: new RegExp("^/grpc/channels$"),
+            grpcChannel: new RegExp("^/grpc/channel/(\d+)$"),
+            grpcChannelUnary: new RegExp("^/grpc/channel/(\d+)/unary$"),
+            grpcChannelStreams: new RegExp("^/grpc/channel/(\d+)/streams$"),
+            grpcChannelStream: new RegExp("^/grpc/channel/(\d+)/stream/(\d+)$"),
+        };
+        this.nextGrpcChannelId = 1;
+        this.grpcChannels = new Map();
+    }
+
+    protected async createGrpcChannel(_req: Request): Promise<Response> {
+        const channel = new GRPCChannel(this.nextGrpcChannelId);
+        this.grpcChannels.set(channel.id, channel);
+        this.nextGrpcChannelId += 1;
+
+        return new Response(null, {
+            status: 200,
+            statusText: "OK",
+            headers: {
+                [HEADER_NAME_CHANNEL_ID]: channel.id.toString()
+            }
+        });
     }
 
     /// Process a fetch request.
@@ -23,7 +84,7 @@ export class NativeAPIMock {
                 headers: {}
             });
         }
-
+        // If the request targets /, we consider this a health check and return with 200
         let url = new URL(req.url);
         if (url.pathname == "/") {
             return new Response(null, {
@@ -33,10 +94,24 @@ export class NativeAPIMock {
             });
         }
 
-        return new Response(null, {
+        // Helper to report an invalid request
+        const invalidRequest = () => new Response(null, {
             status: 400,
             statusText: "unknown request path",
             headers: {}
-        })
+        });
+
+        // Match the individual api routes
+        let matches: RegExpExecArray | null = null;
+        if ((matches = this.routeMatchers.grpcChannels.exec(url.pathname)) !== null) {
+            switch (req.method) {
+                case "POST": return this.createGrpcChannel(req);
+                default:
+                    return invalidRequest();
+            }
+        }
+
+
+        return invalidRequest();
     }
 }
