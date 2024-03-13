b0a2556dd9d8cc3e3067ba211e65dad8e160c447
André Kohn <kohn.a@outlook.com>
2024-03-13 13:00:42 +0100

    Work on native hyperdb clients

---
+++ packages/sqlynx-app/src/platform/native_grpc_client.ts
@@ -0,0 +1,172 @@
+import { HEADER_NAME_BATCH_BYTES, HEADER_NAME_BATCH_TIMEOUT, HEADER_NAME_CHANNEL_ID, HEADER_NAME_PATH, HEADER_NAME_READ_TIMEOUT, HEADER_NAME_STREAM_ID, HEADER_NAME_TLS_CACERTS, HEADER_NAME_TLS_CLIENT_CERT, HEADER_NAME_TLS_CLIENT_KEY } from "./native_api_mock.js";
+
+export enum NativeGrpcServerStreamBatchEvent {
+    StreamFailed = "StreamFailed",
+    StreamFinished = "StreamFinished",
+    FlushAfterClose = "FlushAfterClose",
+    FlushAfterTimeout = "FlushAfterTimeout",
+    FlushAfterBytes = "FlushAfterBytes",
+}
+
+export interface NativeGrpcServerStreamBatch {
+    /// The batch event
+    event: NativeGrpcServerStreamBatchEvent;
+    /// The messages
+    messages: Uint8Array[];
+    /// The trailers (if any)
+    trailers?: Record<string, string>;
+}
+
+export interface NativeGrpcEndpoint {
+    /// The endpoint URL
+    baseURL: URL;
+};
+
+const DEFAULT_READ_TIMEOUT = 1000;
+const DEFAULT_BATCH_TIMEOUT = 100;
+const DEFAULT_BATCH_BYTES = 8000000;
+
+function requireStringHeader(headers: Headers, key: string): string {
+    return headers.get(key)!;
+}
+function requireIntegerHeader(headers: Headers, key: string): number {
+    const raw = headers.get(key)!;
+    return Number.parseInt(raw);
+}
+
+export class NativeGrpcServerStream {
+    /// The endpoint
+    endpoint: NativeGrpcEndpoint;
+    /// The channel id
+    channelId: number;
+    /// The stream id
+    streamId: number;
+
+    constructor(endpoint: NativeGrpcEndpoint, channelId: number, streamId: number) {
+        this.endpoint = endpoint;
+        this.channelId = channelId;
+        this.streamId = streamId;
+    }
+
+    /// Read the next messages from the stream
+    async read(): Promise<NativeGrpcServerStreamBatch> {
+        const url = new URL(this.endpoint.baseURL);
+        url.pathname = `/grpc/channels/${this.channelId}/stream/${this.streamId}`;
+        const request = new Request(url, {
+            method: 'GET',
+            headers: {
+                [HEADER_NAME_READ_TIMEOUT]: DEFAULT_READ_TIMEOUT.toString(),
+                [HEADER_NAME_BATCH_TIMEOUT]: DEFAULT_BATCH_TIMEOUT.toString(),
+                [HEADER_NAME_BATCH_BYTES]: DEFAULT_BATCH_BYTES.toString(),
+            }
+        });
+        const response = await fetch(request);
+
+        if (response.status != 200) {
+            // Throw error
+        }
+
+        const streamBatchEvent = requireStringHeader(response.headers, "sqlynx-batch-event");
+        const streamBatchMessages = requireIntegerHeader(response.headers, "sqlynx-batch-messages");
+        const bodyBuffer = await response.arrayBuffer();
+        const bodyBufferView = new DataView(bodyBuffer);
+
+        // Unpack the messages
+        let offset = 0;
+        let messages = [];
+        while (offset < bodyBuffer.byteLength) {
+            const length = bodyBufferView.getUint32(offset, true);
+            messages.push(new Uint8Array(bodyBuffer, offset + 4, length));
+        }
+
+        // Message count mismatch?
+        // We treat this as an error since this means our encoded response buffer is corrupt.
+        if (streamBatchMessages != messages.length) {
+            // XXX
+        }
+
+        return {
+            event: streamBatchEvent as NativeGrpcServerStreamBatchEvent,
+            messages: messages,
+        };
+    }
+}
+
+interface StartServerStreamArgs {
+    path: string;
+    body: Uint8Array;
+    tlsClientKeyPath: string | null;
+    tlsClientCertPath: string | null;
+    tlsCacertsPath: string | null;
+}
+
+export class NativeGrpcChannel {
+    /// The endpoint
+    endpoint: NativeGrpcEndpoint;
+    /// The channel id
+    channelId: number;
+
+    constructor(endpoint: NativeGrpcEndpoint, channelId: number) {
+        this.endpoint = endpoint;
+        this.channelId = channelId;
+    }
+
+
+    /// Call a server streaming
+    protected async startServerStream(args: StartServerStreamArgs): Promise<NativeGrpcServerStream> {
+        const url = new URL(this.endpoint.baseURL);
+        url.pathname = `/grpc/channels/${this.channelId}/streams`;
+
+        // Collect the request headers
+        const headers = new Headers();
+        headers.set(HEADER_NAME_CHANNEL_ID, this.channelId.toString());
+        headers.set(HEADER_NAME_PATH, args.path);
+        if (args.tlsClientKeyPath) {
+            headers.set(HEADER_NAME_TLS_CLIENT_KEY, args.tlsClientKeyPath);
+        }
+        if (args.tlsClientCertPath) {
+            headers.set(HEADER_NAME_TLS_CLIENT_CERT, args.tlsClientCertPath);
+        }
+        if (args.tlsCacertsPath) {
+            headers.set(HEADER_NAME_TLS_CACERTS, args.tlsCacertsPath);
+        }
+
+        // Send the request
+        const request = new Request(url, {
+            method: 'POST',
+            headers,
+            body: args.body,
+        });
+        const response = await fetch(request);
+        if (response.status != 200) {
+            // Throw error
+        }
+
+        const streamId = requireIntegerHeader(response.headers, HEADER_NAME_STREAM_ID);
+        return new NativeGrpcServerStream(this.endpoint, this.channelId, streamId);
+    }
+}
+
+export class NativeGrpcClient {
+    /// The endpoint
+    endpoint: NativeGrpcEndpoint;
+
+    constructor(endpoint: NativeGrpcEndpoint) {
+        this.endpoint = endpoint;
+    }
+
+    /// Create a gRPC channel
+    protected async connectChannel(): Promise<NativeGrpcChannel> {
+        const url = new URL(this.endpoint.baseURL);
+        url.pathname = `/grpc/channels`;
+
+        const request = new Request(url, {
+            method: 'POST',
+            headers: {}
+        });
+        const response = await fetch(request);
+        const channelId = requireIntegerHeader(response.headers, HEADER_NAME_STREAM_ID);
+
+        return new NativeGrpcChannel(this.endpoint, channelId);
+    }
+}
--- packages/sqlynx-app/src/platform/native_hyperdb_client.ts
+++ packages/sqlynx-app/src/platform/native_hyperdb_client.ts
@@ -2,8 +2,14 @@ import * as arrow from 'apache-arrow';
 import * as proto from "@ankoh/hyperdb-proto";
 
 import { HyperDatabaseClient, HyperDatabaseConnection, HyperQueryExecutionProgress, HyperQueryExecutionStatus, HyperQueryResultStream } from "./platform_hyperdb_client.js";
+import { NativeGrpcChannel, NativeGrpcClient, NativeGrpcEndpoint, NativeGrpcServerStream } from './native_grpc_client.js';
 
-class QueryResultStream implements HyperQueryResultStream {
+class NativeHyperQueryResultStream extends NativeGrpcServerStream implements HyperQueryResultStream {
+
+    constructor(endpoint: NativeGrpcEndpoint, channelId: number, streamId: number) {
+        super(endpoint, channelId, streamId);
+    }
+
     /// Get the schema message
     public async getSchema(): Promise<arrow.Schema | null> {
         return null;
@@ -22,16 +28,24 @@ class QueryResultStream implements HyperQueryResultStream {
     }
 }
 
-class DatabaseConnection implements HyperDatabaseConnection {
+class NativeHyperDatabaseConnection extends NativeGrpcChannel implements HyperDatabaseConnection {
+    constructor(endpoint: NativeGrpcEndpoint, channelId: number) {
+        super(endpoint, channelId);
+    }
+
     /// Execute Query
     public async executeQuery(_param: proto.pb.QueryParam): Promise<HyperQueryResultStream> {
-        return new QueryResultStream();
+        return new NativeHyperQueryResultStream(this.endpoint, 0, 0);
     }
 }
 
-export class NativeHyperDatabaseClient implements HyperDatabaseClient {
+export class NativeHyperDatabaseClient extends NativeGrpcClient implements HyperDatabaseClient {
+    constructor(endpoint: NativeGrpcEndpoint) {
+        super(endpoint);
+    }
+
     /// Create a database connection
     public async connect(): Promise<HyperDatabaseConnection> {
-        return new DatabaseConnection();
+        return new NativeHyperDatabaseConnection(this.endpoint, 0);
     }
 }
