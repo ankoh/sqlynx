c10a6f542d6e1b7ca2066b40d393d5ec3e6f27b7
André Kohn <kohn.a@outlook.com>
2024-03-17 10:14:18 +0100

    Configure log tauri log plugin

---
+++ packages/sqlynx-native/src/logging.rs
@@ -0,0 +1,20 @@
+
+#[cfg(debug_assertions)]
+pub mod config {
+    use log::LevelFilter;
+    use tauri_plugin_log::Target as LogTarget;
+    use tauri_plugin_log::TargetKind as LogTargetKind;
+
+    pub const LOG_TARGETS: [LogTarget; 2] = [LogTarget::new(LogTargetKind::Stdout), LogTarget::new(LogTargetKind::Webview)];
+    pub const LOG_LEVEL: LevelFilter = LevelFilter::Trace;
+}
+
+#[cfg(not(debug_assertions))]
+pub mod config {
+    use log::LevelFilter;
+    use tauri_plugin_log::Target as LogTarget;
+    use tauri_plugin_log::TargetKind as LogTargetKind;
+
+    pub const LOG_TARGETS: [LogTarget; 2] = [LogTarget::Stdout, LogTarget::LogDir];
+    pub const LOG_LEVEL: LevelFilter = LevelFilter::Info;
+}
