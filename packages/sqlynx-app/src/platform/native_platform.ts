6d599ddbf4d9d6ad49546935595b4164e79820ac
André Kohn <kohn.a@outlook.com>
2024-03-19 16:25:59 +0100

    Flip 'Open In' button based on platform type

--- packages/sqlynx-app/src/platform/native_platform.ts
+++ packages/sqlynx-app/src/platform/native_platform.ts
@@ -9,22 +9,28 @@ interface NativeGlobals {
     }
 }
 
+/// The tauri globals
+const TAURI = ((window as any).__TAURI__ as NativeGlobals) ?? null;
+
+/// Is a running natively?
+export function isNativePlatform(): boolean {
+    return TAURI != null;
+}
+
 /// Initialize the native api, if we're running in a Native app
 export async function setupNativePlatform(setApi: (api: PlatformApi) => void) {
-    const tauri = (window as any).__TAURI__ as NativeGlobals;
-
     // Not running in tauri? (e.g. regular web app?)
     // Silently stop.
-    if (tauri === undefined) {
+    if (TAURI == null) {
         return;
     }
-    console.log(tauri);
+    console.log(TAURI);
 
     // Test command call
-    const os = await tauri.core.invoke("sqlynx_get_os");
+    const os = await TAURI.core.invoke("sqlynx_get_os");
     console.log(os);
     // Test streaming call via custom scheme
-    const path = tauri.core.convertFileSrc("foo", "sqlynx-native");
+    const path = TAURI.core.convertFileSrc("foo", "sqlynx-native");
     const response = await fetch(path);
     console.log(await response.text());
 
--- packages/sqlynx-app/src/platform/platform_api_provider.tsx
+++ packages/sqlynx-app/src/platform/platform_api_provider.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 
 import { PlatformApi } from './platform_api.js';
-import { setupNativePlatform } from './native_platform.js';
+import { isNativePlatform, setupNativePlatform } from './native_platform.js';
+import { setupWebPlatform } from './web_platform.js';
 
-/// Expose the native api through a context.
-/// In the browser, this context will just be null
 const PLATFORM_API_CTX = React.createContext<PlatformApi | null>(null);
 export const usePlatformApi = () => React.useContext(PLATFORM_API_CTX);
 
@@ -15,7 +14,11 @@ type Props = {
 export const PlatformApiProvider: React.FC<Props> = (props: Props) => {
     const [api, setApi] = React.useState<PlatformApi | null>(null);
     React.useEffect(() => {
-        setupNativePlatform(setApi);
+        if (isNativePlatform()) {
+            setupNativePlatform(setApi);
+        } else {
+            setupWebPlatform(setApi);
+        }
     }, []);
     return <PLATFORM_API_CTX.Provider value={api}>{props.children}</PLATFORM_API_CTX.Provider>;
 };
