a06aa77168fef41f6a3a347125412ecbf0e86ab8
André Kohn <kohn.a@outlook.com>
2024-03-26 20:44:24 +0100

    Don't expose log version through React state

--- packages/sqlynx-app/src/platform/logger_provider.tsx
+++ packages/sqlynx-app/src/platform/logger_provider.tsx
@@ -6,10 +6,8 @@ import { NativeLogger } from './native_logger.js';
 import { WebLogger } from './web_logger.js';
 
 const LOGGER_CTX = React.createContext<Logger | null>(null);
-const LOG_BUFFER_VERSION_CTX = React.createContext<number>(0);
 
 export const useLogger = () => React.useContext(LOGGER_CTX)!;
-export const useLogVersion = () => React.useContext(LOG_BUFFER_VERSION_CTX)!;
 
 type Props = {
     children: React.ReactElement;
@@ -20,19 +18,9 @@ export const LoggerProvider: React.FC<Props> = (props: Props) => {
     const logger = React.useMemo<Logger>(() => isNativePlatform() ? new NativeLogger(getNativeGlobals()) : new WebLogger(), []);
 
     // Maintain a log buffer version
-    const [version, setVersion] = React.useState<number>(0);
-    const versionCallback = React.useCallback(() => setVersion(v => v + 1), []);
-    React.useEffect(() => {
-        logger.buffer.observe(versionCallback, true);
-        return () => {
-            logger.buffer.observers.delete(versionCallback)
-        };
-    }, []);
     return (
         <LOGGER_CTX.Provider value={logger}>
-            <LOG_BUFFER_VERSION_CTX.Provider value={version}>
-                {props.children}
-            </LOG_BUFFER_VERSION_CTX.Provider>
+            {props.children}
         </LOGGER_CTX.Provider>
     )
 };
--- packages/sqlynx-app/src/view/log_viewer.tsx
+++ packages/sqlynx-app/src/view/log_viewer.tsx
@@ -2,19 +2,67 @@ import * as React from 'react';
 import { motion } from "framer-motion"
 
 import { createPortal } from 'react-dom';
+import { VariableSizeGrid as Grid } from 'react-window';
 import { IconButton } from '@primer/react';
 import { XIcon } from '@primer/octicons-react';
 
-import { useLogVersion, useLogger } from '../platform/logger_provider.js';
+import { useLogger } from '../platform/logger_provider.js';
 import styles from './log_viewer.module.css';
+import { observeSize } from './size_observer.js';
 
 interface LogViewerProps {
     onClose: () => void;
 }
 
+const COLUMN_0_WIDTH = 100;
+const ROW_HEIGHT = 40;
+
 export const LogViewer: React.FC<LogViewerProps> = (props: LogViewerProps) => {
     const logger = useLogger();
-    const logVersion = useLogVersion();
+    const logRowCount = logger.buffer.length;
+
+    // Determine log container dimensions
+    const logContainerRef = React.useRef<HTMLDivElement>(null);
+    const logContainerSize = observeSize(logContainerRef);
+    const logContainerWidth = logContainerSize?.width ?? 0;
+    const logContainerHeight = logContainerSize?.height ?? 0;
+    const getColumnWidth = (col: number) => (col == 0) ? COLUMN_0_WIDTH : (Math.max(logContainerWidth, COLUMN_0_WIDTH) - COLUMN_0_WIDTH);
+    const getRowHeight = (_row: number) => ROW_HEIGHT;
+
+    // Poll the log version when the viewer is opened
+    const [logVersion, setLogVersion] = React.useState<number>(logger.buffer.version);
+    React.useEffect(() => {
+        let intervalId = setInterval(() => {
+            if (logger.buffer.version !== logVersion) {
+                setLogVersion(logger.buffer.version);
+            }
+        }, 100);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    // Build key for grid refreshes
+    const logGridKey = (logContainerWidth & 0xFFFF) | ((logContainerHeight & 0xFFFF) << 16) | ((logVersion & 0xFFFF) << 32);
+    console.log(`width=${logContainerWidth}, height=${logContainerHeight}, logGridKey=${logGridKey}`);
+
+    const Cell = ({ columnIndex, rowIndex, style }: any) => {
+        const record = logger.buffer.at(rowIndex);
+        if (columnIndex == 0) {
+            return (
+                <div style={style}>
+                    {record!.level}
+                </div>
+            );
+        } else {
+            return (
+                <div style={style}>
+                    {record!.message}
+                </div>
+            );
+        }
+    };
+
     return (
         <div className={styles.overlay}>
             <motion.div
@@ -43,8 +91,19 @@ export const LogViewer: React.FC<LogViewerProps> = (props: LogViewerProps) => {
                         />
                     </div>
                 </div>
-                {logger.buffer.length}
-                {logVersion}
+                <div className={styles.log_grid_container} ref={logContainerRef}>
+                    <Grid
+                        key={logGridKey}
+                        width={logContainerWidth}
+                        height={logContainerHeight}
+                        columnCount={2}
+                        columnWidth={getColumnWidth}
+                        rowCount={logRowCount}
+                        rowHeight={getRowHeight}
+                    >
+                        {Cell}
+                    </Grid>
+                </div>
             </motion.div>
         </div>
     );
