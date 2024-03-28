cac9093ffdfb3b8f31019183777b38a30e616979
André Kohn <kohn.a@outlook.com>
2024-03-29 00:22:44 +0100

    Display first information from update manifests

--- packages/sqlynx-app/src/platform/native_version_check.tsx
+++ packages/sqlynx-app/src/platform/native_version_check.tsx
@@ -7,6 +7,7 @@ import { Result, RESULT_ERROR, RESULT_OK } from '../utils/result.js';
 import { Logger } from './logger.js';
 import { loadReleaseManifest, ReleaseChannel, ReleaseManifest } from './web_version_check.js';
 import { SQLYNX_CANARY_RELEASE_MANIFEST, SQLYNX_STABLE_RELEASE_MANIFEST } from '../globals.js';
+import { STABLE_RELEASE_MANIFEST_CTX, STABLE_UPDATE_MANIFEST_CTX, CANARY_RELEASE_MANIFEST_CTX, CANARY_UPDATE_MANIFEST_CTX, UPDATE_STATUS_CTX, UpdateStatus } from './version_check.js';
 
 type Props = {
     children: React.ReactElement;
@@ -45,10 +46,10 @@ export const NativeVersionCheck: React.FC<Props> = (props: Props) => {
     // It is actually redundant to fetch the dedicated release manifest just for the UI as well.
     // Let's check if we can contribute upstream if `check` can return version information also if there's no newer version available.
 
-    const [_stableRelease, setStableRelease] = React.useState<Result<ReleaseManifest> | null>(null);
-    const [_stableUpdate, setStableUpdate] = React.useState<Result<Update | null> | null>(null);
-    const [_canaryRelease, setCanaryRelease] = React.useState<Result<ReleaseManifest> | null>(null);
-    const [_canaryUpdate, setCanaryUpdate] = React.useState<Result<Update | null> | null>(null);
+    const [stableRelease, setStableRelease] = React.useState<Result<ReleaseManifest> | null>(null);
+    const [stableUpdate, setStableUpdate] = React.useState<Result<Update | null> | null>(null);
+    const [canaryRelease, setCanaryRelease] = React.useState<Result<ReleaseManifest> | null>(null);
+    const [canaryUpdate, setCanaryUpdate] = React.useState<Result<Update | null> | null>(null);
 
     React.useEffect(() => {
         loadReleaseManifest("stable", SQLYNX_STABLE_RELEASE_MANIFEST, setStableRelease, logger);
@@ -57,7 +58,36 @@ export const NativeVersionCheck: React.FC<Props> = (props: Props) => {
         checkChannelUpdates("canary", setCanaryUpdate, logger);
     }, []);
 
-
-    return props.children;
+    let updateFetched = false;
+    let updateAvailable = false;
+    if (stableUpdate != null && stableUpdate.type == RESULT_OK) {
+        updateFetched = true;
+        updateAvailable ||= stableUpdate.value != null;
+    }
+    if (canaryUpdate != null && canaryUpdate.type == RESULT_OK) {
+        updateFetched = true;
+        updateAvailable ||= canaryUpdate.value != null;
+    }
+    let status = UpdateStatus.Unknown;
+    if (updateFetched) {
+        if (updateAvailable) {
+            status = UpdateStatus.UpdateAvailable;
+        } else {
+            status = UpdateStatus.UpToDate;
+        }
+    }
+    return (
+        <UPDATE_STATUS_CTX.Provider value={status}>
+            <STABLE_RELEASE_MANIFEST_CTX.Provider value={stableRelease}>
+                <STABLE_UPDATE_MANIFEST_CTX.Provider value={stableUpdate}>
+                    <CANARY_RELEASE_MANIFEST_CTX.Provider value={canaryRelease}>
+                        <CANARY_UPDATE_MANIFEST_CTX.Provider value={canaryUpdate}>
+                            {props.children}
+                        </CANARY_UPDATE_MANIFEST_CTX.Provider>
+                    </CANARY_RELEASE_MANIFEST_CTX.Provider>
+                </STABLE_UPDATE_MANIFEST_CTX.Provider>
+            </STABLE_RELEASE_MANIFEST_CTX.Provider>
+        </UPDATE_STATUS_CTX.Provider>
+    );
 };
 
--- packages/sqlynx-app/src/platform/version_check.tsx
+++ packages/sqlynx-app/src/platform/version_check.tsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
+
+import { Update } from '@tauri-apps/plugin-updater';
+
 import { isNativePlatform } from './native_globals.js';
 import { NativeVersionCheck } from './native_version_check.js';
-import { WebVersionCheck } from './web_version_check.js';
+import { ReleaseManifest, WebVersionCheck } from './web_version_check.js';
+import { Result } from '../utils/result.js';
 
 export enum UpdateStatus {
     Unknown,
+    Disabled,
     UpToDate,
     UpdateAvailable
 }
 
 export const UPDATE_STATUS_CTX = React.createContext<UpdateStatus>(UpdateStatus.Unknown);
+export const STABLE_RELEASE_MANIFEST_CTX = React.createContext<Result<ReleaseManifest> | null>(null);
+export const STABLE_UPDATE_MANIFEST_CTX = React.createContext<Result<Update | null> | null>(null);
+export const CANARY_RELEASE_MANIFEST_CTX = React.createContext<Result<ReleaseManifest> | null>(null);
+export const CANARY_UPDATE_MANIFEST_CTX = React.createContext<Result<Update | null> | null>(null);
+
+export const useUpdateStatus = () => React.useContext(UPDATE_STATUS_CTX)!;
+export const useStableReleaseManifest = () => React.useContext(STABLE_RELEASE_MANIFEST_CTX)!;
+export const useStableUpdateManifest = () => React.useContext(STABLE_UPDATE_MANIFEST_CTX)!;
+export const useCanaryReleaseManifest = () => React.useContext(CANARY_RELEASE_MANIFEST_CTX)!;
+export const useCanaryUpdateManifest = () => React.useContext(CANARY_UPDATE_MANIFEST_CTX)!;
 
 type Props = {
     children: React.ReactElement;
@@ -22,4 +37,3 @@ export const VersionCheck: React.FC<Props> = (props: Props) => {
         return <WebVersionCheck {...props} />;
     }
 };
-
