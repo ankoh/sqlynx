ff5d18a35ced810d87abb4abcd715b89e22e9a23
André Kohn <kohn.a@outlook.com>
2024-04-01 20:03:22 +0200

    Use banner page style for session setup

--- packages/sqlynx-app/src/session/session_setup_page.tsx
+++ packages/sqlynx-app/src/session/session_setup_page.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import * as LZString from 'lz-string';
 import { Button, IconButton } from '@primer/react';
-import { SyncIcon } from '@primer/octicons-react';
 
 import {
     CONNECTOR_INFOS,
@@ -19,8 +18,10 @@ import { REPLACE_SCRIPT_CONTENT } from './session_state_reducer.js';
 import { useActiveSessionState, useActiveSessionStateDispatch } from './session_state_provider.js';
 import { ScriptKey } from './session_state.js';
 import { SQLYNX_VERSION } from '../globals.js';
+import { TextField } from '../view/text_field.js';
+import { LogViewerInPortal } from '../view/log_viewer.js';
 
-import styles from './session_setup_page.module.css';
+import page_styles from '../view/banner_page.module.css';
 
 import * as symbols from '../../static/svg/symbols.generated.svg';
 
@@ -36,6 +37,8 @@ interface State {
 }
 
 export const SessionSetupPage: React.FC<Props> = (props: Props) => {
+    const [logsAreOpen, setLogsAreOpen] = React.useState<boolean>(false);
+
     const salesforceAuth = useSalesforceAuthState();
     const selectedScript = useActiveSessionState();
     const selectedScriptDispatch = useActiveSessionStateDispatch();
@@ -79,7 +82,7 @@ export const SessionSetupPage: React.FC<Props> = (props: Props) => {
             break;
     }
 
-    // Initial auth trigger
+    // Distinguish the different connector types
     const authOnce = React.useRef<boolean>(false);
     React.useEffect(() => {
         if (
@@ -135,104 +138,179 @@ export const SessionSetupPage: React.FC<Props> = (props: Props) => {
     }, [state, selectedScript, connectorAuthCheck]);
 
     // Get the auth status
-    const canContinue = connectorAuthCheck === null || connectorAuthCheck === ConnectorAuthCheck.AUTHENTICATED;
-    let statusText: string = 'ready';
-    switch (connectorAuthCheck) {
-        case ConnectorAuthCheck.AUTHENTICATED:
-            statusText = 'Authenticated';
-            break;
-        case ConnectorAuthCheck.AUTHENTICATION_IN_PROGRESS:
-            statusText = 'Authentication In Progress';
-            break;
-        case ConnectorAuthCheck.AUTHENTICATION_NOT_STARTED:
-        case ConnectorAuthCheck.CLIENT_ID_MISMATCH:
-            statusText = 'Authentication Required';
-            break;
-        case ConnectorAuthCheck.AUTHENTICATION_FAILED:
-            statusText = 'Authentication Failed';
-            break;
-        case ConnectorAuthCheck.UNKNOWN:
-            statusText = '';
+    // const canContinue = connectorAuthCheck === null || connectorAuthCheck === ConnectorAuthCheck.AUTHENTICATED;
+    // let statusText: string = 'ready';
+    // switch (connectorAuthCheck) {
+    //     case ConnectorAuthCheck.AUTHENTICATED:
+    //         statusText = 'Authenticated';
+    //         break;
+    //     case ConnectorAuthCheck.AUTHENTICATION_IN_PROGRESS:
+    //         statusText = 'Authentication In Progress';
+    //         break;
+    //     case ConnectorAuthCheck.AUTHENTICATION_NOT_STARTED:
+    //     case ConnectorAuthCheck.CLIENT_ID_MISMATCH:
+    //         statusText = 'Authentication Required';
+    //         break;
+    //     case ConnectorAuthCheck.AUTHENTICATION_FAILED:
+    //         statusText = 'Authentication Failed';
+    //         break;
+    //     case ConnectorAuthCheck.UNKNOWN:
+    //         statusText = '';
+    //         break;
+    // }
+
+    // Render connector options
+    let connectorOptionsSection: React.ReactElement = <div />;
+    switch (state?.connectorParams?.type) {
+        case SALESFORCE_DATA_CLOUD:
+            console.log(state.connectorParams);
+            connectorOptionsSection = (
+                <div className={page_styles.card_section}>
+                    <div className={page_styles.section_entries}>
+                        <TextField
+                            name="Salesforce Instance URL"
+                            value={state.connectorParams.value.instanceUrl ?? ""}
+                            readOnly={true}
+                            disabled={true}
+                            leadingVisual={() => <div>URL</div>}
+                        />
+                        <TextField
+                            name="Connected App"
+                            value={state.connectorParams.value.appConsumerKey ?? ""}
+                            readOnly={true}
+                            disabled={true}
+                            leadingVisual={() => <div>ID</div>}
+                        />
+                    </div>
+                </div>
+            );
             break;
     }
 
-    const DetailEntry = (props: {
-        label: string;
-        children: (React.ReactElement | undefined)[] | React.ReactElement;
-    }) => (
-        <>
-            <div className={styles.detail_entry_key}>{props.label}</div>
-            <div className={styles.detail_entry_value}>{props.children}</div>
-        </>
-    );
-    const Bean = (props: { text: string }) => <span className={styles.bean}>{props.text}</span>;
-
+    // Construct the page
     return (
-        <div className={styles.page}>
-            <div className={styles.banner_container}>
-                <div className={styles.banner_logo}>
+        <div className={page_styles.page}>
+            <div className={page_styles.banner_container}>
+                <div className={page_styles.banner_logo}>
                     <svg width="100%" height="100%">
                         <use xlinkHref={`${symbols}#sqlynx-inverted`} />
                     </svg>
                 </div>
-                <div className={styles.banner_text_container}>
-                    <div className={styles.banner_title}>sqlynx</div>
-                    <div className={styles.app_version}>version {SQLYNX_VERSION ?? '-'}</div>
+                <div className={page_styles.banner_text_container}>
+                    <div className={page_styles.banner_title}>sqlynx</div>
+                    <div className={page_styles.app_version}>version {SQLYNX_VERSION}</div>
                 </div>
             </div>
-            <div className={styles.card_container}>
-                <div className={styles.card_header}>
-                    <div>Setup</div>
-                </div>
-                <div className={styles.card_section}>
-                    <div className={styles.card_section_header}>SQL</div>
-                    <div className={styles.detail_entries}>
-                        <DetailEntry label="Inline Script">
-                            <Bean text={(state?.scriptText?.length ?? 0) + ' chars'} />
-                        </DetailEntry>
-                        <DetailEntry label="Inline Schema">
-                            <Bean text={(state?.schemaText?.length ?? 0) + ' chars'} />
-                        </DetailEntry>
+            <div className={page_styles.card_container}>
+                <div className={page_styles.card_header}>
+                    <div className={page_styles.card_header_left_container}>
+                        Setup
+                    </div>
+                    <div className={page_styles.card_header_right_container}>
+                        <IconButton
+                            variant="invisible"
+                            icon={() => (
+                                <svg width="16px" height="16px">
+                                    <use xlinkHref={`${symbols}#log`} />
+                                </svg>
+                            )}
+                            aria-label="close-overlay"
+                            onClick={() => setLogsAreOpen(s => !s)}
+                        />
+                        {logsAreOpen && <LogViewerInPortal onClose={() => setLogsAreOpen(false)} />}
                     </div>
                 </div>
-                <div className={styles.card_section}>
-                    <div className={styles.card_section_header}>Connector</div>
-                    <div className={styles.detail_entries}>
-                        <DetailEntry label="Connector Type">
-                            <Bean text={connectorInfo?.displayName.long ?? 'unknown'} />
-                        </DetailEntry>
-                        <DetailEntry label="Supported Platforms">
-                            {connectorInfo?.platforms.native ? <Bean text="Native" /> : undefined}
-                            {connectorInfo?.platforms.browser ? <Bean text="Web" /> : undefined}
-                        </DetailEntry>
-                        {state?.connectorParams?.type == SALESFORCE_DATA_CLOUD && (
-                            <>
-                                <DetailEntry label="Instance Url">
-                                    <Bean text={state?.connectorParams.value.instanceUrl ?? 'not set'} />
-                                </DetailEntry>
-                                <DetailEntry label="Connected App">
-                                    <Bean text={state?.connectorParams.value.appConsumerKey ?? 'not set'} />
-                                </DetailEntry>
-                            </>
-                        )}
+                <div className={page_styles.card_section}>
+                    <div className={page_styles.section_entries}>
+                        <TextField
+                            name="Inline Script"
+                            value={state?.scriptText ?? ""}
+                            readOnly={true}
+                            disabled={true}
+                            leadingVisual={() => <div>Base64 script text with 0 characters</div>}
+                        />
+                        {(state?.schemaText?.length ?? 0) > 0 &&
+                            <TextField
+                                name="Inline Schema"
+                                value={state?.schemaText ?? ""}
+                                readOnly={true}
+                                disabled={true}
+                                leadingVisual={() => <div>Text</div>}
+                            />
+                        }
                     </div>
                 </div>
-                <div className={styles.card_actions}>
-                    {canContinue ? (
-                        <Button className={styles.card_action_continue} variant="primary" onClick={props.onDone}>
-                            Continue
-                        </Button>
-                    ) : (
-                        <>
-                            <Button variant="danger" onClick={props.onDone}>
-                                Skip
-                            </Button>
-                            <div className={styles.card_status_text}>{statusText}</div>
-                            <IconButton className={styles.card_action_restart} icon={SyncIcon} aria-labelledby="sync" />
-                        </>
-                    )}
-                </div>
+                {connectorOptionsSection}
             </div>
         </div>
     );
+
+    // return (
+    //     <div className={styles.page}>
+    //         <div className={styles.banner_container}>
+    //             <div className={styles.banner_logo}>
+    //                 <svg width="100%" height="100%">
+    //                     <use xlinkHref={`${symbols}#sqlynx-inverted`} />
+    //                 </svg>
+    //             </div>
+    //             <div className={styles.banner_text_container}>
+    //                 <div className={styles.banner_title}>sqlynx</div>
+    //                 <div className={styles.app_version}>version {SQLYNX_VERSION ?? '-'}</div>
+    //             </div>
+    //         </div>
+    //         <div className={styles.card_container}>
+    //             <div className={styles.card_header}>
+    //                 <div>Setup</div>
+    //             </div>
+    //             <div className={styles.card_section}>
+    //                 <div className={styles.card_section_header}>SQL</div>
+    //                 <div className={styles.detail_entries}>
+    //                     <DetailEntry label="Inline Script">
+    //                         <Bean text={(state?.scriptText?.length ?? 0) + ' chars'} />
+    //                     </DetailEntry>
+    //                     <DetailEntry label="Inline Schema">
+    //                         <Bean text={(state?.schemaText?.length ?? 0) + ' chars'} />
+    //                     </DetailEntry>
+    //                 </div>
+    //             </div>
+    //             <div className={styles.card_section}>
+    //                 <div className={styles.card_section_header}>Connector</div>
+    //                 <div className={styles.detail_entries}>
+    //                     <DetailEntry label="Connector Type">
+    //                         <Bean text={connectorInfo?.displayName.long ?? 'unknown'} />
+    //                     </DetailEntry>
+    //                     <DetailEntry label="Supported Platforms">
+    //                         {connectorInfo?.platforms.native ? <Bean text="Native" /> : undefined}
+    //                         {connectorInfo?.platforms.browser ? <Bean text="Web" /> : undefined}
+    //                     </DetailEntry>
+    //                     {state?.connectorParams?.type == SALESFORCE_DATA_CLOUD && (
+    //                         <>
+    //                             <DetailEntry label="Instance Url">
+    //                                 <Bean text={state?.connectorParams.value.instanceUrl ?? 'not set'} />
+    //                             </DetailEntry>
+    //                             <DetailEntry label="Connected App">
+    //                                 <Bean text={state?.connectorParams.value.appConsumerKey ?? 'not set'} />
+    //                             </DetailEntry>
+    //                         </>
+    //                     )}
+    //                 </div>
+    //             </div>
+    //             <div className={styles.card_actions}>
+    //                 {canContinue ? (
+    //                     <Button className={styles.card_action_continue} variant="primary" onClick={props.onDone}>
+    //                         Continue
+    //                     </Button>
+    //                 ) : (
+    //                     <>
+    //                         <Button variant="danger" onClick={props.onDone}>
+    //                             Skip
+    //                         </Button>
+    //                         <div className={styles.card_status_text}>{statusText}</div>
+    //                         <IconButton className={styles.card_action_restart} icon={SyncIcon} aria-labelledby="sync" />
+    //                     </>
+    //                 )}
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
 };
