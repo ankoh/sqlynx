7a1f093378b7a7d8f021f4f2b6c27a958e65b719
André Kohn <kohn.a@outlook.com>
2024-04-17 09:32:25 +0200

    Prepare sending debug hint to oauth redirect

--- packages/sqlynx-app/src/platform/native_globals.ts
+++ packages/sqlynx-app/src/platform/native_globals.ts
@@ -1,4 +1,12 @@
+import { invoke } from "@tauri-apps/api/core";
+
 /// Is a running natively?
 export function isNativePlatform(): boolean {
     return '__TAURI__' in (globalThis as any);
+}
+
+/// Is the native side built with debug flags?
+/// We assume that debug builds are not registered properly to receive deep-links from the OS.
+export async function isNativeDebugBuild(): Promise<boolean> {
+    return await invoke("sqlynx_is_debug_build") as boolean;
 }
