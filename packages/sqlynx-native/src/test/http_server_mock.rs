e9ec0380577a388af9c8d85b1d11ae377b7288b5
André Kohn <kohn.a@outlook.com>
2024-05-07 10:11:25 +0200

    Work on http server mock

--- packages/sqlynx-native/src/test/http_server_mock.rs
+++ packages/sqlynx-native/src/test/http_server_mock.rs
@@ -1,10 +1,14 @@
-use std::convert::Infallible;
-
 use http::{HeaderMap, HeaderValue};
 use http_body_util::{Full, StreamBody};
 use hyper::body::Body;
 use hyper::body::Frame;
+use hyper::body;
 use hyper::{Request, Response};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::sync::oneshot;
 use tokio_stream::wrappers::ReceiverStream;
 
 pub enum PartialResponse {
@@ -15,11 +19,11 @@ pub enum PartialResponse {
 pub type ResponseSender = tokio::sync::mpsc::Sender<PartialResponse>;
 
 pub struct HttpServiceMock {
-    pub setup_server_stream: tokio::sync::mpsc::Sender<(Request<Vec<u8>>, ResponseSender)>,
+    pub setup_server_stream: tokio::sync::mpsc::Sender<(Request<body::Incoming>, ResponseSender)>,
 }
 
 impl HttpServiceMock {
-    pub async fn handle_request(&self, request: Request<Vec<u8>>) -> hyper::Result<Response<Box<dyn Body<Data = bytes::Bytes, Error = Infallible>>>> {
+    pub async fn handle_request(&self, request: Request<body::Incoming>) -> hyper::Result<Response<Box<dyn Body<Data = bytes::Bytes, Error = Infallible>>>> {
         // Setup a channel for sending the result
         let (result_sender, mut receiver) = tokio::sync::mpsc::channel(1);
 
@@ -76,53 +80,56 @@ impl HttpServiceMock {
         Ok(response)
     }
 
-    pub fn new() -> (Self, tokio::sync::mpsc::Receiver<(Request<Vec<u8>>, ResponseSender)>) {
+    pub fn new() -> (Self, tokio::sync::mpsc::Receiver<(Request<body::Incoming>, ResponseSender)>) {
         let (setup_server_stream, recv_server_stream) = tokio::sync::mpsc::channel(1);
         (Self { setup_server_stream }, recv_server_stream)
     }
 }
 
+async fn accept_connections(listener: tokio::net::TcpListener, mut shutdown_rx: oneshot::Receiver<()>, mock: Arc<HttpServiceMock>) {
+    loop {
+        tokio::select! {
+            conn = listener.accept() => {
+                let (stream, peer_addr) = match conn {
+                    Ok(conn) => conn,
+                    Err(e) => {
+                        log::error!("accept error: {}", e);
+                        tokio::time::sleep(Duration::from_secs(1)).await;
+                        continue;
+                    }
+                };
+                log::info!("incomming connection accepted: {}", peer_addr);
+                let stream = hyper_util::rt::TokioIo::new(Box::pin(stream));
 
-// pub async fn spawn_http_service_mock(mock: HttpServiceMock) -> (SocketAddr, oneshot::Sender<()>) {
-//     // Create the listener up front so the server is immediately ready
-//     // bind to port `0` so the OS finds a free port
-//     let listener = tokio::net::TcpListener::bind("0.0.0.0:0").await.unwrap();
-//     let addr = listener.local_addr().unwrap();
-// 
-// 
-//     (addr)
-// }
+                 tokio::spawn(async move {
+                    let server = hyper_util::server::conn::auto::Builder::new(hyper_util::rt::TokioExecutor::new());
+                    let conn = server.serve_connection_with_upgrades(stream, hyper::service::service_fn(|request| async move {
+                        mock.handle_request(request).await
+                    }));
+                    if let Err(err) = conn.await {
+                        log::error!("connection error: {}", err);
+                    }
+                    log::info!("connection dropped: {}", peer_addr);
+                });
+            }
+            _ = &mut shutdown_rx => {
+                drop(listener);
+                break;
+            }
+        }
+    }
+}
 
-// 
-//     let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
-// 
-//     loop {
-//         let (stream, _) = listener.accept().await?;
-//         let io = TokioIo::new(stream);
-// 
-//         tokio::task::spawn(async move {
-//             if let Err(err) = http1::Builder::new()
-//                 .preserve_header_case(true)
-//                 .title_case_headers(true)
-//                 .serve_connection(io, service_fn(proxy))
-//                 .with_upgrades()
-//                 .await
-//             {
-//                 println!("Failed to serve connection: {:?}", err);
-//             }
-//         });
-//     }
-// 
-//     tokio::spawn(async move {
-//         tonic::transport::Server::builder()
-//             .add_service(service)
-//             .serve_with_incoming_shutdown(
-//                 tokio_stream::wrappers::TcpListenerStream::new(listener),
-//                 async { drop(shutdown_rx.await) },
-//             )
-//             .await
-//             .unwrap();
-//     });
-//     (addr, shutdown_tx)
-// 
-// }
+pub async fn spawn_http_service_mock(mock: Arc<HttpServiceMock>) -> (SocketAddr, oneshot::Sender<()>) {
+    // Create a listener
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+    let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
+
+    // Create the server loop
+    tokio::spawn(async move {
+        accept_connections(listener, shutdown_rx, mock).await
+    });
+
+    (addr, shutdown_tx)
+}
