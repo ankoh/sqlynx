e0b6bc8e095625351131bd344e0a5348116834fe
André Kohn <kohn.a@outlook.com>
2024-05-09 00:27:25 +0200

    Add http proxy test

--- packages/sqlynx-native/src/ipc_router.rs
+++ packages/sqlynx-native/src/ipc_router.rs
@@ -39,7 +39,6 @@ pub async fn route_ipc_request(mut request: Request<Vec<u8>>) -> Response<Vec<u8
                     .unwrap();
             }
         };
-        log::trace!("http proxy responded with {:?}", response);
         return response;
     }
 
