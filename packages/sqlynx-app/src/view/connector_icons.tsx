136246219311958bde419ba48e9db506c1b8dd9b
André Kohn <kohn.a@outlook.com>
2024-05-25 22:59:15 +0200

    Use placeholder icon for brainstorm connector

--- packages/sqlynx-app/src/view/connector_icons.tsx
+++ packages/sqlynx-app/src/view/connector_icons.tsx
@@ -3,16 +3,15 @@ import * as React from 'react';
 import { ConnectorInfo, ConnectorType } from '../connectors/connector_info.js';
 
 import * as icons from '../../static/svg/symbols.generated.svg';
-import { ZapIcon } from '@primer/octicons-react';
 
 export const SalesforceIcon = () => (
     <svg width="20px" height="20px">
         <use xlinkHref={`${icons}#salesforce_notext`} />
     </svg>
 );
-const CloudOfflineIcon = () => (
+const BrainstormIcon = () => (
     <svg width="20px" height="20px">
-        <use xlinkHref={`${icons}#cloud_offline`} />
+        <use xlinkHref={`${icons}#square`} />
     </svg>
 );
 const HyperIcon = () => (
@@ -24,7 +23,7 @@ const HyperIcon = () => (
 export function getConnectorIcon(connector: ConnectorInfo): React.ReactElement {
     switch (connector.connectorType) {
         case ConnectorType.BRAINSTORM_MODE:
-            return <ZapIcon />;
+            return <BrainstormIcon />;
         case ConnectorType.SALESFORCE_DATA_CLOUD:
             return <SalesforceIcon />;
         case ConnectorType.HYPER_GRPC:
