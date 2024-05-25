a75b0312ffbd11f37fdccb4793320e402dd06c9e
André Kohn <kohn.a@outlook.com>
2024-05-25 09:51:53 +0200

    Use ESLint properly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,38 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import ts from 'typescript-eslint';
 
-export default tseslint.config(
+const config = ts.config(
     eslint.configs.recommended,
-    ...tseslint.configs.recommended,
+    ...ts.configs.recommended,
+    {
+        // ...
+        "rules": {
+            // Turn off unused vars as it can report incorrect errors
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                // Ignore variables with leading underscore
+                {
+                    "argsIgnorePattern": "^_",
+                    "varsIgnorePattern": "^_",
+                    "caughtErrorsIgnorePattern": "^_"
+                }
+            ]
+        }
+    },
+    {
+        "ignores": [
+            "target/*",
+            "node_modules/*",
+            "packages/sqlynx-app/build",
+            "packages/sqlynx-core",
+            "packages/sqlynx-native",
+            "packages/sqlynx-pack",
+            "packages/sqlynx-core-api/dist",
+            "packages/sqlynx-core-api/gen",
+            "packages/sqlynx-pb/dist",
+            "packages/sqlynx-pb/gen",
+        ],
+    }
 );
+export default config;
