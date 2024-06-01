2d7658e7add0938c0810cff1415655acaadffa8f
André Kohn <kohn.a@outlook.com>
2024-06-01 09:24:55 +0200

    Work on history list in file connector

--- packages/sqlynx-app/src/view/connectors/file_connector_settings.tsx
+++ packages/sqlynx-app/src/view/connectors/file_connector_settings.tsx
@@ -6,28 +6,29 @@ import { EXAMPLE_SCHEMAS } from '../../session/example_scripts.js';
 import * as symbols from '../../../static/svg/symbols.generated.svg';
 import * as baseStyle from './connector_settings.module.css';
 import * as style from './file_connector_settings.module.css';
-import { ScriptMetadata, ScriptType } from '../../session/script_metadata.js';
+import { createScriptMetadata, ScriptMetadata, ScriptOriginType, ScriptType } from '../../session/script_metadata.js';
+import { ConnectorType } from '../../connectors/connector_info.js';
 
 const LOG_CTX = "files_connector";
 
 interface Props {}
 
 function SchemaEntry(props: {metadata: ScriptMetadata }) {
-    return (<div key={props.metadata.scriptId} className={style.example_script}>
-        <div className={classNames(style.example_script_icon, style.example_script_icon_schema)}>
+    return (<div key={props.metadata.scriptId} className={style.file}>
+        <div className={classNames(style.file_icon, style.file_icon_schema)}>
             <svg width="14px" height="14px">
                 <use xlinkHref={`${symbols}#database`} />
             </svg>
         </div>
-        <div className={style.example_script_name}>
+        <div className={style.file_name}>
             {props.metadata.name}
         </div>
-        <div className={style.example_script_info}>
-            <div className={style.example_script_table_count}>
+        <div className={style.file_info}>
+            <div className={style.file_table_count}>
                 <svg width="12px" height="12px">
                     <use xlinkHref={`${symbols}#rows`} />
                 </svg>
-                <span className={style.example_script_table_count_label}>
+                <span className={style.file_table_count_label}>
                     {props.metadata.annotations?.tableDefs?.size ?? 0}
                 </span>
             </div>
@@ -35,21 +36,21 @@ function SchemaEntry(props: {metadata: ScriptMetadata }) {
     </div>)
 };
 function QueryEntry(props: {metadata: ScriptMetadata}) {
-    return (<div key={props.metadata.scriptId} className={style.example_script}>
-        <div className={classNames(style.example_script_icon, style.example_script_icon_query)}>
+    return (<div key={props.metadata.scriptId} className={style.file}>
+        <div className={classNames(style.file_icon, style.file_icon_query)}>
             <svg width="14px" height="14px">
                 <use xlinkHref={`${symbols}#search`} />
             </svg>
         </div>
-        <div className={style.example_script_name}>
+        <div className={style.file_name}>
             {props.metadata.name}
         </div>
-        <div className={style.example_script_info}>
-            <div className={style.example_script_table_count}>
+        <div className={style.file_info}>
+            <div className={style.file_table_count}>
                 <svg width="12px" height="12px">
                     <use xlinkHref={`${symbols}#rows`} />
                 </svg>
-                <span className={style.example_script_table_count_label}>       {props.metadata.annotations?.tableRefs?.size ?? 0}
+                <span className={style.file_table_count_label}>       {props.metadata.annotations?.tableRefs?.size ?? 0}
                 </span>
             </div>
         </div>
@@ -57,7 +58,7 @@ function QueryEntry(props: {metadata: ScriptMetadata}) {
 };
 
 export const FileConnectorSettings: React.FC<Props> = (_props: Props) => {
-    const example_schemas_out: React.ReactElement[] = [];
+    const example_groups: React.ReactElement[] = [];
     for (const example of EXAMPLE_SCHEMAS) {
         const queriesOut: React.ReactElement[] = [];
         let i = 0;
@@ -65,17 +66,48 @@ export const FileConnectorSettings: React.FC<Props> = (_props: Props) => {
         for (const query of example.queries) {
             queriesOut.push(<QueryEntry key={i++} metadata={query} />);
         }
-        example_schemas_out.push(
-            <div key={example.schema.scriptId} className={style.example_schema}>
-                <div className={style.example_schema_name}>
+        example_groups.push(
+            <div key={example.schema.scriptId} className={style.file_group}>
+                <div className={style.file_group_name}>
                     {example.name}
                 </div>
-                <div className={style.example_scripts}>
+                <div className={style.files}>
                     {queriesOut}
                 </div>
             </div>
         );
     }
+
+    const sfSchema0 = createScriptMetadata({
+        schemaId: null,
+        name: "sf_1717225402_0.sql",
+        scriptType: ScriptType.SCHEMA,
+        originType: ScriptOriginType.LOCAL,
+        httpURL: null,
+        annotations: {
+            tableDefs: new Set(["some_dmo__dlm", "another_dmo__dlm"]),
+            tenantName: "a360/falcondev/0b306c1896f34230a54b13db23f019a6"
+        }
+    });
+
+    const sfQuery0 = createScriptMetadata({
+        schemaId: sfSchema0.schemaId,
+        name: "sf_1717225482_0.sql",
+        scriptType: ScriptType.QUERY,
+        originType: ScriptOriginType.LOCAL,
+        httpURL: null,
+        annotations: {
+            tableRefs: new Set(["some_dmo__dlm"]),
+            tenantName: "a360/falcondev/0b306c1896f34230a54b13db23f019a6"
+        }
+    })
+
+    const history = {
+        [ConnectorType.SALESFORCE_DATA_CLOUD]: [
+            sfQuery0, sfSchema0
+        ],
+        [ConnectorType.HYPER_GRPC]: []
+    };
 
     return (
         <div className={baseStyle.layout}>
@@ -91,14 +123,18 @@ export const FileConnectorSettings: React.FC<Props> = (_props: Props) => {
             </div >
             <div className={baseStyle.body_container}>
                 <div className={baseStyle.section}>
-                    <div className={classNames(baseStyle.body_section_layout)}>
-                        History
+                    <div className={classNames(style.scripts_layout, baseStyle.body_section_layout)}>
+                        <div className={style.scripts_section_header}>
+                            History
+                        </div>
                     </div>
                 </div>
                 <div className={baseStyle.section}>
-                    <div className={classNames(style.example_section_layout, baseStyle.body_section_layout)}>
-                        Examples
-                        {example_schemas_out}
+                    <div className={classNames(style.scripts_layout, baseStyle.body_section_layout)}>
+                        <div className={style.scripts_section_header}>
+                            Examples
+                        </div>
+                        {example_groups}
                     </div>
                 </div>
             </div>
