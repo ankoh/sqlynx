21a24d93bb39aa062bbda2c07977912c694ab1e8
André Kohn <kohn.a@outlook.com>
2024-06-18 12:56:43 +0200

    Transmit native app events as string array

--- packages/sqlynx-app/src/platform/native_event_listener.ts
+++ packages/sqlynx-app/src/platform/native_event_listener.ts
@@ -3,6 +3,8 @@ import { UnlistenFn, listen } from "@tauri-apps/api/event";
 import { Logger } from './logger.js';
 import { AppEventListener } from "./event_listener.js";
 
+const LOG_CTX = "native_event_listener";
+
 const NATIVE_EVENT_CHANNEL = "sqlynx:event";
 
 export class NativeAppEventListener extends AppEventListener {
@@ -15,9 +17,13 @@ export class NativeAppEventListener extends AppEventListener {
 
     public listenForAppEvents() {
         this.unlistenNativeEvents = listen(NATIVE_EVENT_CHANNEL, (event: any) => {
-            const data = this.readAppEvent(event.payload.message, `${NATIVE_EVENT_CHANNEL} message`);
-            if (data != null) {
-                super.dispatchAppEvent(data);
+            const events = event.payload as string[];
+            this.logger.debug(`received native app events: ${events.toString()}`, LOG_CTX);
+            for (const event of events) {
+                const data = this.readAppEvent(event, `${NATIVE_EVENT_CHANNEL} message`);
+                if (data != null) {
+                    super.dispatchAppEvent(data);
+                }
             }
         });
     }
