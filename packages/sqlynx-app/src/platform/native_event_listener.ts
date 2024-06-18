0516ebfe85c6973809437a056a02939e0caf7d3a
André Kohn <kohn.a@outlook.com>
2024-06-18 14:48:54 +0200

    Try reading the initial deep-link event

--- packages/sqlynx-app/src/platform/native_event_listener.ts
+++ packages/sqlynx-app/src/platform/native_event_listener.ts
@@ -1,4 +1,5 @@
 import { UnlistenFn, listen } from "@tauri-apps/api/event";
+import { invoke } from "@tauri-apps/api/core";
 
 import { Logger } from './logger.js';
 import { AppEventListener } from "./event_listener.js";
@@ -15,6 +16,26 @@ export class NativeAppEventListener extends AppEventListener {
         this.unlistenNativeEvents = null;
     }
 
+    // Best-effort attempt to read the initial deep links after setting up the event listener
+    protected async readInitialDeepLinkEvents() {
+        try {
+            const currentLinks: string[] = await invoke("plugin:deep-link|get_current");
+            for (const currentLink of currentLinks) {
+                const url = new URL(currentLink);
+                const search = url.searchParams;
+                const data = search.get("data");
+                if (data) {
+                    const event = this.readAppEvent(data, "initial deep link");
+                    if (event != null) {
+                        super.dispatchAppEvent(event);
+                    }
+                }
+            }
+        } catch (e: any) {
+            console.warn(e)
+        }
+    }
+
     public listenForAppEvents() {
         this.unlistenNativeEvents = listen(NATIVE_EVENT_CHANNEL, (event: any) => {
             const events = event.payload as string[];
@@ -26,5 +47,8 @@ export class NativeAppEventListener extends AppEventListener {
                 }
             }
         });
+
+        // Read the initial deep link events
+        this.readInitialDeepLinkEvents();
     }
 }
