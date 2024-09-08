86b0c3ac9a2d6f3d8528dafd8b8442e1b3cbcbd5
André Kohn <kohn.a@outlook.com>
2024-09-08 16:01:25 +0200

    Work on completion

--- packages/sqlynx-core/include/sqlynx/text/names.h
+++ packages/sqlynx-core/include/sqlynx/text/names.h
@@ -1,69 +1,17 @@
 #pragma once
 
-#include <type_traits>
-
 #include "ankerl/unordered_dense.h"
 #include "sqlynx/catalog_object.h"
 #include "sqlynx/proto/proto_generated.h"
 #include "sqlynx/utils/chunk_buffer.h"
+#include "sqlynx/utils/enum_bitset.h"
 #include "sqlynx/utils/intrusive_list.h"
 
 namespace sqlynx {
 
 namespace sx = proto;
 
-struct NameTags {
-    using ValueType = uint8_t;
-
-    /// The value
-    ValueType value;
-    /// Constructor
-    NameTags(ValueType value = 0) : value(value) {}
-    /// Constructor
-    NameTags(proto::NameTag value) : value(static_cast<ValueType>(value)) {}
-    /// Constructor
-    NameTags(std::initializer_list<proto::NameTag> values) : value(0) {
-        for (auto v : values) {
-            value |= static_cast<ValueType>(v);
-        }
-    }
-    /// Contains a name tag
-    bool contains(proto::NameTag tag) const { return (value & static_cast<ValueType>(tag)) != 0; }
-    /// Get the value
-    operator ValueType() const { return value; }
-    /// Tag a name
-    NameTags& operator|=(proto::NameTag tag) {
-        value |= static_cast<ValueType>(tag);
-        return *this;
-    }
-    /// Tag a name
-    NameTags& operator|=(NameTags tags) {
-        value |= tags.value;
-        return *this;
-    }
-    /// Tag a name
-    NameTags operator|(proto::NameTag tag) {
-        ValueType v = value | static_cast<ValueType>(tag);
-        return NameTags{v};
-    }
-    /// Untag a name
-    NameTags& operator^=(proto::NameTag tag) {
-        value &= ~static_cast<ValueType>(tag);
-        return *this;
-    }
-
-    template <typename Fn> void ForEach(Fn f) const {
-        for (auto tag = proto::NameTag::KEYWORD; tag <= proto::NameTag::MAX;
-             tag = static_cast<proto::NameTag>(static_cast<uint8_t>(tag) << 1)) {
-            if (contains(tag)) {
-                f(tag);
-            }
-        }
-    }
-};
-static_assert(std::is_trivially_copyable<NameTags::ValueType>::value);
-static_assert(sizeof(NameTags::ValueType) == sizeof(NameTags));
-static_assert(sizeof(NameTags::ValueType) == sizeof(proto::NameTag));
+using NameTags = EnumBitset<uint8_t, proto::NameTag, proto::NameTag::MAX>;
 
 /// An indexed name id
 using RegisteredNameID = uint32_t;
