82b5929a90048084960ac36e33e3d9b4fc63bf12
André Kohn <kohn.a@outlook.com>
2024-09-16 17:04:00 +0200

    Draw focused edges in separate layer

--- packages/sqlynx-app/src/view/catalog/edge_layer.tsx
+++ packages/sqlynx-app/src/view/catalog/edge_layer.tsx
@@ -1,34 +1,28 @@
 import * as React from 'react';
-import * as styles from './catalog_viewer.module.css';
-
-import { LayoutGroup, motion } from "framer-motion";
 
 interface EdgeLayerProps {
     width: number;
     height: number;
     padding: number;
     paths: React.ReactElement[];
+    className: string;
 }
 
 export function EdgeLayer(props: EdgeLayerProps) {
     return (
         <div
-            className={styles.edge_layer}
+            className={props.className}
             style={{
                 padding: props.padding,
             }}
         >
-            <LayoutGroup>
-                <motion.svg
-                    viewBox={`0 0 ${props.width} ${props.height}`}
-                    width={props.width}
-                    height={props.height}
-                    initial="hidden"
-                    animate="visible"
-                >
-                    {props.paths}
-                </motion.svg>
-            </LayoutGroup>
+            <svg
+                viewBox={`0 0 ${props.width} ${props.height}`}
+                width={props.width}
+                height={props.height}
+            >
+                {props.paths}
+            </svg>
         </div>
     );
 }
