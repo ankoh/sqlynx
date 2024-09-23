4a664fe634471a3d26c59e1b16feb8eb526e01b7
André Kohn <kohn.a@outlook.com>
2024-09-22 18:03:38 -0700

    Remove catalog logging output

--- packages/sqlynx-app/src/connectors/salesforce_catalog_update.ts
+++ packages/sqlynx-app/src/connectors/salesforce_catalog_update.ts
@@ -4,7 +4,6 @@ import { SalesforceMetadata } from './salesforce_api_client.js';
 
 export function updateDataCloudCatalog(catalog: sqlynx.SQLynxCatalog, metadata: SalesforceMetadata) {
     const tables: sqlynx.proto.SchemaTableT[] = [];
-    console.log(metadata.toString());
     if (metadata.metadata) {
         for (const entry of metadata.metadata) {
             const table = new sqlynx.proto.SchemaTableT();
