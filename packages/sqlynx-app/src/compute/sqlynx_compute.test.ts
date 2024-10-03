81eeb72fad5ecc993c440c511cd0f0e631b0e94f
André Kohn <kohn.a@outlook.com>
2024-10-03 18:36:53 +0200

    Test Arrow reordering

--- packages/sqlynx-app/src/compute/sqlynx_compute.test.ts
+++ packages/sqlynx-app/src/compute/sqlynx_compute.test.ts
@@ -5,7 +5,7 @@ import * as sqlynx_compute from '@ankoh/sqlynx-compute';
 import * as path from 'path';
 import * as fs from 'fs';
 import { fileURLToPath } from 'node:url';
-import { DataFrameIpcStreamIterable } from './sqlynx_compute.js';
+import { DataFrameIpcStreamIterable, dataFrameFromTable as createDataFrameFromTable, readDataFrame } from './sqlynx_compute.js';
 
 const distPath = path.resolve(fileURLToPath(new URL('../../../sqlynx-compute/dist/', import.meta.url)));
 const wasmPath = path.resolve(distPath, './sqlynx_compute_bg.wasm');
@@ -27,24 +27,15 @@ describe('SQLynxCompute Arrow IO', () => {
     });
 
     it('Ingest', async () => {
-        const ingest = new sqlynx_compute.ArrowIngest();
-        const tableBuffer = arrow.tableToIPC(inTable0, 'stream');
-        ingest.read(tableBuffer);
-        const dataFrame = ingest.finish();
-
-        ingest.free();
+        const dataFrame = createDataFrameFromTable(inTable0);
         dataFrame.free();
     });
 
-    it('Ipc stream', async () => {
-        const ingest = new sqlynx_compute.ArrowIngest();
-        const tableBuffer = arrow.tableToIPC(inTable0, 'stream');
-        ingest.read(tableBuffer);
-        const dataFrame = ingest.finish();
+    it('IPC stream', async () => {
+        const dataFrame = createDataFrameFromTable(inTable0);
 
         const dataFrameStream = dataFrame.createIpcStream();
         const stream = new DataFrameIpcStreamIterable(dataFrame, dataFrameStream);
-
         const batchReader = arrow.RecordBatchReader.from<{ 'test': arrow.Int32 }>(stream);
         expect(batchReader.isStream()).toBeTruthy();
 
@@ -60,5 +51,35 @@ describe('SQLynxCompute Arrow IO', () => {
             { test: 7 },
             { test: 8 },
         ]);
+
+        dataFrameStream.free();
+        dataFrame.free();
     })
+});
+
+const testOrderByColumn = async (inTable: arrow.Table, columnId: number, asc: boolean, nullsFirst: boolean, mapper: (o: any) => any, expected: any[]) => {
+    const dataFrame = createDataFrameFromTable(inTable);
+    const orderedFrame = await dataFrame.orderByColumn(columnId, asc, nullsFirst);
+    dataFrame.free();
+
+    const table = readDataFrame(orderedFrame);
+    orderedFrame.free();
+
+    const mapped = table.toArray().map(mapper);
+    expect(mapped).toEqual(expected);
+};
+
+describe('SQLynxCompute OrderBy', () => {
+    it('Sort Float64', async () => {
+        const t = arrow.tableFromArrays({
+            id: new Int32Array([1, 2, 3, 4]),
+            score: new Float64Array([42.0, 10.2, 10.1, 30.005])
+        });
+        testOrderByColumn(t, 1, true, false, o => ({ id: o.id, score: o.score }), [
+            { id: 3, score: 10.1 },
+            { id: 2, score: 10.2 },
+            { id: 4, score: 30.005 },
+            { id: 1, score: 42.0 },
+        ]);
+    });
 });
