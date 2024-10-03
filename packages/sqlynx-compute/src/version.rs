a010cf21f1919e41e15b8474ff39b0a4dc06b47e
André Kohn <kohn.a@outlook.com>
2024-10-03 14:10:17 +0200

    Bake git hash as perliminary version into compute module

--- packages/sqlynx-compute/src/version.rs
+++ packages/sqlynx-compute/src/version.rs
@@ -1,6 +1,11 @@
 use wasm_bindgen::prelude::*;
 
+pub static SQLYNX_COMPUTE_GIT_HASH_SHORT: &str = env!(
+    "GIT_HASH_SHORT",
+    "Can not find find GIT_HASH_SHORT in build environment"
+);
+
 #[wasm_bindgen(js_name = "getVersion")]
 pub fn get_version() -> String {
-    return "foo".to_string();
+    return SQLYNX_COMPUTE_GIT_HASH_SHORT.to_string();
 }
