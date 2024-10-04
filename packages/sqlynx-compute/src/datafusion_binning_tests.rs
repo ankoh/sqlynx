eedd0294dca7a926091484319a9500211f0b657b
André Kohn <kohn.a@outlook.com>
2024-10-05 01:32:10 +0200

    Test interval binning with datafusion

--- packages/sqlynx-compute/src/datafusion_binning_tests.rs
+++ packages/sqlynx-compute/src/datafusion_binning_tests.rs
@@ -4,21 +4,26 @@ use arrow::datatypes::DataType;
 use arrow::datatypes::TimeUnit;
 use arrow::util::pretty::pretty_format_batches;
 use chrono::DateTime;
+use datafusion_common::ScalarValue;
 use datafusion_execution::TaskContext;
 use datafusion_expr::AggregateUDF;
+use datafusion_expr::Operator;
 use datafusion_functions_aggregate::min_max::{Max, Min};
 use datafusion_physical_expr::aggregate::AggregateExprBuilder;
+use datafusion_physical_expr::expressions::{binary, CastExpr};
 use datafusion_physical_expr::expressions::col;
+use datafusion_physical_expr::expressions::lit;
 use datafusion_physical_plan::aggregates::{AggregateMode, AggregateExec, PhysicalGroupBy};
 use datafusion_physical_plan::collect;
 use datafusion_physical_plan::memory::MemoryExec;
+use datafusion_physical_plan::projection::ProjectionExec;
 use indoc::indoc;
 use pretty_assertions::assert_eq;
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[tokio::test]
-async fn test_minmax_timestamp() -> anyhow::Result<()> {
+async fn test_bin_timestamps() -> anyhow::Result<()> {
     let mut schema_builder = SchemaBuilder::with_capacity(2);
     schema_builder.push(Field::new("id", DataType::Int32, false));
     schema_builder.push(Field::new("when", DataType::Timestamp(TimeUnit::Millisecond, None), false));
@@ -55,7 +60,7 @@ async fn test_minmax_timestamp() -> anyhow::Result<()> {
         .alias("max")
         .build()?;
 
-    let input = Arc::new(
+    let data_scan = Arc::new(
         MemoryExec::try_new(&[vec![data.clone()]], data.schema(), None)?,
     );
     let groupby_exec = Arc::new(AggregateExec::try_new(
@@ -63,17 +68,16 @@ async fn test_minmax_timestamp() -> anyhow::Result<()> {
         grouping.clone(),
         vec![when_min, when_max],
         vec![None, None],
-        input.clone(),
+        data_scan.clone(),
         data.schema()
     )?);
     let task_ctx = Arc::new(TaskContext::default());
-    let result = collect(groupby_exec, task_ctx.clone()).await?;
+    let minmax_result = collect(groupby_exec, task_ctx.clone()).await?;
 
-    assert_eq!(result.len(), 1);
-    assert_eq!(result[0].num_columns(), 2);
-    assert_eq!(result[0].num_rows(), 1);
-
-    assert_eq!(format!("{}", pretty_format_batches(&result)?), indoc! {"
+    assert_eq!(minmax_result.len(), 1);
+    assert_eq!(minmax_result[0].num_columns(), 2);
+    assert_eq!(minmax_result[0].num_rows(), 1);
+    assert_eq!(format!("{}", pretty_format_batches(&minmax_result)?), indoc! {"
         +---------------------+---------------------+
         | min                 | max                 |
         +---------------------+---------------------+
@@ -81,9 +85,57 @@ async fn test_minmax_timestamp() -> anyhow::Result<()> {
         +---------------------+---------------------+
     "}.trim());
 
-    let min_col = result[0].column(0).as_primitive::<TimestampMillisecondType>();
-    let max_col = result[0].column(1).as_primitive::<TimestampMillisecondType>();
+    let min_col = minmax_result[0].column(0).as_primitive::<TimestampMillisecondType>();
+    let max_col = minmax_result[0].column(1).as_primitive::<TimestampMillisecondType>();
     assert_eq!(min_col.value_as_datetime(0).unwrap().to_string(), "2024-04-01 12:00:00");
     assert_eq!(max_col.value_as_datetime(0).unwrap().to_string(), "2024-04-03 12:00:00");
+
+    let range_millis = max_col.value(0) - min_col.value(0);
+    let bin_millis = range_millis / 100;
+
+    // Maybe we get around date_part here for now by just casting to milliseconds.
+    // Otherwise, we can fall back to:
+    // let date_part_udf = Arc::new(ScalarUDF::new_from_impl(DatePartFunc::new()));
+    // let diff_extract = Arc::new(ScalarFunctionExpr::new("min_ms", date_part_udf.clone(), vec![lit("day"), time_diff.clone()], DataType::Float64));
+    // let time_diff_cast = Arc::new(CastExpr::new(time_diff.clone(), DataType::Int64, None));
+
+    let when_diff_min = binary(
+        col("when", data.schema_ref())?,
+        Operator::Minus,
+        lit(ScalarValue::TimestampMillisecond(Some(min_col.value(0)), None)),
+        data.schema_ref())?;
+    let when_diff_min_cast = Arc::new(CastExpr::new(when_diff_min, DataType::Int64, None));
+    let when_bin = binary(
+        when_diff_min_cast,
+        Operator::Divide,
+        lit(ScalarValue::Int64(Some(bin_millis))),
+        data.schema_ref())?;
+
+    let data_scan = Arc::new(
+        MemoryExec::try_new(&[vec![data.clone()]], data.schema(), None)?,
+    );
+    let projection_exec = Arc::new(ProjectionExec::try_new(
+        vec![
+            (col("id", data.schema_ref())?, "id".to_string()),
+            (when_bin, "bin".to_string())
+        ],
+        data_scan
+    )?);
+    let task_ctx = Arc::new(TaskContext::default());
+    let projection_result = collect(projection_exec, task_ctx.clone()).await?;
+
+    assert_eq!(projection_result.len(), 1);
+    assert_eq!(projection_result[0].num_columns(), 2);
+    assert_eq!(projection_result[0].num_rows(), 3);
+    assert_eq!(format!("{}", pretty_format_batches(&projection_result)?), indoc! {"
+        +----+-----+
+        | id | bin |
+        +----+-----+
+        | 1  | 0   |
+        | 2  | 50  |
+        | 3  | 100 |
+        +----+-----+
+    "}.trim());
+
     Ok(())
 }
