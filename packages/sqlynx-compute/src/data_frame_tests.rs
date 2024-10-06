01232f19332a0792267fef65a46eece3aad35faa
André Kohn <kohn.a@outlook.com>
2024-10-06 14:14:46 +0200

    Test string and string length aggregation

--- packages/sqlynx-compute/src/data_frame_tests.rs
+++ packages/sqlynx-compute/src/data_frame_tests.rs
@@ -1,4 +1,5 @@
-use arrow::array::{ArrayRef, Int32Array, Int64Array, RecordBatch};
+use arrow::array::{ArrayRef, Int32Array, Int64Array, RecordBatch, StringArray};
+use arrow::datatypes::{Field, SchemaBuilder, DataType};
 use arrow::util::pretty::pretty_format_batches;
 use std::sync::Arc;
 use indoc::indoc;
@@ -76,6 +77,84 @@ async fn test_minmax_int64() -> anyhow::Result<()> {
         +-----------+-----------+
         | 7000      | 9000      |
         +-----------+-----------+
+    "}.trim());
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_minmax_string() -> anyhow::Result<()> {
+    let mut schema_builder = SchemaBuilder::with_capacity(2);
+    schema_builder.push(Field::new("v1", DataType::Utf8, false));
+    schema_builder.push(Field::new("v2", DataType::Utf8, false));
+    let schema = schema_builder.finish();
+    let data = RecordBatch::try_new(schema.into(), vec![
+        Arc::new(StringArray::from(vec![
+            "860f346a-5e02-4f32-93db-6c54378e31bb/0",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/1",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/2",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/3",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/4",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/5",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/6",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/7",
+            "860f346a-5e02-4f32-93db-6c54378e31bb/8",
+        ])) as ArrayRef,
+        Arc::new(StringArray::from(vec![
+            "5f20bedd",
+            "5f20bedd-6ee4",
+            "5f20bedd",
+            "5f20bedd-6ee4-4df9",
+            "5f20bedd-6ee4",
+            "5f20bedd-6ee4-4df9-8cac",
+            "5f20bedd-6ee4-4df9",
+            "5f20bedd-6ee4-4df9-8cac-2d4907900011",
+            "5f20bedd-6ee4-4df9-8cac-2d4907900012",
+        ])) as ArrayRef,
+    ])?;
+    let data_frame = DataFrame::new(data.schema(), vec![data]);
+    let transform = DataFrameTransform {
+        order_by: None,
+        group_by: Some(GroupByTransform {
+            keys: vec![],
+            aggregates: vec![
+                GroupByAggregate {
+                    field_name: "v1".into(),
+                    output_alias: "v1_min".into(),
+                    aggregation_function: AggregationFunction::Min.into(),
+                    aggregate_distinct: false,
+                    aggregate_lengths: false
+                },
+                GroupByAggregate {
+                    field_name: "v1".into(),
+                    output_alias: "v1_max".into(),
+                    aggregation_function: AggregationFunction::Max.into(),
+                    aggregate_distinct: false,
+                    aggregate_lengths: false
+                },
+                GroupByAggregate {
+                    field_name: "v2".into(),
+                    output_alias: "v2_chars_min".into(),
+                    aggregation_function: AggregationFunction::Min.into(),
+                    aggregate_distinct: false,
+                    aggregate_lengths: true
+                },
+                GroupByAggregate {
+                    field_name: "v2".into(),
+                    output_alias: "v2_chars_max".into(),
+                    aggregation_function: AggregationFunction::Max.into(),
+                    aggregate_distinct: false,
+                    aggregate_lengths: true
+                }
+            ]
+        }),
+    };
+    let transformed = data_frame.transform(&transform, None).await?;
+    assert_eq!(format!("{}", pretty_format_batches(&transformed.partitions[0])?), indoc! {"
+        +----------------------------------------+----------------------------------------+--------------+--------------+
+        | v1_min                                 | v1_max                                 | v2_chars_min | v2_chars_max |
+        +----------------------------------------+----------------------------------------+--------------+--------------+
+        | 860f346a-5e02-4f32-93db-6c54378e31bb/0 | 860f346a-5e02-4f32-93db-6c54378e31bb/8 | 8            | 36           |
+        +----------------------------------------+----------------------------------------+--------------+--------------+
     "}.trim());
     Ok(())
 }
