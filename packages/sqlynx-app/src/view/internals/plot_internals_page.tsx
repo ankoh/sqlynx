8d635abe2007b56be63589abc52e774abc3d2a72
André Kohn <kohn.a@outlook.com>
2024-10-21 13:23:25 +0200

    Refactor scales in histogram example

--- packages/sqlynx-app/src/view/internals/plot_internals_page.tsx
+++ packages/sqlynx-app/src/view/internals/plot_internals_page.tsx
@@ -81,10 +81,8 @@ function filterTable(input: arrow.Table<testSchemaType>, selectBegin: number, se
 function Example(): React.ReactElement {
     const containerRef = React.useRef<HTMLDivElement>(null);
     const rows = React.useMemo(() => testTable.toArray(), [testTable]);
-    const xDomain = [rows[0].binField, rows[rows.length - 1].binField];
 
     const [rangeSelection, setRangeSelection] = React.useState<[number, number] | null>(null);
-
     const onRangeSelection = React.useCallback((selBegin: number, selEnd: number) => {
         setRangeSelection([selBegin, selEnd]);
         filterTable(testTable, selBegin, selEnd);
@@ -93,20 +91,22 @@ function Example(): React.ReactElement {
         setRangeSelection(null);
     }, []);
 
-    React.useLayoutEffect(() => {
-        var margin = { top: 0, right: 0, bottom: 0, left: 0 },
-            width = 130 - margin.left - margin.right,
-            height = 30 - margin.top - margin.bottom;
+    var margin = { top: 0, right: 0, bottom: 0, left: 0 },
+        width = 130 - margin.left - margin.right,
+        height = 30 - margin.top - margin.bottom;
 
-        var x = d3.scaleBand()
+    const xDomain = [rows[0].binField, rows[rows.length - 1].binField];
+    const [xScale, yScale] = React.useMemo(() => ([
+        d3.scaleBand()
             .range([0, width])
             .domain(rows.map(d => d.binField))
-            .padding(0.1);
-
-        var y = d3.scaleLinear()
+            .padding(0.1),
+        d3.scaleLinear()
             .domain([0, 100])
-            .range([height, 0]);
+            .range([height, 0])
+    ]), []);
 
+    React.useLayoutEffect(() => {
         d3.select(containerRef.current).selectChildren().remove();
 
         const svg = d3
@@ -137,10 +137,10 @@ function Example(): React.ReactElement {
             .data(rows)
             .enter()
             .append("rect")
-            .attr("x", d => x(d.binField)!)
-            .attr("y", d => y(d.countField)!)
-            .attr("width", x.bandwidth())
-            .attr("height", d => (height - y(d.countField)!))
+            .attr("x", d => xScale(d.binField)!)
+            .attr("y", d => yScale(d.countField)!)
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => (height - yScale(d.countField)!))
             .attr("fill", "gray");
 
         // Draw the clipped bars
@@ -148,10 +148,10 @@ function Example(): React.ReactElement {
             .data(rows)
             .enter()
             .append('rect')
-            .attr("x", d => x(d.binField)!)
-            .attr("y", d => y(d.countField)!)
-            .attr("width", x.bandwidth())
-            .attr("height", d => (height - y(d.countField)!))
+            .attr("x", d => xScale(d.binField)!)
+            .attr("y", d => yScale(d.countField)!)
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => (height - yScale(d.countField)!))
             .attr('clip-path', 'url(#brush_clip)');
 
         const onBrushEnd = (e: d3.D3BrushEvent<unknown>) => {
@@ -170,14 +170,14 @@ function Example(): React.ReactElement {
                 .attr('x', sel[0])
                 .attr('width', sel[1] - sel[0]);
 
-            var eachBand = x.step();
+            var eachBand = xScale.step();
             onRangeSelection(sel[0] / eachBand, sel[1] / eachBand);
         }
         // Define the brush
         const brush = d3.brushX()
             .extent([
-                [x.range()[0], margin.top],
-                [x.range()[1], height]
+                [xScale.range()[0], margin.top],
+                [xScale.range()[1], height]
             ])
             .on('start', onBrush)
             .on('brush', onBrush)
@@ -193,7 +193,7 @@ function Example(): React.ReactElement {
         // Add the x-axis
         svg.append("g")
             .attr("transform", `translate(${margin.left}, ${margin.top + height})`)
-            .call(d3.axisBottom(x).ticks(2).tickSize(0))
+            .call(d3.axisBottom(xScale).ticks(2).tickSize(0))
 
     }, []);
 
