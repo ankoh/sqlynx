6341ea7a3ff159aa45d8e1ec0b27a4721717920e
André Kohn <kohn.a@outlook.com>
2024-11-25 00:00:45 +0100

    Try adding null bar to histogram

--- packages/sqlynx-app/src/view/internals/plot_internals_page.tsx
+++ packages/sqlynx-app/src/view/internals/plot_internals_page.tsx
@@ -78,31 +78,46 @@ function filterTable(input: arrow.Table<testSchemaType>, selectBegin: number, se
     return filteredTable;
 }
 
+const NULL_SYMBOL = "∅";
+
 export function ExampleHistogram(): React.ReactElement {
     const rows = React.useMemo(() => testTable.toArray(), [testTable]);
 
-    const dataBarsContainer = React.useRef<SVGGElement>(null);
+    const histBarContainer = React.useRef<SVGGElement>(null);
+    const nullBarContainer = React.useRef<SVGGElement>(null);
     const selectionBarContainer = React.useRef<SVGGElement>(null);
     const brushContainer = React.useRef<SVGGElement>(null);
-    const xAxisContainer = React.useRef<SVGGElement>(null);
+    const histAxisContainer = React.useRef<SVGGElement>(null);
+    const nullAxisContainer = React.useRef<SVGGElement>(null);
 
     const rootContainer = React.useRef<HTMLDivElement>(null);
     const rootSize = observeSize(rootContainer);
 
-    var margin = { top: 8, right: 8, bottom: 16, left: 8 },
+    const margin = { top: 8, right: 8, bottom: 16, left: 8 },
         width = (rootSize?.width ?? 130) - margin.left - margin.right,
         height = (rootSize?.height ?? 50) - margin.top - margin.bottom;
 
-    // const xDomain = [rows[0].binField, rows[rows.length - 1].binField];
-    const [xScale, yScale] = React.useMemo(() => ([
+    const nullsWidth = 16;
+    const nullsPadding = 2;
+    const histWidth = width - nullsWidth - nullsPadding;
+
+    const [histXScale, histYScale, nullsXScale, nullsYScale] = React.useMemo(() => ([
         d3.scaleBand()
-            .range([0, width])
+            .range([0, histWidth])
             .domain(rows.map(d => d.binField))
             .padding(0.1),
         d3.scaleLinear()
-            .domain([0, 100])
             .range([height, 0])
-    ]), [width, height]);
+            .domain([0, 100]),
+        d3.scaleBand()
+            .range([0, nullsWidth])
+            .domain([NULL_SYMBOL])
+            .padding(0.1),
+        d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, 100])
+
+    ]), [histWidth, height]);
 
     const [rangeSelection, setRangeSelection] = React.useState<[number, number] | null>(null);
     const onRangeSelection = React.useCallback((selBegin: number, selEnd: number) => {
@@ -117,30 +132,48 @@ export function ExampleHistogram(): React.ReactElement {
             .data(rows)
             .enter()
             .append('rect')
-            .attr("x", d => xScale(d.binField)!)
-            .attr("y", d => yScale(d.countField)!)
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => (height - yScale(d.countField)!));
-    }, [xScale, yScale]);
+            .attr("x", d => histXScale(d.binField)!)
+            .attr("y", d => histYScale(d.countField)!)
+            .attr("width", histXScale.bandwidth())
+            .attr("height", d => (height - histYScale(d.countField)!));
+    }, [histXScale, histYScale]);
     const onRangeSelectionEnd = React.useCallback(() => {
         setRangeSelection(null);
     }, []);
 
     React.useLayoutEffect(() => {
-        // Draw the default bars
-        d3.select(dataBarsContainer.current)
+        // Draw the histogram bars
+        d3.select(histBarContainer.current)
             .selectChildren()
             .remove();
-        d3.select(dataBarsContainer.current)
+        d3.select(histBarContainer.current)
             .selectAll("rect")
             .data(rows)
             .enter()
             .append("rect")
-            .attr("x", d => xScale(d.binField)!)
-            .attr("y", d => yScale(d.countField)!)
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => (height - yScale(d.countField)!))
+            .attr("x", d => histXScale(d.binField)!)
+            .attr("y", d => histYScale(d.countField)!)
+            .attr("width", histXScale.bandwidth())
+            .attr("height", d => (height - histYScale(d.countField)!))
             .attr("fill", "gray");
+
+        // Draw null bar
+        d3.select(nullBarContainer.current)
+            .selectChildren()
+            .remove();
+        d3.select(nullBarContainer.current)
+            .selectAll("rect")
+            .data([{
+                x: nullsXScale(NULL_SYMBOL)!,
+                y: nullsYScale(50)!
+            }])
+            .enter()
+            .append("rect")
+            .attr("x", d => d.x)
+            .attr("y", d => d.y)
+            .attr("width", histXScale.bandwidth())
+            .attr("height", d => (height - d.y))
+            .attr("fill", "orange");
 
         const onBrushEnd = (e: d3.D3BrushEvent<unknown>) => {
             if (!e.selection || !e.selection.length) {
@@ -149,14 +182,14 @@ export function ExampleHistogram(): React.ReactElement {
         }
         const onBrush = (e: d3.D3BrushEvent<unknown>) => {
             const sel = e.selection as [number, number];
-            var eachBand = xScale.step();
+            var eachBand = histXScale.step();
             onRangeSelection(sel[0] / eachBand, sel[1] / eachBand);
         }
         // Define the brush
         const brush = d3.brushX()
             .extent([
-                [xScale.range()[0], 0],
-                [xScale.range()[1], height]
+                [histXScale.range()[0], 0],
+                [histXScale.range()[1], height]
             ])
             .on('start', onBrush)
             .on('brush', onBrush)
@@ -170,11 +203,15 @@ export function ExampleHistogram(): React.ReactElement {
             // .attr("transform", `translate(${margin.left}, ${margin.top})`)
             .attr('height', height);
 
-        // Add the x-axis
-        d3.select(xAxisContainer.current!)
-            .call(d3.axisBottom(xScale).ticks(2).tickSize(0));
+        // Add the histogram x-axis
+        d3.select(histAxisContainer.current!)
+            .call(d3.axisBottom(histXScale).ticks(2).tickSize(0));
 
-    }, [xScale, yScale]);
+        // Add the nulls x-axis
+        d3.select(nullAxisContainer.current!)
+            .call(d3.axisBottom(nullsXScale).tickValues([NULL_SYMBOL]).tickSize(0));
+
+    }, [histXScale, histYScale]);
 
     return (
         <div className={styles.histogram_container} ref={rootContainer}>
@@ -184,10 +221,12 @@ export function ExampleHistogram(): React.ReactElement {
                 height={height + margin.top + margin.bottom}
             >
                 <g transform={`translate(${margin.left},${margin.top})`}>
-                    <g ref={dataBarsContainer} />
+                    <g ref={histBarContainer} />
                     <g ref={selectionBarContainer} />
                     <g ref={brushContainer} />
-                    <g ref={xAxisContainer} transform={`translate(0, ${height})`} />
+                    <g ref={histAxisContainer} transform={`translate(0, ${height})`} />
+                    <g ref={nullBarContainer} transform={`translate(${histWidth + nullsPadding}, 0)`} />
+                    <g ref={nullAxisContainer} transform={`translate(${histWidth + nullsPadding}, ${height})`} />
                 </g>
 
             </svg>
