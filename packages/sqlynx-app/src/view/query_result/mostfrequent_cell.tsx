34d6e7f0f008b639090983b340a17b86459a265b
André Kohn <kohn.a@outlook.com>
2024-12-05 10:18:57 +0100

    Construct x-scale for frequent value plot

--- packages/sqlynx-app/src/view/query_result/mostfrequent_cell.tsx
+++ packages/sqlynx-app/src/view/query_result/mostfrequent_cell.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import * as d3 from 'd3';
 import * as styles from './mostfrequent_cell.module.css';
 
-import { ColumnSummaryVariant, TableSummary } from '../../compute/table_transforms.js';
-import { dataTypeToString } from './arrow_formatter.js';
+import { ColumnSummaryVariant, FrequentValuesTable, LIST_COLUMN, STRING_COLUMN, TableSummary } from '../../compute/table_transforms.js';
+import { ArrowTableFormatter, dataTypeToString } from './arrow_formatter.js';
+import { observeSize } from '../../view/foundations/size_observer.js';
+import { assert } from '../../utils/assert.js';
 
 interface MostFrequentCellProps {
     tableSummary: TableSummary;
@@ -11,6 +14,47 @@ interface MostFrequentCellProps {
 
 export function MostFrequentCell(props: MostFrequentCellProps): React.ReactElement {
     const rootContainer = React.useRef<HTMLDivElement>(null);
+    const svgContainer = React.useRef<HTMLDivElement>(null);
+    const svgContainerSize = observeSize(svgContainer);
+    const mostFrequentBarContainer = React.useRef<SVGGElement>(null);
+
+    const margin = { top: 8, right: 8, bottom: 16, left: 8 },
+        width = (svgContainerSize?.width ?? 130) - margin.left - margin.right,
+        height = (svgContainerSize?.height ?? 50) - margin.top - margin.bottom;
+
+    // Resolve the frequent values
+    let frequentValues: FrequentValuesTable | null = null;
+    let frequentValuesFormatter: ArrowTableFormatter | null = null;
+    switch (props.columnSummary.type) {
+        case STRING_COLUMN:
+        case LIST_COLUMN:
+            frequentValues = props.columnSummary.value.frequentValues;
+            frequentValuesFormatter = props.columnSummary.value.frequentValuesFormatter;
+            break;
+        default:
+            break;
+    }
+
+    // Compute x-scale and offsets
+    const [xScale, xOffsets, xCounts] = React.useMemo(() => {
+        if (frequentValues == null) {
+            return [null, null, null];
+        }
+        assert(frequentValues.schema.fields[1].name == "count");
+
+        const xCounts: BigInt64Array = frequentValues.getChild("count")!.toArray();
+        const xOffsets: BigInt64Array = new BigInt64Array(xCounts.length);
+        let xSum = BigInt(0);
+        for (let i = 0; i < xCounts.length; ++i) {
+            xOffsets[i] = xSum;
+            xSum += xCounts[i];
+        }
+        const xScale = d3.scaleLinear()
+            .range([0, width])
+            .domain([0, Number(xSum)]);
+
+        return [xScale, xOffsets, xCounts];
+    }, [frequentValues, width]);
 
     if (props.columnSummary.value == null) {
         return <div />;
@@ -20,6 +64,18 @@ export function MostFrequentCell(props: MostFrequentCellProps): React.ReactEleme
         <div className={styles.root} ref={rootContainer}>
             <div className={styles.header_container}>
                 {dataTypeToString(props.columnSummary.value.columnEntry.inputFieldType)}
+            </div>
+            <div className={styles.plot_container} ref={svgContainer}>
+                <svg
+                    className={styles.plot_svg}
+                    width={width + margin.left + margin.right}
+                    height={height + margin.top + margin.bottom}
+                >
+                    <g transform={`translate(${margin.left},${margin.top})`}>
+                        <g ref={mostFrequentBarContainer} />
+                    </g>
+
+                </svg>
             </div>
         </div>
     );
