b06eeec3755d7756f2e26a1fd0ba82b705b334ba
André Kohn <andre.kohn@salesforce.com>
2025-03-21 23:45:50 +0100

    Add helper to transform the catalog file proto to a core flatbuffer

---
+++ packages/dashql-app/src/connection/catalog_import.ts
@@ -0,0 +1,30 @@
+import * as dashql from '@ankoh/dashql-core';
+import * as proto from '@ankoh/dashql-protobuf';
+
+export function decodeCatalogFileFromProto(file: proto.dashql_file.pb.FileCatalog): dashql.proto.SchemaDescriptorsT {
+    if (!file.catalog) {
+        return new dashql.proto.SchemaDescriptorsT();
+    }
+    let tableCount: number = 0;
+    const schemas: dashql.proto.SchemaDescriptorT[] = [];
+
+    for (const dbReader of file.catalog.databases) {
+        for (const schemaReader of dbReader.schemas) {
+            const tables: dashql.proto.SchemaTableT[] = [];
+            for (const tableReader of schemaReader.tables) {
+                const columns: dashql.proto.SchemaTableColumnT[] = [];
+                let ordinalPos = 0;
+                for (const columnReader of tableReader.columns) {
+                    const column = new dashql.proto.SchemaTableColumnT(columnReader.name, ordinalPos++);
+                    columns.push(column);
+                }
+                const tableId = tableCount++;
+                const table = new dashql.proto.SchemaTableT(tableId, tableReader.name, columns);
+                tables.push(table);
+            }
+            const schema = new dashql.proto.SchemaDescriptorT(dbReader.name, schemaReader.name, tables);
+            schemas.push(schema);
+        }
+    }
+    return new dashql.proto.SchemaDescriptorsT(schemas);
+}
